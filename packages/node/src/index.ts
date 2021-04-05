35bc57c91bb5269432b1a48b177f7e1da2d7a47b
Francisco Ramini <framini@gmail.com>
2021-04-05 12:30:10 +0200

    update node package with new bundle settings

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,5 +1,9 @@
+import * as core from "@signalwire/core";
+
 export const sum = (a: number, b: number) => {
   if ("development" === process.env.NODE_ENV) {
+    console.log("Core feature", core.sum(a, b));
+
     console.log("boop");
   }
   return a + b;
