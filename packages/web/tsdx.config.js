63b0e275d4aa4d94d70265ef9278c519cdcadc8a
Francisco Ramini <framini@gmail.com>
2021-04-05 12:23:07 +0200

    add ability to bundle internal packages

---
+++ packages/web/tsdx.config.js
@@ -0,0 +1,30 @@
+const resolve = require("@rollup/plugin-node-resolve");
+const replace = require("@rollup/plugin-replace");
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins = config.plugins.map((plugin) => {
+      if (plugin.name === "replace") {
+        return replace({
+          "process.env.NODE_ENV": JSON.stringify(options.env),
+          preventAssignment: true,
+        });
+      }
+
+      return plugin;
+    });
+
+    config.external = (id) => {
+      if (
+        id.startsWith("@signalwire") ||
+        id.startsWith("regenerator-runtime")
+      ) {
+        return false;
+      }
+
+      return true;
+    };
+
+    return config;
+  },
+};
