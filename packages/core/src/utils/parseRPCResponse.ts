d49d4e8bd7746c994b61f9cb1b27755591a869d4
Edoardo Gallo <edo91.gallo@gmail.com>
2021-04-07 13:40:52 +0200

    parseRPCResponse by request method

---
+++ packages/core/src/utils/parseRPCResponse.ts
@@ -0,0 +1,73 @@
+import { JSONRPCRequest, JSONRPCResponse } from './interfaces'
+import { BladeMethod } from './constants'
+
+type parseRPCResponseParams = {
+  response: JSONRPCResponse
+  request: JSONRPCRequest
+}
+export const parseRPCResponse = ({
+  response,
+  request,
+}: parseRPCResponseParams) => {
+  const { result = {}, error } = response
+  if (error) {
+    return {
+      error,
+    }
+  }
+
+  switch (request.method) {
+    case BladeMethod.Connect:
+      return result
+    // case BladeMethod.Execute:
+    //   return parseBladeExecuteResult(result)
+    default:
+      return parseResponse(response)
+  }
+}
+
+/**
+ * Destructure blade.execute response
+ *
+ * @param response JSONRPCResponse
+ */
+// const parseBladeExecuteResult = (response: JSONRPCResponse['result']) => {
+//   const { result = {}, error } = response
+// }
+
+/**
+ * From the socket we can get:
+ * - JSON-RPC msg with 1 level of 'result' or 'error'
+ * - JSON-RPC msg with 2 nested 'result' and 'code' property to identify error
+ * - JSON-RPC msg with 3 nested 'result' where the third level is the Verto JSON-RPC flat msg.
+ *
+ * @returns Object with error | result key to identify success or fail
+ */
+const parseResponse = (
+  response: JSONRPCResponse,
+  nodeId?: string
+): { [key: string]: any } => {
+  const { result = {}, error } = response
+  if (error) {
+    return { error }
+  }
+  const { result: nestedResult = null } = result
+  if (nestedResult === null) {
+    if (nodeId) {
+      result.node_id = nodeId
+    }
+    return { result }
+  }
+  const {
+    code = null,
+    node_id = null,
+    result: vertoResult = null,
+  } = nestedResult
+  if (code && code !== '200') {
+    return { error: nestedResult }
+  }
+  if (vertoResult) {
+    return parseResponse(vertoResult, node_id)
+  }
+  return { result: nestedResult }
+}
