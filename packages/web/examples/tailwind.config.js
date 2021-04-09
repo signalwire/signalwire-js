e36be203356582c9be1284845b9e99b7d779adbe
Francisco Ramini <framini@gmail.com>
2021-04-09 13:20:37 +0200

    add slightly better example docs

--- packages/web/examples/tailwind.config.js
+++ packages/web/examples/tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   mode: 'jit',
   purge: [
     'index.html',
-    './ts-vanilla/**/*.html',
-    './ts-vanilla/**/*.{js,jsx,ts,tsx,vue}',
+    './*vanilla/**/*.html',
+    './*vanilla/**/*.{js,jsx,ts,tsx,vue}',
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   darkMode: false, // or 'media' or 'class'
