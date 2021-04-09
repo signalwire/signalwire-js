75b4b3d21556315712645c8e3c9b7217a641079b
Francisco Ramini <framini@gmail.com>
2021-04-09 12:22:05 +0200

    group examples by name and framework, add skeleton fro vanilla js example

--- packages/web/examples/index.js
+++ packages/web/examples/index.js
@@ -1,41 +1,60 @@
 import { html, render } from 'https://unpkg.com/lit-html?module'
 
-export const EXAMPLES = [
+const LANGUAGES = {
+  TypeScript:
+    'https://raw.githubusercontent.com/gilbarbara/logos/master/logos/typescript-icon.svg',
+  JavaScript:
+    'https://raw.githubusercontent.com/gilbarbara/logos/master/logos/javascript.svg',
+}
+
+const EXAMPLES = [
   {
     name: 'Vanilla',
-    url: '/ts-vanilla/',
-    language: 'TypeScript',
+    // Example Framework with image and link:
+    //   framework: {
+    //     logo:
+    //       'https://raw.githubusercontent.com/gilbarbara/logos/master/logos/react.svg',
+    //     name: 'React',
+    //     url: 'https://reactjs.org',
+    //   },
     framework: {
       name: 'None',
     },
+    links: [
+      {
+        url: '/ts-vanilla/',
+        language: 'TypeScript',
+      },
+      {
+        url: '/vanilla/',
+        language: 'JavaScript',
+      },
+    ],
   },
-  // {
-  //   name: "React",
-  //   url: "/ts-react",
-  //   language: "TypeScript",
-  //   framework: {
-  //     logo:
-  //       'https://raw.githubusercontent.com/gilbarbara/logos/master/logos/react.svg',
-  //     name: 'React',
-  //     url: 'https://reactjs.org',
-  //   },
-  // }
 ]
 
 const template = (examples) => html`
   ${examples.map(
     (example) => html`<tr>
-      <td class="px-6 py-4 whitespace-nowrap">
-        <a class="text-indigo-600 hover:text-indigo-900" href="${example.url}"
-          >${example.name}</a
-        >
-      </td>
+      <td class="px-6 py-4 whitespace-nowrap">${example.name}</td>
       <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-        ${example.language}
+        <div class="flex items-center">
+          ${example.links.map((link, i) => {
+            return html`<a href="${link.url}">
+              <img
+                src="${LANGUAGES[link.language]}"
+                width="20"
+                height="20"
+                title="${link.language}"
+                class="${i > 0 ? 'ml-2' : ''}"
+              />
+            </a>`
+          })}
+        </div>
       </td>
       <td class="px-6 py-4 whitespace-nowrap text-sm">
         ${example.framework.logo && example.framework.url
-          ? html` <div class="flex items-center">
+          ? html`<div class="flex items-center">
               <img
                 src="${example.framework.logo}"
                 width="20"
