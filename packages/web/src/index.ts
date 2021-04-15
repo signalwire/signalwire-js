d7268d770077f11d8d4a5272a8d3e262c102a6e2
Edoardo Gallo <edo91.gallo@gmail.com>
2021-04-15 16:33:02 +0200

    move createSession within web package

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,3 +1,54 @@
-import { JWTSession, createSession } from '@signalwire/core'
+import {
+  JWTSession,
+  SignalWire,
+  configureStore,
+  connect,
+} from '@signalwire/core'
+import { Call } from '@signalwire/webrtc'
 
-export { JWTSession, createSession }
+export { JWTSession }
+
+class Client extends SignalWire {
+  get rooms() {
+    return {
+      // TODO: use CallOptions interface here
+      makeCall: (options: any) => {
+        return connect({
+          store: this.store,
+          Component: Call,
+          onStateChangeListeners: {
+            state: 'onStateChange',
+            remoteSDP: 'onRemoteSDP',
+            errors: 'onError',
+            responses: 'onSuccess',
+          },
+        })(options)
+      },
+    }
+  }
+}
+
+export const createSession = (userOptions: any) => {
+  return new Promise((resolve, _reject) => {
+    const store = configureStore()
+    const client = new Client(userOptions, store)
+    if (userOptions.autoConnect) {
+      store.subscribe(() => {
+        const state = store.getState()
+        // @ts-ignore
+        if (state?.STORE_READY) {
+          resolve(client)
+        }
+      })
+
+      client.connect()
+      // Fake the redux subscribe above for now
+      setTimeout(() => {
+        resolve(client)
+      }, 2000)
+      // store.dispatch(initSessionAction(userOptions))
+    } else {
+      resolve(client)
+    }
+  })
+}
