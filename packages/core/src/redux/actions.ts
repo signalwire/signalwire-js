711b208eabe9603348976fd12f326b2603d2ab55
Edoardo Gallo <edo91.gallo@gmail.com>
2021-04-15 16:30:55 +0200

    create executeAction and remove custom strings

--- packages/core/src/redux/actions.ts
+++ packages/core/src/redux/actions.ts
@@ -2,3 +2,5 @@ import { createAction } from '@reduxjs/toolkit'
 
 export const initSessionAction = createAction<any>('swSdk/init')
 export const destroySessionAction = createAction('swSdk/destroy')
+
+export const executeAction = createAction<any>('swSdk/executeRequest')
