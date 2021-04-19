8959a425ba214ea33b9fcfa840247962ee2a6138
Francisco Ramini <framini@gmail.com>
2021-04-19 12:38:42 +0200

    make SignalWire an emitter

--- packages/core/src/SignalWire.ts
+++ packages/core/src/SignalWire.ts
@@ -1,7 +1,13 @@
 import { initSessionAction, destroySessionAction } from './redux'
-
-export class SignalWire {
+import { Emitter } from './utils/interfaces'
+export class SignalWire implements Emitter<SignalWire> {
   constructor(public options: any, public store: any) {}
+
+  on = this.options.emitter.on
+  off = this.options.emitter.off
+  once = this.options.emitter.once
+  removeAllListeners = this.options.emitter.removeAllListeners
+  emit = this.options.emitter.emit
 
   connect() {
     this.store.dispatch(initSessionAction())
