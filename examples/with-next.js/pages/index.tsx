e5bbe8e2e6d629258cb98b866fea1449c871287c
Francisco Ramini <framini@gmail.com>
2021-04-20 12:11:12 +0200

    add basic next.js project

---
+++ examples/with-next.js/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title='Home | Next.js + VideoSDK Example'>
+    <h1>👋</h1>
+  </Layout>
+)
+
+export default IndexPage
