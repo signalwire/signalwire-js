cca7220df192379c4d4a0cafaa57610ba0577131
Francisco Ramini <framini@gmail.com>
2021-04-22 11:37:18 +0200

    add app controller for handling access control and show and handle global state

---
+++ examples/with-next.js/src/components/AppController.tsx
@@ -0,0 +1,79 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  useContext,
+  useReducer,
+} from 'react'
+
+type AppStatus = 'idle' | 'authorized' | 'active'
+
+type AppControllerState =
+  | {
+      status: Extract<AppStatus, 'idle'>
+    }
+  | {
+      status: Exclude<AppStatus, 'idle'>
+      projectId: string
+      token: string
+    }
+
+const initialState: AppControllerState = {
+  status: 'idle',
+}
+
+const AppControllerStateContext = createContext<AppControllerState>(
+  initialState
+)
+const AppControllerDispatchContext = createContext<
+  Dispatch<AppControllerAction>
+>(() => {})
+
+type AppControllerAction = {
+  type: 'authorized'
+  payload: {
+    projectId: string
+    token: string
+  }
+}
+
+const reducer = (state: AppControllerState, action: AppControllerAction) => {
+  switch (action.type) {
+    case 'authorized': {
+      const newState: AppControllerState = {
+        ...state,
+        status: 'authorized',
+        ...action.payload,
+      }
+
+      return newState
+    }
+
+    default:
+      return state
+  }
+}
+
+interface AppControllerProps {
+  children: ReactNode
+}
+
+export const AppController = ({ children }: AppControllerProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <AppControllerDispatchContext.Provider value={dispatch}>
+      <AppControllerStateContext.Provider value={state}>
+        {children}
+      </AppControllerStateContext.Provider>
+    </AppControllerDispatchContext.Provider>
+  )
+}
+
+export const useAppState = () => {
+  return useContext(AppControllerStateContext)
+}
+
+export const useAppDispatch = () => {
+  return useContext(AppControllerDispatchContext)
+}
