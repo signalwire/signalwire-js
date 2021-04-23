2030f71579e203644ceae53b3ef5162423ebb4fa
Francisco Ramini <framini@gmail.com>
2021-04-23 11:22:20 +0200

    improve ui of join-widget

--- examples/with-next.js/src/components/JoinWidget.tsx
+++ examples/with-next.js/src/components/JoinWidget.tsx
@@ -4,14 +4,19 @@ import { useAppDispatch } from './AppController'
 
 const InputField = forwardRef<
   HTMLInputElement,
-  InputHTMLAttributes<HTMLInputElement>
->((props, ref) => {
+  InputHTMLAttributes<HTMLInputElement> & { label: string }
+>(({ label, ...props }, ref) => {
   const hasErrors = props['aria-invalid'] === 'true'
   return (
-    <>
-      <input {...props} ref={ref} />
+    <label className='block text-sm font-medium text-gray-700 text-left'>
+      {label}
+      <input
+        {...props}
+        ref={ref}
+        className='appearance-none block w-full px-3 py-2 mt-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+      />
       {hasErrors && <span role='alert'>This is required</span>}
-    </>
+    </label>
   )
 })
 
@@ -39,18 +44,28 @@ export const JoinWidget = () => {
 
         dispatch({ type: 'authorized', payload: data.data })
       })}
+      className='space-y-6'
     >
       <InputField
         id='room_name'
+        label='Room Name'
         aria-invalid={errors.room_name ? 'true' : 'false'}
         {...register('room_name', { required: true })}
       />
+
       <InputField
         id='user_name'
+        label='User Name'
         aria-invalid={errors.user_name ? 'true' : 'false'}
         {...register('user_name', { required: true })}
       />
-      <button type='submit'>Join</button>
+
+      <button
+        type='submit'
+        className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+      >
+        Join
+      </button>
     </form>
   )
 }
