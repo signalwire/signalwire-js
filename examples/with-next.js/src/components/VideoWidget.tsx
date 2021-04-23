b31e9bbba69ad7b5cc37308ce580948c10aaa3e4
Francisco Ramini <framini@gmail.com>
2021-04-23 11:22:41 +0200

    add client.disconnect when clicking on hangup, add styles

--- examples/with-next.js/src/components/VideoWidget.tsx
+++ examples/with-next.js/src/components/VideoWidget.tsx
@@ -52,9 +52,11 @@ export const VideoWidget = ({
   if (status === 'ready') {
     return (
       <button
-        onClick={() => {
-          call.hangup()
+        onClick={async () => {
+          await call.hangup()
+          client.disconnect()
         }}
+        className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
       >
         Hangup
       </button>
