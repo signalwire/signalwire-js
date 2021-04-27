034b9d60b54cebf25c1e3c47fc6e0ac8997f0009
Edoardo Gallo <edo91.gallo@gmail.com>
2021-04-27 10:12:53 +0200

    create testUtils file and provide initial slice specs for sessionSlice

---
+++ packages/core/src/redux/features/session/sessionSlice.test.ts
@@ -0,0 +1,28 @@
+import { Store } from 'redux'
+import { configureJestStore, bladeConnectResultVRT } from '../../../testUtils'
+import { sessionActions, initialSessionState } from './sessionSlice'
+import { destroyAction } from '../../actions'
+
+describe('SessionState Tests', () => {
+  let store: Store
+
+  beforeEach(() => {
+    store = configureJestStore()
+  })
+
+  it('should listen calls/destroy actions and save the history', () => {
+    store.dispatch(sessionActions.connected(bladeConnectResultVRT))
+
+    expect(store.getState().session).toStrictEqual({
+      protocol: bladeConnectResultVRT.result?.protocol,
+      iceServers: bladeConnectResultVRT.result?.iceServers,
+    })
+  })
+
+  it('should reset to initial on destroyAction', () => {
+    store.dispatch(sessionActions.connected(bladeConnectResultVRT))
+
+    store.dispatch(destroyAction())
+    expect(store.getState().session).toStrictEqual(initialSessionState)
+  })
+})
