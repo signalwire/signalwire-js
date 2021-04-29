511dfeab8cb0c5248036239b5cfe6cdf1d7cbe73
Edoardo Gallo <edo91.gallo@gmail.com>
2021-04-29 15:51:50 +0200

    initial tests for connect

--- packages/core/src/redux/connect.ts
+++ packages/core/src/redux/connect.ts
@@ -17,7 +17,6 @@ export const connect = <T extends typeof BaseComponent>(
   const componentKeys = Object.keys(onStateChangeListeners)
 
   return (userOptions: any) => {
-    console.log('userOptions ?', { ...userOptions, store })
     const instance = new Component({ ...userOptions, store })
     const cacheMap = new Map<string, any>()
 
