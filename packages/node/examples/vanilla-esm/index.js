fce487455f949d87c0ade8a65107437aa42ec64a
Francisco Ramini <framini@gmail.com>
2021-05-05 12:10:55 +0200

    update node example to use the new apis

--- packages/node/examples/vanilla-esm/index.js
+++ packages/node/examples/vanilla-esm/index.js
@@ -1,3 +1,29 @@
-import * as sdkNode from '@signalwire/node'
+import { createClient } from '@signalwire/node'
 
-console.log('@signalwire/node', sdkNode)
+try {
+  const client = createClient({
+    projectId: '<project-id>',
+    projectToken: '<project-token>',
+  })
+
+  const roomName = 'lobby'
+
+  const existingRoom = await client.getRoomByName({
+    name: roomName,
+  })
+
+  if (!existingRoom) {
+    await client.createRoom({
+      name: roomName,
+    })
+  }
+
+  const vrt = await client.createVRT({
+    roomName: roomName,
+    userName: 'Some User',
+  })
+
+  console.log('VRT', vrt)
+} catch (error) {
+  console.error('Error', error.response)
+}
