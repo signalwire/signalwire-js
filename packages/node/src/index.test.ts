476a47e8690926ffb03c00316a92a7deb3405b89
Francisco Ramini <framini@gmail.com>
2021-05-07 10:18:37 +0200

    add basic test for createRestClient

--- packages/node/src/index.test.ts
+++ packages/node/src/index.test.ts
@@ -1,5 +1,21 @@
-describe('Node Entry Point', () => {
-  it('should work', () => {
-    expect(2).toEqual(2)
+import { createRestClient } from './index'
+
+describe('createRestClient', () => {
+  it('should return an object with all the available methods', () => {
+    const client = createRestClient({
+      projectId: '<pid>',
+      projectToken: '<pt>',
+    })
+
+    expect(client).toHaveProperty('createRoom')
+    expect(client).toHaveProperty('getRoomById')
+    expect(client).toHaveProperty('getRoomByName')
+    expect(client).toHaveProperty('listAllRooms')
+    expect(client).toHaveProperty('updateRoom')
+    expect(client).toHaveProperty('createVRT')
+  })
+
+  it('should throw when required options are missing', () => {
+    expect(() => createRestClient()).toThrow('Missing required options')
   })
 })
