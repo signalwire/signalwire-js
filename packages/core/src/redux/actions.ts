fbdeb94fc26ab5843baed2816f21e7db4ad8fd54
Francisco Ramini <framini@gmail.com>
2021-05-12 10:18:47 +0200

    update typings

--- packages/core/src/redux/actions.ts
+++ packages/core/src/redux/actions.ts
@@ -1,6 +1,6 @@
 import { createAction } from '@reduxjs/toolkit'
 import { JSONRPCRequest, SessionAuthError } from '../utils/interfaces'
-import { ExecuteActionParams } from './interfaces'
+import { ExecuteActionParams, SocketCloseParams } from './interfaces'
 
 export const initAction = createAction('swSdk/init')
 export const destroyAction = createAction('swSdk/destroy')
@@ -15,9 +15,7 @@ export const executeAction = createAction<ExecuteActionParams>(
 export const authError = createAction<{ error: SessionAuthError }>('auth/error')
 export const authSuccess = createAction('auth/success')
 
-export const socketClosed = createAction<{ reason: string; code: number }>(
-  'socket/closed'
-)
+export const socketClosed = createAction<SocketCloseParams>('socket/closed')
 export const socketError = createAction('socket/error')
 export const socketMessage = createAction<JSONRPCRequest, string>(
   'socket/message'
