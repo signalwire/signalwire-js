947fc8702528b4aff0777cf2cd3ce2ff13e06492
Francisco Ramini <framini@gmail.com>
2021-05-18 11:12:07 +0200

    simplified generics for Call, fix typing issue for BaseCall

--- packages/webrtc/src/Call.ts
+++ packages/webrtc/src/Call.ts
@@ -1,7 +1,6 @@
-import { CallEvents } from '@signalwire/core'
 import { BaseCall } from './BaseCall'
 
-export class Call<T extends string = CallEvents> extends BaseCall<T> {
+export class Call extends BaseCall {
   desktopOnlyMethod() {
     console.debug('Desktop Method')
   }
