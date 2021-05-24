f806b5939b45cc43c587130bb371b5e9966df136
Francisco Ramini <framini@gmail.com>
2021-05-24 12:07:31 +0200

    move video exports to separate file so we can generate it as a namespace rather than a variable

---
+++ packages/web/src/video.ts
@@ -0,0 +1,6 @@
+import { createRoomObject } from './createRoomObject'
+import { createClient } from './createClient'
+import { Client } from './Client'
+import { joinRoom } from './joinRoom'
+
+export { createRoomObject, createClient, Client, joinRoom }
