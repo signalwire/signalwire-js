d7cb0ac6604451d198cdaa6b23eb68cb5b333c98
Edoardo Gallo <edo91.gallo@gmail.com>
2021-06-16 13:47:47 +0200

    whitelist export from deviceHelpers

--- packages/webrtc/src/index.ts
+++ packages/webrtc/src/index.ts
@@ -1,4 +1,26 @@
 export { Call } from './Call'
-export * from './utils/deviceHelpers'
+export {
+  getDevices,
+  getCameraDevices,
+  getMicrophoneDevices,
+  getSpeakerDevices,
+  getDevicesWithPermissions,
+  getCameraDevicesWithPermissions,
+  getMicrophoneDevicesWithPermissions,
+  getSpeakerDevicesWithPermissions,
+  checkPermissions,
+  checkCameraPermissions,
+  checkMicrophonePermissions,
+  checkSpeakerPermissions,
+  assureDeviceId,
+  assureVideoDevice,
+  assureAudioInDevice,
+  assureAudioOutDevice,
+  requestPermissions,
+  createDeviceWatcher,
+  createMicrophoneDeviceWatcher,
+  createSpeakerDeviceWatcher,
+  createCameraDeviceWatcher,
+} from './utils/deviceHelpers'
 export * from './utils/webrtcHelpers'
 export * from './utils/interfaces'
