7c7a950928d42ed9c6a53ba9163b1227662a1dcf
Edoardo Gallo <edo91.gallo@gmail.com>
2021-06-18 19:55:40 +0200

    expose interfaces from core

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,2 +1,8 @@
 export * as Video from './video'
 export * as WebRTC from '@signalwire/webrtc'
+export {
+  BaseClientOptions,
+  ClientEvents,
+  UserOptions,
+  Emitter,
+} from '@signalwire/core'
