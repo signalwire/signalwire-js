fe198e48d1a500681d923915773574255d74b41e
Edoardo Gallo <edo91.gallo@gmail.com>
2021-06-25 23:34:21 +0200

    do not expose createClient

--- packages/js/src/video.ts
+++ packages/js/src/video.ts
@@ -1,10 +1,9 @@
-import { createClient } from './createClient'
 import { createRoomObject } from './createRoomObject'
 import { joinRoom } from './joinRoom'
 import { Client } from './Client'
 
 export { Room } from '@signalwire/webrtc'
-export { Client, createClient, createRoomObject, joinRoom }
+export { Client, createRoomObject, joinRoom }
 
 export type { RoomObject } from '@signalwire/webrtc'
 export type { CreateRoomObjectOptions } from './createRoomObject'
