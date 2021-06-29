f9703aa10f10de87520fc0bca8fecb0e7701192c
Francisco Ramini <framini@gmail.com>
2021-06-29 10:45:21 +0200

    add tests for executeQueueSaga

---
+++ packages/core/src/redux/features/executeQueue/executeQueueSlice.test.ts
@@ -0,0 +1,50 @@
+import { Store } from 'redux'
+import { configureJestStore } from '../../../testUtils'
+import {
+  executeQueueActions,
+  initialExecuteQueueState,
+} from './executeQueueSlice'
+import { ExecuteActionParams } from '../../interfaces'
+import { destroyAction } from '../../actions'
+
+describe('ExecuteQueueState Tests', () => {
+  let store: Store
+
+  beforeEach(() => {
+    store = configureJestStore()
+  })
+
+  it('should add ExecuteParams to the queue on executeQueueActions.add action and reset it on executeQueueActions.add action', () => {
+    const payload: ExecuteActionParams = {
+      method: 'signalwire.subscribe',
+      params: {
+        event_channel: 'rooms',
+        get_initial_state: true,
+        events: ['room.started', 'room.ended'],
+      },
+    }
+
+    store.dispatch(executeQueueActions.add(payload))
+    expect(store.getState().executeQueue.queue).toStrictEqual([payload])
+
+    store.dispatch(executeQueueActions.clean())
+    expect(store.getState().executeQueue.queue).toStrictEqual([])
+  })
+
+  it('should reset to initial on destroyAction', () => {
+    const payload: ExecuteActionParams = {
+      method: 'signalwire.subscribe',
+      params: {
+        event_channel: 'rooms',
+        get_initial_state: true,
+        events: ['room.started', 'room.ended'],
+      },
+    }
+    store.dispatch(executeQueueActions.add(payload))
+
+    store.dispatch(destroyAction())
+    expect(store.getState().executeQueue).toStrictEqual(
+      initialExecuteQueueState
+    )
+  })
+})
