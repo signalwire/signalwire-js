ccd91cb328d0fbf1dd6cee87cae27b93bb390711
Francisco Ramini <framini@gmail.com>
2021-07-02 12:16:44 +0200

    remove comment

--- packages/js/examples/heroku/index.js
+++ packages/js/examples/heroku/index.js
@@ -297,7 +297,6 @@ window.changeVideoSource = (select) => {
   }
 
   roomObj.updateDevices(constraints)
-  // roomObj.setLayout({ name: select.value })
 }
 
 window.rangeInputHandler = (range) => {
