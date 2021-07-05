4055ce2bbf3b99f5dab1d2941d16af04e518d3aa
Francisco Ramini <framini@gmail.com>
2021-07-05 12:19:54 +0200

    restore option

--- packages/js/examples/heroku/index.js
+++ packages/js/examples/heroku/index.js
@@ -133,7 +133,7 @@ window.connect = () => {
     host: document.getElementById('host').value,
     token: document.getElementById('token').value,
     rootElementId: 'rootElement',
-    audio: document.getElementById('audio').value,
+    audio: true,
     video: true,
   }).then((roomObject) => {
     roomObj = roomObject
