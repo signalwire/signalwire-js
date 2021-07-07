7c6099dcbd210e3076c628e947f0da8450370c57
Francisco Ramini <framini@gmail.com>
2021-07-07 17:06:00 +0200

    add logic to emit both, global and namespaced events

--- packages/core/src/redux/features/pubSub/pubSubSaga.ts
+++ packages/core/src/redux/features/pubSub/pubSubSaga.ts
@@ -1,5 +1,5 @@
 import { take } from 'redux-saga/effects'
-import { logger } from '../../../utils'
+import { logger, isGlobalEvent } from '../../../utils'
 import { getNamespacedEvent } from '../../../utils/EventEmitter'
 
 const findNamespaceInPayload = (payload?: any): string => {
@@ -17,6 +17,17 @@ export function* pubSubSaga({ pubSubChannel, emitter }: any) {
     const { type, payload } = yield take(pubSubChannel)
     try {
       const namespace = findNamespaceInPayload(payload)
+
+      /**
+       * There are events (like `room.started`/`room.ended`) that can
+       * be consumed from different places, like from a `roomObj`
+       * (namespaced Event Emitter) or from a `client`
+       * (non-namespaced/global Event Emitter) so we must trigger the
+       * event twice to reach everyone.
+       */
+      if (isGlobalEvent(type)) {
+        emitter.emit(type, payload)
+      }
 
       emitter.emit(
         getNamespacedEvent({
