485207f71ae0a850c01e3bba745fbe39120c7551
Francisco Ramini <framini@gmail.com>
2021-07-19 10:04:49 +0200

    export types used in Room constructor

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,3 +1,12 @@
 export * as Video from './video'
 export * as WebRTC from './webrtc'
-export type { ClientEvents, UserOptions, Emitter } from '@signalwire/core'
+export type {
+  ClientEvents,
+  UserOptions,
+  Emitter,
+  BaseComponentOptions,
+} from '@signalwire/core'
+export type {
+  BaseConnectionOptions,
+  ConnectionOptions,
+} from '@signalwire/webrtc'
