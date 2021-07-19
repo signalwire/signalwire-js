7f9573e973d16797f7074e7702db2bdb6a86c9c5
Francisco Ramini <framini@gmail.com>
2021-07-19 11:07:14 +0200

    add makeroomoptions

--- packages/js/src/video.ts
+++ packages/js/src/video.ts
@@ -1,11 +1,12 @@
 import { createRoomObject } from './createRoomObject'
 import { joinRoom } from './joinRoom'
-import { Client } from './Client'
+import { Client, MakeRoomOptions } from './Client'
 import { Room } from './Room'
 import { RoomScreenShare } from './RoomScreenShare'
 import { RoomDevice } from './RoomDevice'
 
 export { Client, createRoomObject, joinRoom, Room, RoomScreenShare, RoomDevice }
 
+export type { MakeRoomOptions }
 export type { RoomObject } from './utils/interfaces'
 export type { CreateRoomObjectOptions } from './createRoomObject'
