17106bb7659ed317e9da30456c1bc5b83c482b76
Edoardo Gallo <edo91.gallo@gmail.com>
2021-07-23 12:40:20 +0200

    set agent on children classes and read version from package.json

--- packages/js/src/JWTSession.ts
+++ packages/js/src/JWTSession.ts
@@ -1,5 +1,7 @@
 import { BaseJWTSession } from '@signalwire/core'
+import { version } from '../package.json'
 
 export class JWTSession extends BaseJWTSession {
   public WebSocketConstructor = WebSocket
+  public agent = `JavaScript SDK/${version}`
 }
--- packages/node/src/Session.ts
+++ packages/node/src/Session.ts
@@ -1,6 +1,8 @@
 import { BaseSession } from '@signalwire/core'
 import WebSocket from 'ws'
+import { version } from '../package.json'
 
 export class Session extends BaseSession {
   public WebSocketConstructor = WebSocket
+  public agent = `Node.js SDK/${version}`
 }
