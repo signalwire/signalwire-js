814c55290dbd3afc4d72a3cf75a710c1fe38c7a8
Francisco Ramini <framini@gmail.com>
2021-07-26 12:33:17 +0200

    update packages to use the new env variable

--- packages/js/src/JWTSession.ts
+++ packages/js/src/JWTSession.ts
@@ -1,7 +1,6 @@
 import { BaseJWTSession } from '@signalwire/core'
-import { version } from '../package.json'
 
 export class JWTSession extends BaseJWTSession {
   public WebSocketConstructor = WebSocket
-  public agent = `JavaScript SDK/${version}`
+  public agent = process.env.SDK_PKG_AGENT!
 }
--- packages/node/src/Session.ts
+++ packages/node/src/Session.ts
@@ -1,8 +1,7 @@
 import { BaseSession } from '@signalwire/core'
 import WebSocket from 'ws'
-import { version } from '../package.json'
 
 export class Session extends BaseSession {
   public WebSocketConstructor = WebSocket
-  public agent = `Node.js SDK/${version}`
+  public agent = process.env.SDK_PKG_AGENT!
 }
