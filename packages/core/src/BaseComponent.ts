74d71be7fd926eca20c624043b294fa3c1f15f7f
Edoardo Gallo <edo91.gallo@gmail.com>
2021-08-02 13:34:22 +0200

    add _eventsPrefix to BaseComponent and use it to prefix the user-end event names on the emitter

--- packages/core/src/BaseComponent.ts
+++ packages/core/src/BaseComponent.ts
@@ -34,6 +34,9 @@ const identity: ExecuteTransform<any, any> = (payload) => payload
 
 export class BaseComponent implements Emitter {
   id = uuid()
+
+  /** @internal */
+  protected _eventsPrefix: '' | 'video' = ''
   private _eventsRegisterQueue = new Set<EventRegisterHandlers>()
   private _eventsEmitQueue = new Set<any>()
   private _eventsNamespace?: string
@@ -41,11 +44,18 @@ export class BaseComponent implements Emitter {
   private _customSagaTriggers = new Map()
   private _destroyer?: () => void
   private _getNamespacedEvent(event: string | symbol) {
-    if (typeof event === 'string' && this._eventsNamespace !== undefined) {
-      return getNamespacedEvent({
-        namespace: this._eventsNamespace,
-        event,
-      })
+    if (typeof event === 'string') {
+      /**
+       * Add event prefix like `video.` or `chat.`
+       */
+      event = `${this._eventsPrefix}.${event}`
+
+      if (this._eventsNamespace !== undefined) {
+        return getNamespacedEvent({
+          namespace: this._eventsNamespace,
+          event,
+        })
+      }
     }
 
     return event
