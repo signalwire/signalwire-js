cc51263398d7dbb6d8337e5a12bbc091ddcb79b2
Francisco Ramini <framini@gmail.com>
2021-08-04 12:19:26 +0200

    add logic for passing the Room instance to the handler

--- packages/node/src/Client.ts
+++ packages/node/src/Client.ts
@@ -4,14 +4,41 @@ import {
   logger,
   SessionState,
   GlobalVideoEvents,
+  connect,
 } from '@signalwire/core'
-
+import { Room } from './Room'
 interface Consumer {
   subscribe: (event: GlobalVideoEvents, handler: any) => void
   run: () => Promise<unknown>
 }
 export class Client extends BaseClient {
   private _consumers: Consumer[] = []
+
+  protected _emitterTransforms = new Map<any, any>([
+    [
+      'room.started',
+      (handler: any) => (payload: any) => {
+        const room: Room = connect({
+          store: this.store,
+          Component: Room,
+          componentListeners: {
+            errors: 'onError',
+            responses: 'onSuccess',
+          },
+        })({
+          name: payload.room.name,
+          roomId: payload.room.room_id,
+          roomSessionId: payload.room.room_session_id,
+          // FIXME: this `event_channel` is not coming in `payload` yet.
+          eventChannel: payload.room.event_channel,
+          store: this.store,
+          emitter: this.options.emitter,
+        })
+
+        return handler(room)
+      },
+    ],
+  ])
 
   async onAuth(session: SessionState) {
     if (session.authStatus === 'authorized' && session.authCount > 1) {
