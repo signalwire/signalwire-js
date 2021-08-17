91bd834552fdf16cbbf8d328a095c6b07e559916
Francisco Ramini <framini@gmail.com>
2021-08-17 12:31:48 +0200

    update BaseConsumer to reuse eventNames and originalEvents from BaseComponent

--- packages/node/src/BaseConsumer.ts
+++ packages/node/src/BaseConsumer.ts
@@ -1,7 +1,6 @@
 import {
   BaseComponent,
   BaseComponentOptions,
-  Emitter,
   ExecuteParams,
   logger,
 } from '@signalwire/core'
@@ -13,8 +12,7 @@ interface BaseConsumerOptions extends BaseComponentOptions {
   eventChannel: string
 }
 
-export class BaseConsumer<T extends string = string> extends BaseComponent {
-  private _subscriptions: T[] = []
+export class BaseConsumer extends BaseComponent {
   protected _namespace: string
   subscribeParams?: Record<string, any> = {}
 
@@ -31,36 +29,25 @@ export class BaseConsumer<T extends string = string> extends BaseComponent {
     return this.options.eventChannel
   }
 
-  private setSubscription(event: T) {
-    this._subscriptions = Array.from(new Set(this._subscriptions.concat(event)))
-    return this._subscriptions
-  }
-
-  on(...options: Parameters<Emitter['on']>) {
-    this.setSubscription(options[0] as T)
-    return super.on(...options)
-  }
-
-  once(...options: Parameters<Emitter['once']>) {
-    this.setSubscription(options[0] as T)
-    return super.once(...options)
+  protected getSubscriptions(): (string | symbol)[] {
+    throw new Error('getSubscriptions is not implemented')
   }
 
   run() {
     return new Promise(async (resolve, reject) => {
-      if (this._subscriptions.length > 0) {
+      const subscriptions = this.getSubscriptions()
+      if (subscriptions.length > 0) {
         const execParams: ExecuteParams = {
           method: 'signalwire.subscribe',
           params: {
             ...this.subscribeParams,
             event_channel: this.eventChannel,
-            events: this._subscriptions,
+            events: subscriptions,
           },
         }
 
         try {
           await this.execute(execParams)
-          this._subscriptions = []
         } catch (error) {
           return reject(error)
         }
