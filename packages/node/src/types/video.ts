1b2020370b8a6f9a93cce785d971c298f7e0a10e
Francisco Ramini <framini@gmail.com>
2021-08-19 12:43:04 +0200

    add type structure for typing individual event handlers

--- packages/node/src/types/video.ts
+++ packages/node/src/types/video.ts
@@ -1,13 +1,49 @@
-import type { GlobalVideoEvents } from '@signalwire/core'
+import StrictEventEmitter from 'strict-event-emitter-types'
+import type {
+  GlobalVideoEvents,
+  RoomEventNames,
+  LayoutEvent,
+  MemberJoinedEventName,
+  MemberLeftEventName,
+  MemberUpdatedEventName,
+  MemberUpdatedEventNames,
+  MemberTalkingEventNames,
+  RoomEvent,
+} from '@signalwire/core'
 import { Room } from '../Room'
 
-type RealTimeVideoApiGlobalEvents = GlobalVideoEvents
+// `Video` namespace related typings
+export type RealTimeVideoApiGlobalEvents = GlobalVideoEvents
 
-export type EventsHandlerMapping = Record<
+export type RealTimeVideoApiEventsHandlerMapping = Record<
   RealTimeVideoApiGlobalEvents,
-  (room: Room) => void
+  (room: StrictEventEmitter<Room, RealTimeRoomApiEvents>) => void
 >
 
 export type RealTimeVideoApiEvents = {
-  [k in RealTimeVideoApiGlobalEvents]: EventsHandlerMapping[k]
+  [k in RealTimeVideoApiGlobalEvents]: RealTimeVideoApiEventsHandlerMapping[k]
+}
+
+// `Room` related typings
+export type RealTimeRoomApiEventNames = Exclude<RoomEventNames, 'track'>
+
+// TODO: replace `any` with proper types.
+export type RealTimeRoomApiEventsHandlerMapping = Record<
+  LayoutEvent,
+  (layout: any) => void
+> &
+  Record<MemberJoinedEventName, (member: any) => void> &
+  Record<MemberLeftEventName, (member: any) => void> &
+  Record<
+    MemberUpdatedEventName | MemberUpdatedEventNames,
+    (member: any) => void
+  > &
+  Record<MemberTalkingEventNames, (member: any) => void> &
+  Record<
+    RoomEvent,
+    (room: StrictEventEmitter<Room, RealTimeRoomApiEvents>) => void
+  >
+
+export type RealTimeRoomApiEvents = {
+  [k in RealTimeRoomApiEventNames]: RealTimeRoomApiEventsHandlerMapping[k]
 }
