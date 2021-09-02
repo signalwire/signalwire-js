6fac1c51943dd261c555cae83cdaf53ab1ffbd14
Edoardo Gallo <edo91.gallo@gmail.com>
2021-09-02 13:40:00 +0200

    add fixme to rework applyEmitterTransforms

--- packages/webrtc/src/BaseConnection.ts
+++ packages/webrtc/src/BaseConnection.ts
@@ -283,6 +283,7 @@ export class BaseConnection
     } else {
       this._attachListeners(component.roomSessionId)
     }
+    // FIXME: Move to a better place when rework _attachListeners too.
     this.applyEmitterTransforms()
   }
 
