ab6bd2bab5c9c828f59ad63ea10239b40d441242
Edoardo Gallo <edo91.gallo@gmail.com>
2021-09-06 10:52:26 +0200

    update BaseComponent tests

--- packages/core/src/BaseComponent.test.ts
+++ packages/core/src/BaseComponent.test.ts
@@ -4,23 +4,29 @@ import { EventEmitter } from './utils/EventEmitter'
 
 describe('BaseComponent', () => {
   describe('as an event emitter', () => {
+    class JestComponent extends BaseComponent {
+      protected _eventsPrefix = 'video' as const
+
+      constructor(namespace = '') {
+        super({
+          store: configureJestStore(),
+          emitter: new EventEmitter(),
+        })
+
+        this._attachListeners(namespace)
+      }
+    }
+
     let instance: BaseComponent
-    const serverEvent = 'jest.event.server_side'
 
     beforeEach(() => {
-      instance = new BaseComponent({
-        store: configureJestStore(),
-        emitter: new EventEmitter(),
-      })
-      // @ts-expect-error
-      instance._eventsPrefix = 'jest'
-      // @ts-expect-error
-      instance._attachListeners('')
+      instance = new JestComponent()
     })
 
     it('should handle snake_case events', (done) => {
+      const serverEvent = 'video.event.server_side'
       const payload = { test: 1 }
-      instance.on(serverEvent, (data) => {
+      instance.on('event.server_side', (data) => {
         expect(data).toStrictEqual(payload)
 
         done()
@@ -30,6 +36,7 @@ describe('BaseComponent', () => {
     })
 
     it('should handle camelCase events', (done) => {
+      const serverEvent = 'video.event.server_side'
       const payload = { test: 1 }
       instance.on('event.serverSide', (data) => {
         expect(data).toStrictEqual(payload)
