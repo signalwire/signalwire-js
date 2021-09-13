44d21fa10ef70fa4d3d022de59077a37e360f83d
Edoardo Gallo <edo91.gallo@gmail.com>
2021-09-13 18:28:17 +0200

    update realtime-api example

--- packages/realtime-api/examples/ts-vanilla-websockets/index.ts
+++ packages/realtime-api/examples/ts-vanilla-websockets/index.ts
@@ -9,39 +9,58 @@ async function run() {
     })
 
     client.video.on('room.started', async (room) => {
-      room.on('member.talking.started', (member) => {
-        console.log('---> member.talking.started', member)
-      })
-      room.on('member.talking.ended', (member) => {
-        console.log('---> member.talking.ended', member)
-      })
+      // room.on('member.talking.started', (member) => {
+      //   console.log('---> member.talking.started', member)
+      // })
+      // room.on('member.talking.ended', (member) => {
+      //   console.log('---> member.talking.ended', member)
+      // })
 
       room.on('member.joined', async (member) => {
-        console.log('---> member.joined', member)
+        console.log('---> member.joined', member.id, member.name)
         await member.videoMute()
       })
 
+      room.on('member.left', async (member) => {
+        console.log('---> member.left', member.id, member.name)
+      })
+
       room.on('room.updated', async (room) => {
-        console.log('---> room.updated', room)
+        // @ts-expect-error
+        console.log('---> room.updated', room.id, room.name, room.updated)
       })
 
       room.on('layout.changed', async (layout) => {
-        console.log('---> layout.changed', layout)
+        console.log('---> layout.changed', layout, layout.name)
       })
 
       room.on('member.updated', (member) => {
-        // @ts-ignore
-        console.log('---> member.updated', member.updated)
+        console.log(
+          '---> member.updated',
+          member.id,
+          member.name,
+          // @ts-expect-error
+          member.updated
+        )
       })
 
+      // @ts-expect-error
+      room.on('member.updated.audio_muted', (member) => {
+        console.log('---> audio_muted', member.id, member.audioMuted)
+      })
       room.on('member.updated.audioMuted', (member) => {
-        console.log('---> AUDIO MUTED', member)
+        console.log('---> audioMuted', member.id, member.audioMuted)
+      })
+
+      // @ts-expect-error
+      room.on('member.updated.video_muted', (member) => {
+        console.log('---> video_muted', member.id, member.videoMuted)
       })
       room.on('member.updated.videoMuted', (member) => {
-        console.log('---> VIDEO MUTED', member)
+        console.log('---> videoMuted', member.id, member.videoMuted)
       })
       room.on('member.updated.visible', (member) => {
-        console.log('---> VISIBLE', member.visible)
+        console.log('---> visible', member.id, member.visible)
       })
 
       await room.run()
@@ -50,7 +69,7 @@ async function run() {
     })
 
     client.video.on('room.ended', (room) => {
-      console.log('🔴 ROOOM ENDED 🔴', room)
+      console.log('🔴 ROOOM ENDED 🔴', room, room.name)
     })
 
     await client.connect()
