f98e2e21e4c3962fa8a26db0e7a9d37bffc7d90c
Francisco Ramini <framini@gmail.com>
2021-09-17 09:47:24 +0200

    add some tests to validate new logic

--- packages/core/src/utils/toInternalEventName.test.ts
+++ packages/core/src/utils/toInternalEventName.test.ts
@@ -1,6 +1,18 @@
+import { EVENT_NAMESPACE_DIVIDER } from './constants'
 import { toInternalEventName } from './toInternalEventName'
 
 describe('toInternalEventName', () => {
+  it('should use the EVENT_NAMESPACE_DIVIDER constant for namespacing the event', () => {
+    const internalEvent = toInternalEventName({
+      event: 'video.room.started',
+      namespace: '1234',
+    })
+
+    expect(internalEvent).toEqual(
+      `1234${EVENT_NAMESPACE_DIVIDER}video.room.started`
+    )
+  })
+
   it('should transform public events into internal without namespace', () => {
     const events = [
       // no-op
@@ -27,6 +39,35 @@ describe('toInternalEventName', () => {
       expect(toInternalEventName({ event: input, namespace })).toEqual(
         `${namespace}:${output}`
       )
+    })
+  })
+
+  it('should not add the namespace to events that are already namespaced', () => {
+    const events = [
+      {
+        input: 'some-namespace:video.room.started',
+        output: 'some-namespace:video.room.started',
+        namespace: 'some-namespace',
+      },
+      {
+        input: '111-222-333:video.member.updated',
+        output: '111-222-333:video.member.updated',
+        namespace: '111-222-333',
+      },
+      {
+        input: 'xxxxxx:video.member.updated.video_muted',
+        output: 'xxxxxx:video.member.updated.video_muted',
+        namespace: 'xxxxxx',
+      },
+      {
+        input: 'y_y:video.other_snake_case',
+        output: 'y_y:video.other_snake_case',
+        namespace: 'y_y',
+      },
+    ]
+
+    events.forEach(({ input, output, namespace }) => {
+      expect(toInternalEventName({ event: input, namespace })).toEqual(output)
     })
   })
 })
