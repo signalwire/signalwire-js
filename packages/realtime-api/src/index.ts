319afe6279955f847ffbd5ff9b98b7f027f92997
Daniel Di Sarli <danieleds0@gmail.com>
2021-09-22 16:06:16 +0200

    main example should be runnable in plain javascript (#303)
    * main example should be runnable in plain javascript
    
    * add changeset

--- packages/realtime-api/src/index.ts
+++ packages/realtime-api/src/index.ts
@@ -5,25 +5,25 @@
  * To get started, create a realtime client with {@link createClient} and listen
  * for events. For example:
  *
- * ```typescript
- * import { createClient } from '@signalwire/realtime-api'
- *
- * const client = await createClient({
+ * ```javascript
+ * const { createClient } = require('@signalwire/realtime-api')
+ * 
+ * createClient({
  *   project: '<project-id>',
  *   token: '<project-token>'
- * })
- *
- * client.video.on('room.started', async (roomSession) => {
- *   console.log("Room started")
- *
- *   roomSession.on('member.joined', async (member) => {
- *     await member.videoMute()
- *   })
- *
- *   await roomSession.subscribe()
- * }
- *
- * await client.connect()
+ * }).then((client) => {
+ *   client.video.on('room.started', async (roomSession) => {
+ *     console.log("Room started")
+ *   
+ *     roomSession.on('member.joined', async (member) => {
+ *       console.log(member)
+ *     })
+ *   
+ *     await roomSession.subscribe()
+ *   });
+ * 
+ *   client.connect()
+ * });
  * ```
  *
  * @module
