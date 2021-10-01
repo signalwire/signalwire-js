7f5f126ae1f7c9d3e84e2816f5ac7258ef6f282a
Edoardo Gallo <edo91.gallo@gmail.com>
2021-10-01 13:00:37 +0200

    add comment on _instanceByTransformKey

--- packages/core/src/utils/eventTransformUtils.ts
+++ packages/core/src/utils/eventTransformUtils.ts
@@ -19,7 +19,13 @@ interface NestedFieldToProcess {
   eventTransformType: EventTransformType
 }
 
-/** Exported for test purposes */
+/**
+ * Note: the cached instances within `_instanceByTransformKey` will never be
+ * cleaned since we're caching by `transform.key` so we will always have one
+ * instance per type regardless of the Room/Member/Recording we're working on.
+ * This is something we can improve in the future, but not an issue right now.
+ * Exported for test purposes
+ */
 export const _instanceByTransformKey = new Map<string, EventTransform>()
 export const NESTED_FIELDS_TO_PROCESS: NestedFieldToProcess[] = [
   {
