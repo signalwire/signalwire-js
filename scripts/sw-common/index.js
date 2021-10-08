fc32c4717f1287b47e63c0835fc74f9ad882f070
Francisco Ramini <framini@gmail.com>
2021-10-08 11:39:23 +0200

    Publishing script (#324)
    * add package for handling common utils
    
    * add option to filter unreleased packages
    
    * wip for sw-release cli
    
    * group individual tasks into subtasks
    
    * add util for getting last sha, normalize names
    
    * add script for releasing dev
    
    * add dev tasks
    
    * improve progress logic
    
    * change wording
    
    * change wording
    
    * rename util, add empty test script
    
    * add dev tag
    
    * make us of private field for filtering
    
    * remove dry run

---
+++ scripts/sw-common/index.js
@@ -0,0 +1,68 @@
+import path from 'node:path'
+import fs from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import execa from 'execa'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const TARGET_DEPTH_LEVEL = 1
+const _scan = (pathname, level = 0, acc = []) => {
+  if (level > TARGET_DEPTH_LEVEL) {
+    return
+  }
+
+  const list = fs.readdirSync(pathname)
+
+  list.forEach(function (item) {
+    if (!fs.lstatSync(pathname + '/' + item).isDirectory()) {
+      if (item !== 'package.json') {
+        return
+      }
+
+      const pkgJson = JSON.parse(
+        fs.readFileSync(path.resolve(pathname, 'package.json'), 'utf-8')
+      )
+
+      if (pkgJson.private) {
+        return
+      }
+
+      acc.push({
+        name: pkgJson.name,
+        pathname,
+      })
+    } else {
+      return _scan(path.join(pathname, item), level + 1, acc)
+    }
+  })
+
+  return acc
+}
+
+const PACKAGES_PATH = path.join(__dirname, '../../packages')
+const DEFAULT_OPTIONS = {
+  pathname: PACKAGES_PATH,
+}
+
+const getPackages = ({ pathname = PACKAGES_PATH } = DEFAULT_OPTIONS) => {
+  const pkgDeps = _scan(pathname)
+
+  return pkgDeps
+}
+
+const BUILD_MODES = ['--development', '--production']
+const isModeFlag = (flag) => {
+  return BUILD_MODES.includes(flag)
+}
+const getModeFlag = (flags = []) => {
+  return flags.find((f) => isModeFlag(f))
+}
+
+const getLastGitSha = async (length = 7) => {
+  const { stdout: sha } = await execa('git', ['log', '-1', '--format=%H'])
+
+  return sha.substring(0, length)
+}
+
+export { getPackages, getModeFlag, getLastGitSha }
