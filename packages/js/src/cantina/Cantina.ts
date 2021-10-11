664f914acdfb56ba355f9680cb3eaa3d46e8a2e3
Aye Min Aung <ayemin@signalwire.com>
2021-10-11 18:34:07 +0700

    remove unused import

--- packages/js/src/cantina/Cantina.ts
+++ packages/js/src/cantina/Cantina.ts
@@ -1,5 +1,4 @@
 import { BaseComponentOptions, BaseConsumer, connect, ConsumerContract, EventTransform, InternalUserOptions, toExternalJSON } from '@signalwire/core'
-import { SDKStore } from '@signalwire/core/src/redux'
 import { createBaseRoomSessionObject } from '../BaseRoomSession';
 import { RoomSession } from "../RoomSession";
 
