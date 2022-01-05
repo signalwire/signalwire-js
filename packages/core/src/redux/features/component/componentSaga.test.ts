a2134864219f96732dac48921df3ca3ca95ad14e
Francisco Ramini <framini@gmail.com>
2022-01-05 13:17:48 +0100

    update saga test

--- packages/core/src/redux/features/component/componentSaga.test.ts
+++ packages/core/src/redux/features/component/componentSaga.test.ts
@@ -1,13 +1,10 @@
 import { Store } from 'redux'
-import { configureJestStore } from '../../../testUtils'
-import {
-  componentCleanupSaga,
-  componentCleanupSagaWorker,
-} from './componentSaga'
 import { expectSaga } from 'redux-saga-test-plan'
+import { configureJestStore } from '../../../testUtils'
+import { componentCleanupSagaWorker } from './componentSaga'
 import { rootReducer } from '../../rootReducer'
 
-describe('componentSelectors', () => {
+describe('componentCleanupSaga', () => {
   let store: Store
 
   beforeEach(() => {
@@ -31,30 +28,26 @@ describe('componentSelectors', () => {
       },
     })
   })
-
-  describe('componentCleanupSaga', () => {
-    it('should cleanup unused components from the store', () => {
-      return expectSaga(componentCleanupSagaWorker)
-        .withReducer(rootReducer, store.getState())
-        // .delay(3000)
-        .hasFinalState({
-          components: {
-            byId: {
-              '268b4cf8-a3c5-4003-8666-3b7a4f0a5af9': {
-                id: '268b4cf8-a3c5-4003-8666-3b7a4f0a5af9',
-              },
+  it('should cleanup unused components from the store', () => {
+    return expectSaga(componentCleanupSagaWorker)
+      .withReducer(rootReducer, store.getState())
+      .hasFinalState({
+        components: {
+          byId: {
+            '268b4cf8-a3c5-4003-8666-3b7a4f0a5af9': {
+              id: '268b4cf8-a3c5-4003-8666-3b7a4f0a5af9',
             },
           },
-          session: {
-            protocol: '',
-            iceServers: [],
-            authStatus: 'unknown',
-            authError: undefined,
-            authCount: 0,
-          },
-          executeQueue: { queue: [] },
-        })
-        .run()
-    })
+        },
+        session: {
+          protocol: '',
+          iceServers: [],
+          authStatus: 'unknown',
+          authError: undefined,
+          authCount: 0,
+        },
+        executeQueue: { queue: [] },
+      })
+      .run()
   })
 })
