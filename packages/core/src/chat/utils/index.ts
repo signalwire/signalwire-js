6fb9c17e9947c9cb9588ba6d29efe8641c02622c
Edoardo Gallo <edo91.gallo@gmail.com>
2022-01-06 10:29:11 +0100

    not accept undefined in isValidChannels guard

--- packages/core/src/chat/utils/index.ts
+++ packages/core/src/chat/utils/index.ts
@@ -1,9 +1,5 @@
 export * from './toInternalChatChannels'
 
-export const isValidChannels = (
-  input: unknown
-): input is string | string[] | undefined => {
-  return (
-    input === undefined || Array.isArray(input) || typeof input === 'string'
-  )
+export const isValidChannels = (input: unknown): input is string | string[] => {
+  return Array.isArray(input) || typeof input === 'string'
 }
