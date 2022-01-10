04946de8dfe96bd32b1ca672a660f6f7ff9c907e
Francisco Ramini <framini@gmail.com>
2022-01-10 10:58:26 +0100

    move MemberPosition to types

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -121,6 +121,13 @@ export type PubSubChannelEvents =
   | InternalVideoEventNames
   | SessionEvents
 
+export type MemberPosition =
+  | 'reserved'
+  | `reserved-${number}`
+  | 'standard'
+  | `standard-${number}`
+  | 'off-canvas'
+
 export * from './video'
 export * from './utils'
 export * from './cantina'
