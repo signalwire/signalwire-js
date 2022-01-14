86399b0ae5bfcda81f5b117c8752b19eb7857af3
Daniele Di Sarli <danieleds0@gmail.com>
2022-01-14 14:30:05 +0100

    additional methods

--- packages/js/src/chat/Client.docs.ts
+++ packages/js/src/chat/Client.docs.ts
@@ -49,9 +49,37 @@ export interface ClientDocs extends
    */
   unsubscribe(channels: string | string[]): Promise<any>
 
+  /**
+   * Publish a message into the specified channel.
+   *
+   * @example Publishing a message as a string:
+   * ```js
+   * await chatClient.publish({
+   *   channel: 'my-channel',
+   *   message: 'Hello, world.'
+   * })
+   * ```
+   * 
+   * @example Publishing a message as an object:
+   * ```js
+   * await chatClient.publish({
+   *   channel: 'my-channel',
+   *   message: {
+   *     field_one: 'value_one',
+   *     field_two: 'value_two',
+   *   }
+   * })
+   * ```
+   */
   publish(params: {
+    /** The message to send. This can be any JSON-serializable object. */
     message: any
+    /** Channel in which to send the message. */
     channel: string
+    /**
+     * Metadata associated with the message. There are no requirements on the
+     * content of metadata.
+     */
     meta?: Record<any, any>
   }): Promise<any>
 
@@ -63,15 +91,64 @@ export interface ClientDocs extends
     }
   }): Promise<any>
 
-  getMembers(params: { channel: string }): Promise<any>
+  /**
+   * Returns the list of members in the given channel.
+   * 
+   * @example
+   * ```js
+   * const m = await chatClient.getMembers({ channel: 'my-channel' })
+   * 
+   * m.members.length;  // 7
+   * m.members[0];  // { id: ..., channel: ..., state: ... }
+   * ```
+   */
+  getMembers(params: {
+    /** The channel for which to get the list of members. */
+    channel: string
+  }): Promise<any>
 
+  /**
+   * Sets a state object for the current member, for the specified channels. The
+   * previous state object will be completely replaced.
+   *
+   * @example
+   * ```js
+   * await chatClient.setMemberState({
+   *   channels: ['chan1', 'chan2'],
+   *   state: {
+   *     online: true,
+   *     typing: false
+   *   }
+   * })
+   * ```
+   */
   setMemberState(params: {
+    /** Channels for which to set the state. */
     channels: string | string[]
+    /**
+     * The state to set. There are no requirements on the content of the state.
+     */
     state: Record<any, any>
   }): Promise<any>
 
+  /**
+   * Returns the states of a member in the specified channels.
+   * 
+   * @example
+   * ```js
+   * const s = await chatClient.getMemberState({
+   *   channels: ['chan1', 'chan2'],
+   *   memberId: 'my-member-id'
+   * })
+   * 
+   * s.channels.length;  // 2
+   * s.channels.chan1.state;  // the state object for chan1
+   * ```
+   */
   getMemberState(params: {
+    /** Channels for which to get the state. */
     channels: string | string[]
+    /** Id of the member for which to get the state. */
     memberId: string
   }): Promise<any>
 
