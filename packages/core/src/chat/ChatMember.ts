14e4f56a507666ee94f026728878be512b32ebed
Daniele Di Sarli <danieleds0@gmail.com>
2022-01-18 15:38:24 +0100

    docs for ChatMember and ChatMessage

--- packages/core/src/chat/ChatMember.ts
+++ packages/core/src/chat/ChatMember.ts
@@ -1,16 +1,22 @@
 import { ChatMemberContract } from '..'
 
+/**
+ * Represents a member in a chat.
+ */
 export class ChatMember implements ChatMemberContract {
   constructor(private payload: ChatMemberContract) {}
 
+  /** The id of this member */
   get id(): string {
     return this.payload.id
   }
 
+  /** The channel of this member */
   get channel(): string {
     return this.payload.channel
   }
 
+  /** The state of this member */
   get state(): any {
     return this.payload.state ?? {}
   }
