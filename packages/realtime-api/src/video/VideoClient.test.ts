5e51643af1afe1e888716b5fd598c18563878860
Francisco Ramini <framini@gmail.com>
2022-01-24 12:26:06 +0100

    add test for subscribe error

--- packages/realtime-api/src/video/VideoClient.test.ts
+++ packages/realtime-api/src/video/VideoClient.test.ts
@@ -109,7 +109,7 @@ describe('VideoClient', () => {
         })
       })
 
-      it('should automatically call subscribe the moment the client is connected', async ()=> {
+      it('should automatically call subscribe the moment the client is connected', async () => {
         const video = new Client({
           // @ts-expect-error
           host,
@@ -120,9 +120,59 @@ describe('VideoClient', () => {
         video.on('room.started', () => {})
 
         // Artificial timer to wait for the connect() to happen
-        await new Promise(r => setTimeout(r, 2000))
+        await new Promise((r) => setTimeout(r, 1000))
 
         expect(mockedVideo.subscribe).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    describe('Subscribe error', () => {
+      let createVideoObjectMock: jest.SpyInstance
+      const mockedVideo: any = {
+        subscribe: jest.fn(() => Promise.reject()),
+        on: jest.fn(),
+      }
+      beforeEach(async () => {
+        createVideoObjectMock = jest
+          .spyOn(Video, 'createVideoObject')
+          .mockImplementationOnce(() => {
+            return mockedVideo
+          })
+      })
+      afterEach(() => {
+        createVideoObjectMock.mockRestore()
+        Object.values(mockedVideo).forEach((mock: any) => {
+          if (typeof mock.mockRestore === 'function') {
+            mock.mockRestore()
+          }
+        })
+      })
+
+      it('should show an error message if the call to subscribe fails', async () => {
+        const logger = {
+          error: jest.fn(),
+          trace: jest.fn(),
+          debug: jest.fn(),
+          warn: jest.fn(),
+        }
+        const video = new Client({
+          // @ts-expect-error
+          host,
+          project: 'some-project-t',
+          token,
+          logger: logger as any,
+        })
+
+        try {
+          // It's not neccessary to call `subcribe()`
+          // manually. We're doing it here to avoid creating
+          // a timer for the `connect()`
+          await video.subscribe()
+        } catch (e) {
+          expect(logger.error).toHaveBeenCalledWith(
+            'Client subscription failed.'
+          )
+        }
       })
     })
 
@@ -131,19 +181,23 @@ describe('VideoClient', () => {
         error: jest.fn(),
         trace: jest.fn(),
         debug: jest.fn(),
-        warn: jest.fn()
+        warn: jest.fn(),
       }
       const video = new Client({
         // @ts-expect-error
         host,
         project: 'some-project',
         token: '<invalid-token>',
-        logger: logger as any
+        logger: logger as any,
       })
 
       await video.subscribe()
 
-      expect(logger.error).toHaveBeenNthCalledWith(1, "Auth Error", {"code": -32002, "message": "Authentication service failed with status ProtocolError, 401 Unauthorized: {}"})
+      expect(logger.error).toHaveBeenNthCalledWith(1, 'Auth Error', {
+        code: -32002,
+        message:
+          'Authentication service failed with status ProtocolError, 401 Unauthorized: {}',
+      })
     })
   })
 })
