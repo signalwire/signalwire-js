09680288e1e1a10915206f062deb2a4de235cab4
Francisco Ramini <framini@gmail.com>
2022-01-26 10:03:29 +0100

    add tests for reading from env vars

--- packages/realtime-api/src/utils/internals.test.ts
+++ packages/realtime-api/src/utils/internals.test.ts
@@ -14,4 +14,38 @@ describe('getCredentials', () => {
     ).toThrow('Missing `token`')
     expect(() => getCredentials({})).toThrow()
   })
+
+  describe('Environment variables', () => {
+    const processEnv = process.env
+    beforeEach(() => {
+      jest.resetModules()
+      process.env = { ...processEnv }
+    })
+    afterAll(() => {
+      process.env = processEnv
+    })
+
+    it('should read from environment variables when the user doesnt provide token and/or project', () => {
+      process.env.SW_PROJECT = 'env-project'
+      process.env.SW_TOKEN = 'env-token'
+
+      expect(getCredentials({})).toEqual({
+        project: 'env-project',
+        token: 'env-token',
+      })
+    })
+
+    it('should give priority to user params over env variables', () => {
+      process.env.SW_PROJECT = 'env-project'
+      process.env.SW_TOKEN = 'env-token'
+
+      expect(getCredentials({
+        project: 'a-project',
+        token: 'a-token'
+      })).toEqual({
+        project: 'a-project',
+        token: 'a-token',
+      })
+    })
+  })
 })
