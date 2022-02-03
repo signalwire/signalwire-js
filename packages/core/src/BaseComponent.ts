f89348ab7a67b53c469a825e55b1c30a1011c16e
Edoardo Gallo <edo91.gallo@gmail.com>
2022-02-03 11:35:05 +0100

    fix BaseComponent type

--- packages/core/src/BaseComponent.ts
+++ packages/core/src/BaseComponent.ts
@@ -547,13 +547,17 @@ export class BaseComponent<
   }
 
   /** @internal */
-  execute<InputType = unknown, OutputType = unknown>(
+  execute<
+    InputType = unknown,
+    OutputType = unknown,
+    ParamsType = Record<string, any>
+  >(
     { method, params }: ExecuteParams,
     {
       transformParams = identity,
       transformResolve = identity,
       transformReject = identity,
-    }: ExecuteExtendedOptions<InputType, OutputType> = {
+    }: ExecuteExtendedOptions<InputType, OutputType, ParamsType> = {
       transformParams: identity,
       transformResolve: identity,
       transformReject: identity,
@@ -573,7 +577,8 @@ export class BaseComponent<
           requestId,
           componentId: this.__uuid,
           method,
-          params: transformParams(params),
+          // FIXME:
+          params: transformParams(params as ParamsType),
         })
       )
     })
