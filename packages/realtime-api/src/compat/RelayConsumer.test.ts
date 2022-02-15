80ef3584a670d785b92469446a0856758f7e33ad
Edoardo Gallo <edo91.gallo@gmail.com>
2022-02-15 11:45:26 +0100

    review tests

--- packages/realtime-api/src/compat/RelayConsumer.test.ts
+++ packages/realtime-api/src/compat/RelayConsumer.test.ts
@@ -1,63 +1,17 @@
 import WS from 'jest-websocket-mock'
-// import { actions } from '@signalwire/core'
-// import { configureFullStack } from '../testUtils'
 import { RelayConsumer } from './RelayConsumer'
+import LegacyCall from '@signalwire/node/dist/common/src/relay/calling/Call'
 
 describe('RoomSession Object', () => {
   const host = 'ws://localhost:1234'
   let server: WS
   let consumer: RelayConsumer
 
-  // const { store, session, emitter } = configureFullStack()
-
-  const dispatchCallingCallReceive = () => {
-    const swEvent = JSON.stringify({
-      jsonrpc: '2.0',
-      id: '75673c0c-afa3-446f-9a74-1390ddbcb48d',
-      method: 'signalwire.event',
-      params: {
-        event_type: 'calling.call.receive',
-        timestamp: 1644582753.297892,
-        project_id: '4b7ae78a-d02e-4889-a63b-08b156d5916e',
-        space_id: '62615f44-2a34-4235-b38b-76b5a1de6ef8',
-        params: {
-          call_state: 'created',
-          context: 'office',
-          device: {
-            type: 'phone',
-            params: { from_number: '+15183601338', to_number: '+12062371092' },
-          },
-          direction: 'inbound',
-          call_id: '08219786-e1a4-48b1-bda0-e3602c8fc84c',
-          node_id: 'b650f7ba-249d-4a8f-907b-d6e71eef8557@us-east',
-          segment_id: '08219786-e1a4-48b1-bda0-e3602c8fc84c',
-        },
-        context: 'office',
-      },
-    })
-    // session.dispatch(actions.socketMessageAction(swEvent))
-    server.send(swEvent)
-  }
-
   beforeEach(async () => {
     server = new WS(host)
     server.on('connection', (socket: any) => {
       socket.on('message', (data: any) => {
         const parsedData = JSON.parse(data)
-
-        // if (
-        //   parsedData.method === 'signalwire.connect' &&
-        //   parsedData.params.authentication.token === '<invalid-token>'
-        // ) {
-        //   socket.send(
-        //     JSON.stringify({
-        //       jsonrpc: '2.0',
-        //       id: parsedData.id,
-        //       error: authError,
-        //     })
-        //   )
-        // }
-
         socket.send(
           JSON.stringify({
             jsonrpc: '2.0',
@@ -74,21 +28,51 @@ describe('RoomSession Object', () => {
     consumer.client.disconnect()
   })
 
-  it('should receive an inbound call on onIncomingCall callback', async () => {
-    const mock = jest.fn()
+  it('should receive an inbound call on onIncomingCall callback', (done) => {
+    const callId = '08219786-e1a4-48b1-bda0-e3602c8fc84c'
+    const swEvent = JSON.stringify({
+      jsonrpc: '2.0',
+      id: '75673c0c-afa3-446f-9a74-1390ddbcb48d',
+      method: 'signalwire.event',
+      params: {
+        event_type: 'calling.call.receive',
+        timestamp: 1644582753.297892,
+        project_id: 'project_id',
+        space_id: 'space_id',
+        params: {
+          call_state: 'created',
+          context: 'office',
+          device: {
+            type: 'phone',
+            params: {
+              from_number: '+12222222222',
+              to_number: '+10000000000',
+            },
+          },
+          direction: 'inbound',
+          call_id: callId,
+          node_id: 'b650f7ba-249d-4a8f-907b-d6e71eef8557@us-east',
+          segment_id: '08219786-e1a4-48b1-bda0-e3602c8fc84c',
+        },
+        context: 'office',
+      },
+    })
 
     consumer = new RelayConsumer({
       host,
       project: 'project',
       token: 'token',
       contexts: ['office'],
-      onIncomingCall: mock,
+      onIncomingCall: (relayCall: LegacyCall) => {
+        expect(relayCall).toBeInstanceOf(LegacyCall)
+        expect(relayCall.id).toBe(callId)
+
+        done()
+      },
     })
-    await consumer.run()
-    await server.connected
 
-    dispatchCallingCallReceive()
-
-    expect(mock).toHaveBeenCalledTimes(1)
+    consumer.run().then(() => {
+      server.send(swEvent)
+    })
   })
 })
