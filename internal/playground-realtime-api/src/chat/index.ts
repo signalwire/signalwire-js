4ce920fcd113c7f6de819ec4a5fdb96a1345cbdb
Aye Min Aung <mgdevine89@gmail.com>
2022-02-24 21:15:20 +0700

    Tweak realtime-api chat examples (#436)
    * added more tests to internal/playground-realtime-api
    
    * test for get member state added
    
    * few naming tweaks
    
    Co-authored-by: Aye Min Aung <ayemin@signalwire.com>
    Co-authored-by: Edoardo Gallo <edo91.gallo@gmail.com>

--- internal/playground-realtime-api/src/chat/index.ts
+++ internal/playground-realtime-api/src/chat/index.ts
@@ -9,11 +9,62 @@ async function run() {
       token: process.env.TOKEN as string,
     })
 
+    const channel = 'lobby'
+
     chat.on('member.joined', (member) => {
-      console.log('member --->', member)
+      console.log('member.joined', member)
     })
 
-    await chat.subscribe(['lobby'])
+    chat.on('member.updated', (member) => {
+      console.log('member.updated', member)
+    })
+
+    chat.on('member.left', (member) => {
+      console.log('member.left', member)
+    })
+
+    chat.on('message', (message) => {
+      console.log('message', message)
+    })
+
+    await chat.subscribe([channel])
+
+    const pubRes = await chat.publish({
+      content: 'Hello There',
+      channel: channel,
+      meta: {
+        fooId: 'randomValue',
+      },
+    })
+
+    console.log('Publish Result --->', pubRes)
+
+    const messagesResult = await chat.getMessages({
+      channel: channel,
+    })
+
+    console.log('Get Messages Result ---> ', messagesResult)
+
+    const setStateResult = await chat.setMemberState({
+      state: {
+        data: 'state data',
+      },
+      channels: [channel],
+      memberId: 'someMemberId',
+    })
+
+    console.log('Set Member State Result --->', setStateResult)
+
+    const getStateResult = await chat.getMemberState({
+      channels: [channel],
+      memberId: 'someMemberId',
+    })
+
+    console.log('Get Member State Result --->', getStateResult)
+
+    const unsubscribeRes = await chat.unsubscribe(channel)
+
+    console.log('Unsubscribe Result --->', unsubscribeRes)
 
     console.log('Client Running..')
   } catch (error) {
