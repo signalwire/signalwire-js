640f7011e9cb4798f623c303b487f97d99c44c42
Francisco Ramini <framini@gmail.com>
2022-03-04 12:07:44 +0100

    change how compound events are dispatched, update layoutWorker to match new api

--- packages/core/src/BaseComponent.ts
+++ packages/core/src/BaseComponent.ts
@@ -102,16 +102,17 @@ export class BaseComponent<
       return
     }
 
+    // TODO: replace with action creator.
+    this.store.dispatch({
+      type: 'compound_event:attach',
+      payload: {
+        compoundEvents,
+        event: internalEvent,
+        namespace: this._eventsNamespace
+      },
+    })
+
     compoundEvents.forEach((compoundEvent) => {
-      const internalCompoundEvent = this._getInternalEvent(compoundEvent)
-      // TODO: replace with action creator.
-      this.store.dispatch({
-        type: 'compound_event:attach',
-        payload: {
-          rawEvent: compoundEvent,
-          internalEvent: internalCompoundEvent,
-        },
-      })
       /**
        * In the future we might want to support defining
        * custom compound event handlers by specifying not
@@ -474,7 +475,7 @@ export class BaseComponent<
     event: T,
     fn: EventEmitter.EventListener<EventTypes, T>,
     once?: boolean
-    ) {
+  ) {
     this._handleCompoundEvents(event)
 
     const internalEvent = this._getInternalEvent(event)
--- packages/realtime-api/src/video/workers/layoutWorker.ts
+++ packages/realtime-api/src/video/workers/layoutWorker.ts
@@ -1,11 +1,23 @@
-import { SagaIterator, SDKWorker } from '@signalwire/core'
+import { sagaEffects, SagaIterator, SDKWorker } from '@signalwire/core'
 import { RoomSession } from '../RoomSession'
+
+function* layoutEventWorker({ compoundEvent, instance }: any) {
+  instance.on(compoundEvent, () => {
+    console.log(`--> layoutWorker: ${compoundEvent}`)
+  })
+}
 
 export const layoutWorker: SDKWorker<RoomSession> = function* layoutWorker({
   instance,
-}): SagaIterator {
-  // @ts-expect-error
-  instance._internal_on('layout.changed', (params) => {
-    console.log('layout.changed > no auto subscribe', params)
+}: any): SagaIterator {
+  const action = yield sagaEffects.take((action: any) => {
+    return (
+      action.type === 'compound_event:attach' &&
+      action.payload.namespace === instance._eventsNamespace
+    )
   })
+
+  for (const compoundEvent of action.payload.compoundEvents) {
+    yield sagaEffects.fork(layoutEventWorker, { compoundEvent, instance })
+  }
 }
