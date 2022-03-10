43e06c8e0906355236cc8b54686975e06d1b643d
Francisco Ramini <framini@gmail.com>
2022-03-10 11:04:09 +0100

    update sessionSaga tests to include the swEventChannel calls

--- packages/core/src/redux/features/session/sessionSaga.test.ts
+++ packages/core/src/redux/features/session/sessionSaga.test.ts
@@ -64,65 +64,7 @@ describe('sessionChannelWatcher', () => {
         })
         .run()
         .finally(() => {
-          expect(dispatchedActions).toHaveLength(3)
-        })
-    })
-
-    it('should handle video.member.updated dispatching the sub-events for what is changed for the user', async () => {
-      const jsonrpc = JSON.parse(
-        '{"jsonrpc":"2.0","id":"90e60fd9-a353-44dd-b179-4ce8f43b0cb1","method":"signalwire.event","params":{"params":{"room_session_id":"8e03ac25-8622-411a-95fc-f897b34ac9e7","room_id":"6e83849b-5cc2-4fc6-80ed-448113c8a426","member":{"updated":["visible","video_muted"],"room_session_id":"8e03ac25-8622-411a-95fc-f897b34ac9e7","id":"ab42641c-e784-42f1-9815-d264105bc24f","visible":true,"room_id":"6e83849b-5cc2-4fc6-80ed-448113c8a426","video_muted":false}},"timestamp":1627374437.3696,"event_type":"video.member.updated","event_channel":"room.0a324e3c-5e2f-443a-a333-10bf005f249e"}}'
-      )
-      let runSaga = true
-      const session = {
-        relayProtocol: jsonrpc.params.protocol,
-      } as any
-      const pubSubChannel = createPubSubChannel()
-      const swEventChannel = createSwEventChannel()
-      const sessionChannel = eventChannel(() => () => {})
-      const dispatchedActions: unknown[] = []
-      const payload = JSON.parse(
-        '{"member":{"updated":["visible","video_muted"],"id":"ab42641c-e784-42f1-9815-d264105bc24f","visible":true,"video_muted":false,"room_id":"6e83849b-5cc2-4fc6-80ed-448113c8a426","room_session_id":"8e03ac25-8622-411a-95fc-f897b34ac9e7"},"room_id":"6e83849b-5cc2-4fc6-80ed-448113c8a426","room_session_id":"8e03ac25-8622-411a-95fc-f897b34ac9e7"}'
-      )
-
-      return expectSaga(sessionChannelWatcher, {
-        session,
-        pubSubChannel,
-        swEventChannel,
-        sessionChannel,
-      })
-        .provide([
-          {
-            take({ channel }, next) {
-              if (runSaga && channel === sessionChannel) {
-                runSaga = false
-                return socketMessageAction(jsonrpc)
-              } else if (runSaga === false) {
-                sessionChannel.close()
-                pubSubChannel.close()
-              }
-              return next()
-            },
-            put(action, next) {
-              dispatchedActions.push(action)
-              return next()
-            },
-          },
-        ])
-        .put(pubSubChannel, {
-          type: 'video.member.updated.visible',
-          payload,
-        })
-        .put(pubSubChannel, {
-          type: 'video.member.updated.video_muted',
-          payload,
-        })
-        .put(pubSubChannel, {
-          type: 'video.member.updated',
-          payload,
-        })
-        .run()
-        .finally(() => {
-          expect(dispatchedActions).toHaveLength(3)
+          expect(dispatchedActions).toHaveLength(4)
         })
     })
 
@@ -180,7 +122,7 @@ describe('sessionChannelWatcher', () => {
         })
         .run()
         .finally(() => {
-          expect(dispatchedActions).toHaveLength(3)
+          expect(dispatchedActions).toHaveLength(4)
         })
     })
 
@@ -238,7 +180,7 @@ describe('sessionChannelWatcher', () => {
         })
         .run()
         .finally(() => {
-          expect(dispatchedActions).toHaveLength(3)
+          expect(dispatchedActions).toHaveLength(4)
         })
     })
 
@@ -288,7 +230,7 @@ describe('sessionChannelWatcher', () => {
         })
         .run()
         .finally(() => {
-          expect(dispatchedActions).toHaveLength(1)
+          expect(dispatchedActions).toHaveLength(2)
         })
     })
 
@@ -335,7 +277,7 @@ describe('sessionChannelWatcher', () => {
         })
         .run()
         .finally(() => {
-          expect(dispatchedActions).toHaveLength(1)
+          expect(dispatchedActions).toHaveLength(2)
         })
     })
 
@@ -408,7 +350,7 @@ describe('sessionChannelWatcher', () => {
         })
         .run()
         .finally(() => {
-          expect(dispatchedActions).toHaveLength(2)
+          expect(dispatchedActions).toHaveLength(3)
         })
     })
   })
@@ -470,7 +412,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(2)
+            expect(dispatchedActions).toHaveLength(3)
           })
       })
     })
@@ -530,7 +472,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(2)
+            expect(dispatchedActions).toHaveLength(3)
           })
       })
 
@@ -589,7 +531,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(2)
+            expect(dispatchedActions).toHaveLength(3)
           })
       })
     })
@@ -652,7 +594,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(2)
+            expect(dispatchedActions).toHaveLength(3)
           })
       })
     })
@@ -705,7 +647,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(1)
+            expect(dispatchedActions).toHaveLength(2)
           })
       })
     })
@@ -799,7 +741,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(1)
+            expect(dispatchedActions).toHaveLength(2)
           })
       })
 
@@ -856,7 +798,7 @@ describe('sessionChannelWatcher', () => {
           )
           .run()
           .finally(() => {
-            expect(dispatchedActions).toHaveLength(1)
+            expect(dispatchedActions).toHaveLength(2)
           })
       })
     })
