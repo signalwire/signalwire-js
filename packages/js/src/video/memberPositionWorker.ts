8154a06adce95150a1ace3c807b0d34c4f2556a0
Francisco Ramini <framini@gmail.com>
2022-03-17 06:37:51 -0300

    throw error when missing payload

--- packages/js/src/video/memberPositionWorker.ts
+++ packages/js/src/video/memberPositionWorker.ts
@@ -1,9 +1,14 @@
-import { SagaIterator, SDKWorker, MemberPosition, sagaEffects } from '@signalwire/core'
+import {
+  SagaIterator,
+  SDKWorker,
+  MemberPosition,
+  sagaEffects,
+} from '@signalwire/core'
 
 export const memberPositionWorker: SDKWorker<any> =
   function* memberPositionWorker(options): SagaIterator {
     if (!options.payload) {
-      return
+      throw new Error('[memberPositionWorker] Missing payload')
     }
 
     yield sagaEffects.fork(MemberPosition.memberPositionWorker, {
