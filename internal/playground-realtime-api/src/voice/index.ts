9a4aff8a9524efb8805fd959870d2491e4266e42
Francisco Ramini <framini@gmail.com>
2022-03-22 16:01:41 -0300

    update example

--- internal/playground-realtime-api/src/voice/index.ts
+++ internal/playground-realtime-api/src/voice/index.ts
@@ -9,7 +9,31 @@ async function run() {
       token: process.env.TOKEN as string,
     })
 
-    call.on('call.created', () => {})
+    // call.on('call.created', () => {})
+
+    try {
+      const res = await call.dial({
+        // to: '+12083660792',
+        // from: '+15183601338',
+        // tag: 'Custom client data',
+        // tag: uuid(),
+        // region: 'us',
+        devices: [
+          [
+            {
+              type: 'phone',
+              to: '+12083660792',
+              from: '+15183601338',
+              timeout: 30,
+            },
+          ],
+        ],
+      })
+      console.log('Dial resolved!', res);
+
+    } catch (e) {
+      console.log('---> E', JSON.stringify(e, null, 2))
+    }
 
     console.log('Client Running..')
   } catch (error) {
