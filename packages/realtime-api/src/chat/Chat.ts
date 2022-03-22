0d0cfa2ab0692bbdb8875e7a447d88cd4200d5fc
Daniele Di Sarli <danieleds0@gmail.com>
2022-03-22 15:10:03 +0100

    prettier

--- packages/realtime-api/src/chat/Chat.ts
+++ packages/realtime-api/src/chat/Chat.ts
@@ -2,12 +2,6 @@ import { Chat } from '@signalwire/core'
 import ChatMember = Chat.ChatMember
 import ChatMessage = Chat.ChatMessage
 
-export {
-    ChatClientApiEvents,
-    Client
-} from './ChatClient'
+export { ChatClientApiEvents, Client } from './ChatClient'
 
-export {
-  ChatMember,
-  ChatMessage,
-}
+export { ChatMember, ChatMessage }
