26fbce965432f929965c00e97ea9e4a1696d3a96
Edoardo Gallo <edo91.gallo@gmail.com>
2022-03-24 17:01:42 +0100

    add contexts

--- packages/core/src/utils/interfaces.ts
+++ packages/core/src/utils/interfaces.ts
@@ -84,7 +84,7 @@ export interface SessionOptions {
   project?: string
   /** SignalWire project token, e.g. `PT9e5660c101cd140a1c93a0197640a369cf5f16975a0079c9` */
   token: string
-  /** Relay contexts, e.g. 'home' or 'office' */
+  /** SignalWire contexts, e.g. 'home', 'office'.. */
   contexts?: string[]
   // From `LogLevelDesc` of loglevel to simplify our docs
   /** logging level */
