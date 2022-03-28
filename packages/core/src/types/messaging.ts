131cb051c3307b5b4e0920b0b8f2b3789ac4e2a6
Edoardo Gallo <edo91.gallo@gmail.com>
2022-03-28 09:43:46 +0200

    improve Messaging types

--- packages/core/src/types/messaging.ts
+++ packages/core/src/types/messaging.ts
@@ -2,7 +2,14 @@ import type { OnlyStateProperties, OnlyFunctionProperties, SwEvent } from '..'
 import type { MapToPubSubShape } from '../redux/interfaces'
 import { PRODUCT_PREFIX_MESSAGING } from '../utils/constants'
 
-// type ToInternalChatEvent<T extends string> = `${MessagingNamespace}.${T}`
+export type MessagingMessageState =
+  | 'queued'
+  | 'initiated'
+  | 'sent'
+  | 'delivered'
+  | 'undelivered'
+  | 'failed'
+
 export type MessagingNamespace = typeof PRODUCT_PREFIX_MESSAGING
 
 export type MessageReceivedEventName = 'message.received'
@@ -32,20 +39,49 @@ export type MessagingMethods = Omit<
 /**
  * 'messaging.state'
  */
-export interface MessagingStateEventParams {}
+export interface MessagingStateEventParams {
+  message_id: string
+  context: string
+  direction: 'inbound' | 'outbound'
+  tag?: string
+  tags: string[]
+  from_number: string
+  to_number: string
+  body: string
+  media?: string[]
+  segments: number
+  message_state: MessagingMessageState
+}
 
 export interface MessagingStateEvent extends SwEvent {
   event_type: MessagingState
+  context: string
+  space_id: string
+  project_id: string
   params: MessagingStateEventParams
 }
 
 /**
  * 'messaging.receive'
  */
-export interface MessagingReceiveEventParams {}
+export interface MessagingReceiveEventParams {
+  message_id: string
+  context: string
+  direction: 'inbound' | 'outbound'
+  tags: string[]
+  from_number: string
+  to_number: string
+  body: string
+  media: string[]
+  segments: number
+  message_state: 'received'
+}
 
 export interface MessagingReceiveEvent extends SwEvent {
   event_type: MessagingReceive
+  context: string
+  space_id: string
+  project_id: string
   params: MessagingReceiveEventParams
 }
 
