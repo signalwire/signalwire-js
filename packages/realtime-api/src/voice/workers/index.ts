c95987ea84883daafcf5c61561dbda1906712485
Edoardo Gallo <edo91.gallo@gmail.com>
2022-03-30 10:10:42 +0200

    Realtime-API Voice Play Methods (#475)
    * Voice call is not a BaseConsumer anymore
    
    * add voice play types and split workers on different files
    
    * add play method on Call
    
    * [ot] fix jest warnings and use room_session instead of room from room.subscribed event
    
    * realtime-api jest fixes
    
    * test coverage for CallPlayback
    
    * improve types and remove ts-x notations
    
    * add derived methods
    
    * changeset

---
+++ packages/realtime-api/src/voice/workers/index.ts
@@ -0,0 +1,15 @@
+import { toSyntheticEvent } from '@signalwire/core'
+export * from './voiceCallStateWorker'
+export * from './voiceCallReceiveWorker'
+export * from './voiceCallPlayWorker'
+
+export const SYNTHETIC_CALL_STATE_ANSWERED_EVENT = toSyntheticEvent(
+  'calling.call.answered'
+)
+
+// export const SYNTHETIC_CALL_STATE_FAILED_EVENT = toSyntheticEvent(
+//   'calling.call.failed'
+// )
+
+export const SYNTHETIC_CALL_STATE_ENDED_EVENT =
+  toSyntheticEvent('calling.call.ended')
