83eabb4c0beb44d07e6b2fe18144831991742882
Edoardo Gallo <edo91.gallo@gmail.com>
2022-03-31 12:27:40 +0200

    passing tests

--- packages/realtime-api/src/voice/CallPrompt.test.ts
+++ packages/realtime-api/src/voice/CallPrompt.test.ts
@@ -39,13 +39,13 @@ describe('CallPrompt', () => {
       // @ts-expect-error
       expect(instance.execute).toHaveBeenLastCalledWith({
         ...baseExecuteParams,
-        method: 'calling.prompt.stop',
+        method: 'calling.play_and_collect.stop',
       })
 
       await instance.setVolume(5)
       // @ts-expect-error
       expect(instance.execute).toHaveBeenLastCalledWith({
-        method: 'calling.prompt.volume',
+        method: 'calling.play_and_collect.volume',
         params: {
           call_id: 'call_id',
           node_id: 'node_id',
