3f2aa3bf0bce83fc8a6404063d58ec682abf39cb
Francisco Ramini <framini@gmail.com>
2022-04-06 11:03:47 +0200

    add util for checking typeof

--- packages/core/src/utils/common.ts
+++ packages/core/src/utils/common.ts
@@ -9,3 +9,34 @@ export const fromCamelToSnakeCase = <T>(event: T): T => {
     return `_${letter.toLowerCase()}`
   }) as T
 }
+
+type ValidTypes =
+  | 'undefined'
+  | 'null'
+  | 'array'
+  | 'string'
+  | 'number'
+  | 'boolean'
+  | 'set'
+  | 'map'
+  | 'function'
+  | 'regexp'
+  | 'date'
+  | 'symbol'
+  | 'bigint'
+  | 'object'
+
+export const typeOf = <T>(obj: T): ValidTypes => {
+  if (obj === null) {
+    return 'null'
+  } else if (Array.isArray(obj)) {
+    return 'array'
+  } else if (obj !== Object(obj)) {
+    return typeof obj
+  }
+
+  const result = {}.toString.call(obj).slice(8, -1).toLowerCase() as ValidTypes
+
+  // strip function adornments (e.g. "AsyncFunction")
+  return result.indexOf('function') > -1 ? 'function' : result
+}
