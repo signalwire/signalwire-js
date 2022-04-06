bd8b343042f24abfa81fce52f0395ebd30e6f428
Francisco Ramini <framini@gmail.com>
2022-04-06 16:43:46 +0200

    Change params casing for Voice RPC methods (#489)
    * add util for converting a record's props to snake_case
    
    * export util from core
    
    * update `record` method to use camelCase
    
    * improve typings for toSnakeCaseProps
    
    * rename function, update prompt method
    
    * convert remaining prompt params
    
    * use camelCase for devices and add conversion for the internal util
    
    * convert digit props
    
    * add util for checking typeof
    
    * add ability to convert deeply nested arrays
    
    * fix typeof
    
    * fix condition
    
    * remove need of the util

---
+++ packages/core/src/utils/toSnakeCaseKeys.ts
@@ -0,0 +1,32 @@
+import { CamelToSnakeCase } from '../types/utils'
+import { fromCamelToSnakeCase } from './common'
+
+type ToSnakeCaseKeys<T> = {
+  [Property in NonNullable<keyof T> as CamelToSnakeCase<
+    Extract<Property, string>
+  >]: T[Property]
+}
+
+export const toSnakeCaseKeys = <T extends Record<string, any>>(
+  obj: T,
+  transform: (value: string) => any = (value: string) => value,
+  result: Record<string, any> = {}
+) => {
+  if (Array.isArray(obj)) {
+    result = obj.map((item: any, index: number) =>
+      toSnakeCaseKeys(item, transform, result[index])
+    )
+  } else {
+    Object.keys(obj).forEach((key) => {
+      const newKey = fromCamelToSnakeCase(key)
+      // Both 'object's and arrays will enter this branch
+      if (obj[key] && typeof obj[key] === 'object') {
+        result[newKey] = toSnakeCaseKeys(obj[key], transform, result[newKey])
+      } else {
+        result[newKey] = transform(obj[key])
+      }
+    })
+  }
+
+  return result as ToSnakeCaseKeys<T>
+}
