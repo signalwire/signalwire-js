acef8907e2c057d402b73a3b52ce937f82b26bc3
Edoardo Gallo <edo91.gallo@gmail.com>
2022-04-08 11:49:47 +0200

    update VideoManager specs

--- packages/js/src/cantina/VideoManager.test.ts
+++ packages/js/src/cantina/VideoManager.test.ts
@@ -4,23 +4,28 @@ import { actions } from '@signalwire/core'
 
 describe('VideoManager namespace', () => {
   let manager: VideoManager
-  const { session, emitter, store } = configureFullStack()
+  let fullStack: ReturnType<typeof configureFullStack>
+
   beforeEach(() => {
-    emitter.removeAllListeners()
+    fullStack = configureFullStack()
+    fullStack.emitter.removeAllListeners()
     manager = createVideoManagerObject({
-      store,
+      store: fullStack.store,
       // @ts-expect-error
-      emitter,
+      emitter: fullStack.emitter,
     })
     // @ts-expect-error
     manager.execute = jest.fn()
   })
 
-  it('should not call execute without subscribed events', (done) => {
+  afterEach(() => {
+    fullStack.destroy()
+  })
+
+  it('should not call execute without subscribed events', () => {
     manager.subscribe()
     // @ts-expect-error
     expect(manager.execute).not.toHaveBeenCalled()
-    done()
   })
 
   it('should call execute with subscribed events', async () => {
@@ -49,25 +54,25 @@ describe('VideoManager namespace', () => {
         done()
       })
       manager.subscribe().then(() => {
-        session.dispatch(actions.socketMessageAction(firstRoom))
+        fullStack.session.dispatch(actions.socketMessageAction(firstRoom))
       })
     })
   })
 
   describe('video-manager.room.updated event', () => {
     const firstRoom = JSON.parse(
-      `{"jsonrpc":"2.0","id":"81e61d45-b7e3-4096-8221-f5d86ff98134","method":"signalwire.event","params":{"event_type":"video-manager.room.updated","event_channel":"video-manager.rooms","timestamp":1631692502.1308,"params":{"id":"room-id","project_id":"project-id","cantina_id":"12382c06-280c-460f-8db2-c138c77d23e7","name":"room-one","preview":"https://preview.image.url","last_snapshot":"https://preview.video.url","member_count":1,"recording":false,"locked":false,"room_type":"permanent","visibility":"normal","room_description":"mocked-room-description","join_button":"mocked-join-button","order_priority":1,"custom_alone":"alone","custom_canvas":"canvas","custom_empty":"empty room","custom_preview":"https://preview.custom.url","has_sms_from_number":false,"auto_open_nav":false,"my_roles":["visitor","attendee"],"event_channel":"rooms.9b3ef3aa-6047-42aa-8d9d-6fca54211a47"}}}`
+      `{"jsonrpc":"2.0","id":"81e61d45-b7e3-4096-8221-f5d86ff98134","method":"signalwire.event","params":{"event_type":"video-manager.room.started","event_channel":"video-manager.rooms","timestamp":1631692502.1308,"params":{"id":"room-id","project_id":"project-id","cantina_id":"12382c06-280c-460f-8db2-c138c77d23e7","name":"room-one","preview":"https://preview.image.url","last_snapshot":"https://preview.video.url","member_count":1,"recording":false,"locked":false,"room_type":"permanent","visibility":"normal","room_description":"mocked-room-description","join_button":"mocked-join-button","order_priority":1,"custom_alone":"alone","custom_canvas":"canvas","custom_empty":"empty room","custom_preview":"https://preview.custom.url","has_sms_from_number":false,"auto_open_nav":false,"my_roles":["visitor","attendee"],"event_channel":"rooms.9b3ef3aa-6047-42aa-8d9d-6fca54211a47"}}}`
     )
 
     it('should pass RoomSession object to the handler', (done) => {
-      manager.on('room.updated', (room) => {
+      manager.on('room.started', (room) => {
         expect(room.id).toBe('room-id')
         expect(room.name).toBe('room-one')
         expect(room.memberCount).toBe(1)
         done()
       })
       manager.subscribe().then(() => {
-        session.dispatch(actions.socketMessageAction(firstRoom))
+        fullStack.session.dispatch(actions.socketMessageAction(firstRoom))
       })
     })
   })
@@ -85,7 +90,7 @@ describe('VideoManager namespace', () => {
         done()
       })
       manager.subscribe().then(() => {
-        session.dispatch(actions.socketMessageAction(firstRoom))
+        fullStack.session.dispatch(actions.socketMessageAction(firstRoom))
       })
     })
   })
