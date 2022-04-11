fa77b947d82b1c62d9951ee69afcb1505287b656
Francisco Ramini <framini@gmail.com>
2022-04-11 10:00:15 +0200

    Split state and Dial events (#493)
    * rename var to match other workers
    
    * split sagas into state and dial events
    
    * wip for having the dialWorker handling dial events
    
    * restore type
    
    * remove call_state filter so the worker can update the instance with all the call.state events
    
    * add ability to handle errors on dial, remove loop
    
    * remove unused event
    
    * remove handlers when the first event occurs
    
    * type array
    
    * remove unnecessary check

---
+++ packages/realtime-api/src/voice/workers/voiceCallDialWorker.ts
@@ -0,0 +1,60 @@
+import {
+  getLogger,
+  sagaEffects,
+  SagaIterator,
+  SDKWorker,
+  SDKActions,
+  MapToPubSubShape,
+  CallingCallDialEvent,
+} from '@signalwire/core'
+import type { Call } from '../Call'
+import {
+  SYNTHETIC_CALL_DIAL_ANSWERED_EVENT,
+  SYNTHETIC_CALL_DIAL_FAILED_EVENT,
+} from './'
+
+const TARGET_DIAL_STATES: CallingCallDialEvent['params']['dial_state'][] = [
+  'answered',
+  'failed',
+]
+
+export const voiceCallDialWorker: SDKWorker<Call> = function* (
+  options
+): SagaIterator {
+  const { channels, instance } = options
+  const { swEventChannel, pubSubChannel } = channels
+  getLogger().trace('voiceCallDialWorker started')
+
+  const action: MapToPubSubShape<CallingCallDialEvent> = yield sagaEffects.take(
+    swEventChannel,
+    (action: SDKActions) => {
+      if (
+        action.type === 'calling.call.dial' &&
+        TARGET_DIAL_STATES.includes(action.payload.dial_state)
+      ) {
+        return instance.tag === action.payload.tag
+      }
+      return false
+    }
+  )
+
+  if (action.payload.dial_state === 'answered') {
+    yield sagaEffects.put(pubSubChannel, {
+      // @ts-expect-error
+      type: SYNTHETIC_CALL_DIAL_ANSWERED_EVENT,
+      // @ts-expect-error
+      payload: action.payload,
+    })
+  } else if (action.payload.dial_state === 'failed') {
+    yield sagaEffects.put(pubSubChannel, {
+      // @ts-expect-error
+      type: SYNTHETIC_CALL_DIAL_FAILED_EVENT,
+      // @ts-expect-error
+      payload: action.payload,
+    })
+  } else {
+    throw new Error('[voiceCallDialWorker] unhandled call_state')
+  }
+
+  getLogger().trace('voiceCallDialWorker ended')
+}
