6d4157eb9a6fbc360566cd6c89eb3e5ecd94ea42
Francisco Ramini <framini@gmail.com>
2022-04-29 12:52:38 +0200

    add e2e for voice namespace

---
+++ internal/e2e-realtime-api/src/voice.test.ts
@@ -0,0 +1,69 @@
+import { Voice } from '@signalwire/realtime-api'
+import { createTestRunner } from './utils'
+
+const handler = () => {
+  return new Promise<number>(async (resolve, reject) => {
+    const client = new Voice.Client({
+      host: process.env.VOICE_HOST || 'relay.swire.io',
+      project: process.env.VOICE_PROJECT as string,
+      token: process.env.VOICE_TOKEN as string,
+      contexts: [process.env.VOICE_CONTEXT as string],
+      // debug: {
+      //   logWsTraffic: true,
+      // },
+    })
+
+    client.on('call.received', async (call) => {
+      console.log('Got call', call.id, call.from, call.to, call.direction)
+
+      try {
+        await call.answer()
+        console.log('Inbound call answered')
+
+        const recording = await call.recordAudio()
+        console.log('Recording STARTED!', recording.id)
+
+        const playlist = new Voice.Playlist({ volume: 2 }).add(
+          Voice.Playlist.TTS({
+            text: 'Message is getting recorded',
+          })
+        )
+        const playback = await call.play(playlist)
+        console.log('Playback', playback.id)
+
+        console.log('Waiting for Playback to end')
+        // TODO: waitForEnded should probably accept a timeout
+        await playback.waitForEnded()
+        console.log('Playback ended')
+
+        console.log('Finishing the call.')
+        await call.hangup()
+
+        resolve(0)
+      } catch (error) {
+        console.error('Error', error)
+        reject(4)
+      }
+    })
+
+    const call = await client.dialPhone({
+      to: process.env.VOICE_DIAL_TO_NUMBER as string,
+      from: process.env.VOICE_DIAL_FROM_NUMBER as string,
+      timeout: 30,
+    })
+
+    console.log('Call resolved', call.id)
+  })
+}
+
+async function main() {
+  const runner = createTestRunner({
+    name: 'Voice E2E',
+    testHandler: handler,
+    executionTime: 30_000,
+  })
+
+  await runner.run()
+}
+
+main()
