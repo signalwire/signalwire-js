4d58da837feddf60e3106038e90dffd7da6b61ab
Francisco Ramini <framini@gmail.com>
2022-05-02 11:19:52 +0200

    upgrade jest to latest

--- packages/js/jest.config.js
+++ packages/js/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   transform: {
     '\\.[jt]sx?$': ['babel-jest', { configFile: './../../babel.config.js' }],
   },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/uuid',
+  ],
+  setupFiles: ['./src/setupTests.ts'],
 }
