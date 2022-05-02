239ab4563566333e06b0e25d6deeb31216ee5e81
Edoardo Gallo <edo91.gallo@gmail.com>
2022-05-02 14:08:41 +0200

    add refs to the PR discussion

--- packages/realtime-api/src/AutoApplyTransformsConsumer.ts
+++ packages/realtime-api/src/AutoApplyTransformsConsumer.ts
@@ -5,6 +5,8 @@ import { BaseConsumer, EventEmitter } from '@signalwire/core'
  * invoke "signalwire.subscribe" but they need to apply the
  * emitter transforms on each `.on()`/`.once()` call.
  * TODO: improve this logic.
+ * https://github.com/signalwire/signalwire-js/pull/477#discussion_r841623381
+ * https://github.com/signalwire/signalwire-js/pull/477#discussion_r841435646
  */
 export class AutoApplyTransformsConsumer<
   EventTypes extends EventEmitter.ValidEventTypes
