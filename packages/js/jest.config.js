4a5f2201b634af343a06a6411226cf7e7526d2ee
Francisco Ramini <framini@gmail.com>
2022-05-02 11:47:25 +0200

    change way of fixing uuid

--- packages/js/jest.config.js
+++ packages/js/jest.config.js
@@ -2,8 +2,5 @@ module.exports = {
   transform: {
     '\\.[jt]sx?$': ['babel-jest', { configFile: './../../babel.config.js' }],
   },
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/uuid',
-  ],
-  setupFiles: ['./src/setupTests.ts'],
+  resolver: '<rootDir>/test/resolver.js',
 }
--- packages/js/src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-const nodeCrypto = require('crypto');
-
-// @ts-expect-error
-global.crypto = {
-  getRandomValues: function (buffer) {
-    return nodeCrypto.randomFillSync(buffer);
-  }
-};
