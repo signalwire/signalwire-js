654257f67e428758dd2d5709510e6e72fa890fd4
Francisco Ramini <framini@gmail.com>
2022-05-04 11:51:14 +0200

    add tap, add tests for checking digits

--- internal/e2e-realtime-api/src/voice.test.ts
+++ internal/e2e-realtime-api/src/voice.test.ts
@@ -1,3 +1,4 @@
+import tap from 'tap'
 import { Voice } from '@signalwire/realtime-api'
 import { createTestRunner } from './utils'
 
@@ -14,36 +15,44 @@ const handler = () => {
       project: process.env.VOICE_PROJECT as string,
       token: process.env.VOICE_TOKEN as string,
       contexts: [process.env.VOICE_CONTEXT as string],
-      debug: {
-        logWsTraffic: true,
-      },
+      // debug: {
+      //   logWsTraffic: true,
+      // },
     })
 
     client.on('call.received', async (call) => {
       console.log('Got call', call.id, call.from, call.to, call.direction)
 
       try {
-        await call.answer()
-        console.log('Inbound call answered')
+        const resultAnswer = await call.answer()
+        tap.ok(resultAnswer.id, 'Inboud call answered')
+        tap.equal(
+          call.id,
+          resultAnswer.id,
+          'Call answered gets the same instance'
+        )
 
-        // const recording = await call.recordAudio()
-        // console.log('Recording STARTED!', recording.id)
+        const recording = await call.recordAudio()
+        tap.ok(recording.id, 'Recording started')
 
-        // const playlist = new Voice.Playlist({ volume: 2 }).add(
-        //   Voice.Playlist.TTS({
-        //     text: 'Message is getting recorded',
-        //   })
-        // )
-        // const playback = await call.play(playlist)
-        // console.log('Playback', playback.id)
+        const playlist = new Voice.Playlist({ volume: 2 }).add(
+          Voice.Playlist.TTS({
+            text: 'Message is getting recorded',
+          })
+        )
+        const playback = await call.play(playlist)
+        tap.ok(playback.id, 'Playback')
 
-        // console.log('Waiting for Playback to end')
-        // // TODO: waitForEnded should probably accept a timeout
-        // await playback.waitForEnded()
-        // console.log('Playback ended')
+        console.log('Waiting for Playback to end')
+        // TODO: waitForEnded should probably accept a timeout
+        await playback.waitForEnded()
+        tap.pass('Playback ended')
 
         call.on('prompt.started', (p) => {
-          console.log('>> prompt.started', p.id)
+          tap.ok(p.id, 'Prompt has started')
+        })
+        call.on('prompt.ended', (p) => {
+          tap.ok(p.id, 'Prompt has ended')
         })
 
         const prompt = await call.prompt({
@@ -60,36 +69,11 @@ const handler = () => {
             terminators: '#',
           },
         })
-        console.log('> prompt.id', prompt.id)
-
-        call.on('prompt.updated', (p) => {
-          console.log('>> prompt.updated', p.id)
-        })
-        call.on('prompt.failed', (p) => {
-          console.log('>> prompt.failed', p.id, p.reason)
-        })
-        call.on('prompt.ended', (p) => {
-          console.log(
-            '>> prompt.ended',
-            p.id,
-            p.type,
-            'Digits: ',
-            p.digits,
-            'Terminator',
-            p.terminator
-          )
-        })
 
         const result = await prompt.waitForResult()
 
-        console.log(
-          '---------> RESULT',
-          result.digits,
-          result.confidence,
-          result.terminator,
-          result.text,
-          result.id
-        )
+        tap.equal(prompt.id, result.id, 'Instances are the same')
+        tap.equal(result.digits, '123', 'Correct Digits were entered')
 
         console.log('Finishing the call.')
         await call.hangup()
@@ -104,19 +88,28 @@ const handler = () => {
       from: process.env.VOICE_DIAL_FROM_NUMBER as string,
       timeout: 30,
     })
+    tap.ok(call.id, 'Call resolved')
 
-    console.log('sleeping')
     await sleep(3000)
-    console.log('Call resolved', call.id)
 
-    console.log('sending digits')
-    await call.sendDigits('123#')
+    const sendDigitResult = await call.sendDigits('1w2w3w#')
+    tap.equal(
+      call.id,
+      sendDigitResult.id,
+      'sendDigit returns the same instance'
+    )
 
-    await Promise.all([
-      call.waitFor('ended'),
-      call.waitFor('ending'),
-      call.waitFor(['ending', 'ended']),
-    ])
+    const waitForParams = ['ended', 'ending', ['ending', 'ended']] as const
+    const results = await Promise.all(
+      waitForParams.map((params) => call.waitFor(params as any))
+    )
+    waitForParams.forEach((value, i) => {
+      if (typeof value === 'string') {
+        tap.ok(results[i], `"${value}": completed successfully.`)
+      } else {
+        tap.ok(results[i], `${JSON.stringify(value)}: completed successfully.`)
+      }
+    })
     resolve(0)
   })
 }
