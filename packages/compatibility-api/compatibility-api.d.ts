c9bd5e939773c1e9701e18c646e817bbd70a255e
Francisco Ramini <framini@gmail.com>
2022-05-18 11:20:17 +0200

    `compatibility-api` package (#534)
    * add compatibility-api package
    
    * tighten types for the rest client options and get host, export twilio types
    
    * add construct signature
    
    * improve type definition so we could support the namespace typings
    
    * add types for FaxResponse
    
    * rename to avoid confusion with twilio's version
    
    * add types for webhook and related utils
    
    * fix typings for the TwilioClient
    
    * fix readme
    
    * add changeset

---
+++ packages/compatibility-api/compatibility-api.d.ts
@@ -0,0 +1,50 @@
+import type { Twilio, TwimlInterface } from 'twilio'
+import * as webhookTools from 'twilio/lib/webhooks/webhooks'
+import TwilioClient from 'twilio/lib/rest/Twilio'
+import type { CompatibilityAPIRestClientOptions } from './src/types'
+
+declare function RestClient(
+  username: string,
+  token: string,
+  opts?: CompatibilityAPIRestClientOptions
+): Twilio
+
+declare class FaxResponse {
+  constructor();
+  receive(attributes?: FaxResponse.ReceiveAttributes): void;
+  toString(): string;
+  reject(): void
+}
+
+declare namespace FaxResponse {
+  type ReceiveMediaType = 'application/pdf'|'image/tiff';
+  type ReceivePageSize = 'letter'|'legal'|'a4';
+  export interface ReceiveAttributes {
+    action?: string;
+    mediaType?: ReceiveMediaType;
+    method?: string;
+    pageSize?: ReceivePageSize;
+    storeMedia?: boolean;
+  }
+}
+
+interface TwimlConstructor<T> {
+  new (): T;
+}
+
+declare namespace RestClient {
+  export interface RestClientLaMLInterface extends TwimlInterface {
+    FaxResponse: TwimlConstructor<FaxResponse>;
+  }
+
+  export import Twilio = TwilioClient
+  export const jwt: JwtInterface
+  export const RequestClient: RequestClient
+  export const validateRequest: typeof webhookTools.validateRequest
+  export const validateRequestWithBody: typeof webhookTools.validateRequestWithBody
+  export const validateExpressRequest: typeof webhookTools.validateExpressRequest
+  export const webhook: typeof webhookTools.webhook
+  export const LaML: RestClientLaMLInterface
+}
+
+export = RestClient
