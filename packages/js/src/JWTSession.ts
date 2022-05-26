5d2a571ef4a9587bdeafd2685b2a970df037ffaa
Francisco Ramini <framini@gmail.com>
2022-05-26 11:22:46 +0200

    add ability to get the host from the jwt

--- packages/js/src/JWTSession.ts
+++ packages/js/src/JWTSession.ts
@@ -1,6 +1,23 @@
-import { BaseJWTSession } from '@signalwire/core'
+import jwtDecode from 'jwt-decode'
+import { BaseJWTSession, getLogger, SessionOptions } from '@signalwire/core'
 
 export class JWTSession extends BaseJWTSession {
   public WebSocketConstructor = WebSocket
   public agent = process.env.SDK_PKG_AGENT!
+
+  constructor(public options: SessionOptions) {
+    let decodedJwt
+    try {
+      decodedJwt = jwtDecode<{ ch?: string }>(options.token, { header: true })
+    } catch (e) {
+      if (process.env.NODE_ENV === 'development') {
+        getLogger().error('[JWTSession] error decoding the JWT')
+      }
+    }
+
+    super({
+      ...options,
+      host: decodedJwt?.ch || options.host,
+    })
+  }
 }
