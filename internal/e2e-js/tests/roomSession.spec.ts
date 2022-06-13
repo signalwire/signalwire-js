6884e1e6d3322bf2b42a0fbc0dc4100006488283
Francisco Ramini <framini@gmail.com>
2022-06-13 11:34:32 +0200

    add e2e for join, mute (self) audio and video and leave the room

--- internal/e2e-js/tests/roomSession.spec.ts
+++ internal/e2e-js/tests/roomSession.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { createTestServer, createTestRoomSession } from '../utils'
 
 test.describe('RoomSession', () => {
@@ -13,12 +13,15 @@ test.describe('RoomSession', () => {
     await server.close()
   })
 
-  test('should handle joining a room', async ({ page }) => {
+  test('should handle joining a room, perform actions and then leave the room', async ({
+    page,
+  }) => {
     await page.goto(server.url)
 
+    const roomName = 'another'
     await createTestRoomSession(page, {
       vrt: {
-        room_name: 'another',
+        room_name: roomName,
         user_name: 'e2e_test',
         auto_create_room: true,
         permissions: [
@@ -43,8 +46,8 @@ test.describe('RoomSession', () => {
       },
     })
 
-    // Joining a room
-    await page.evaluate(() => {
+    // Joining the room
+    const joinParams: any = await page.evaluate(() => {
       return new Promise((r) => {
         // @ts-expect-error
         const roomObj = window._roomObj
@@ -52,5 +55,105 @@ test.describe('RoomSession', () => {
         roomObj.join()
       })
     })
+
+    expect(joinParams.room).toBeDefined()
+    expect(joinParams.room_session).toBeDefined()
+    expect(joinParams.room.members.length).toBe(1)
+    expect(joinParams.room.name).toBe(roomName)
+
+    // Checks that the video is visible
+    await page.waitForSelector('div[id^="sw-sdk-"] > video', { timeout: 5000 })
+
+    // Muting Audio (self)
+    await page.evaluate(
+      ({ joinParams }) => {
+        // @ts-expect-error
+        const roomObj = window._roomObj
+
+        const memberUpdatedAudioMuted = new Promise((resolve, reject) => {
+          roomObj.on('member.updated.audio_muted', (params: any) => {
+            if (params.member.id === joinParams.member_id) {
+              resolve(true)
+            } else {
+              reject()
+            }
+          })
+        })
+
+        const memberUpdated = new Promise((resolve, reject) => {
+          roomObj.on('member.updated', (params: any) => {
+            if (
+              params.member.id === joinParams.member_id &&
+              params.member.updated.includes('audio_muted') &&
+              params.member.audio_muted === true
+            ) {
+              resolve(true)
+            } else {
+              reject()
+            }
+          })
+        })
+
+        queueMicrotask(() => roomObj.audioMute())
+
+        return Promise.all([memberUpdatedAudioMuted, memberUpdated])
+      },
+      { joinParams }
+    )
+
+    // Muting Video (self)
+    await page.evaluate(
+      ({ joinParams }) => {
+        // @ts-expect-error
+        const roomObj = window._roomObj
+
+        const memberUpdatedVideoMuted = new Promise((resolve, reject) => {
+          roomObj.on('member.updated.video_muted', (params: any) => {
+            if (params.member.id === joinParams.member_id) {
+              resolve(true)
+            } else {
+              reject()
+            }
+          })
+        })
+
+        const memberUpdated = new Promise((resolve, reject) => {
+          roomObj.on('member.updated', (params: any) => {
+            if (
+              params.member.id === joinParams.member_id &&
+              params.member.updated.includes('video_muted') &&
+              params.member.updated.includes('visible') &&
+              params.member.video_muted === true &&
+              params.member.visible === false
+            ) {
+              resolve(true)
+            } else {
+              reject()
+            }
+          })
+        })
+
+        queueMicrotask(() => roomObj.videoMute())
+
+        return Promise.all([memberUpdatedVideoMuted, memberUpdated])
+      },
+      { joinParams }
+    )
+
+    // Leaving the room
+    await page.evaluate(() => {
+      // @ts-expect-error
+      return window._roomObj.hangup()
+    })
+
+    // Checks that all the elements added by the SDK are gone.
+    const targetElementsCount = await page.evaluate(() => {
+      return {
+        videos: Array.from(document.querySelectorAll('video')).length,
+        rootEl: document.getElementById('rootElement')!.childElementCount,
+      }
+    })
+    expect(targetElementsCount.videos).toBe(0)
+    expect(targetElementsCount.rootEl).toBe(0)
   })
 })
