de481df7b0a7066e12cd52ace48ad7a21747b3b5
Francisco Ramini <framini@gmail.com>
2022-07-07 11:50:45 +0200

    update docs for js pubSub, move paging cursor

--- packages/js/src/chat/index.ts
+++ packages/js/src/chat/index.ts
@@ -1,20 +1,12 @@
 import { Chat } from '@signalwire/core'
+import { PagingCursor } from '../utils/interfaces'
 import ChatMember = Chat.ChatMember
 import ChatMessage = Chat.ChatMessage
 
 export * from './Client'
 
-export type PagingCursor =
-  | {
-      before: string
-      after?: never
-    }
-  | {
-      before?: never
-      after: string
-    }
-
 export {
   ChatMember,
   ChatMessage,
+  PagingCursor
 }
--- packages/js/src/utils/interfaces.ts
+++ packages/js/src/utils/interfaces.ts
@@ -20,7 +20,6 @@ import type {
   RoomSessionPlayback,
   VideoRoomSessionContract,
   OnlyFunctionProperties,
-  AssertSameType,
   MemberListUpdated,
   VideoPositions,
 } from '@signalwire/core'
@@ -28,7 +27,6 @@ import { INTERNAL_MEMBER_UPDATABLE_PROPS } from '@signalwire/core'
 import type { RoomSession } from '../RoomSession'
 import type { RoomSessionDevice } from '../RoomSessionDevice'
 import type { RoomSessionScreenShare } from '../RoomSessionScreenShare'
-import { RoomMemberSelfMethodsInterfaceDocs } from './interfaces.docs'
 
 /**
  * @privateRemarks
@@ -147,27 +145,127 @@ export interface BaseRoomInterface {
   leave(): Promise<unknown>
 }
 
-interface RoomMemberSelfMethodsInterface
-  extends AssertSameType<
-    {
-      audioMute(): Rooms.AudioMuteMember
-      audioUnmute(): Rooms.AudioUnmuteMember
-      videoMute(): Rooms.VideoMuteMember
-      videoUnmute(): Rooms.VideoUnmuteMember
-      /**
-       * @deprecated Use {@link setInputVolume} instead.
-       * `setMicrophoneVolume` will be removed in v4.0.0
-       */
-      setMicrophoneVolume(params: {
-        volume: number
-      }): Rooms.SetInputVolumeMember
-      setInputVolume(params: { volume: number }): Rooms.SetInputVolumeMember
-      setInputSensitivity(params: {
-        value: number
-      }): Rooms.SetInputSensitivityMember
-    },
-    RoomMemberSelfMethodsInterfaceDocs
-  > {}
+interface RoomMemberSelfMethodsInterface {
+  /**
+   * Puts the microphone on mute. The other participants will not hear audio
+   * from the muted device anymore.
+   *
+   * @permissions
+   *  - `room.self.audio_mute`
+   *
+   * You need to specify the permissions when [creating the Video Room
+   * Token](https://developer.signalwire.com/apis/reference/create_room_token)
+   * on the server side.
+   *
+   * @example Muting the microphone:
+   * ```typescript
+   * await roomdevice.audioMute()
+   * ```
+   */
+  audioMute(): Rooms.AudioMuteMember
+
+  /**
+   * Unmutes the microphone if it had been previously muted.
+   *
+   * @permissions
+   *  - `room.self.audio_unmute`
+   *
+   * You need to specify the permissions when [creating the Video Room
+   * Token](https://developer.signalwire.com/apis/reference/create_room_token)
+   * on the server side.
+   *
+   * @example Unmuting the microphone:
+   * ```typescript
+   * await roomdevice.audioUnmute()
+   * ```
+   */
+  audioUnmute(): Rooms.AudioUnmuteMember
+
+  /**
+   * Puts the video on mute. Participants will see a mute image instead of the
+   * video stream.
+   *
+   * @permissions
+   *  - `room.self.video_mute`
+   *
+   * You need to specify the permissions when [creating the Video Room
+   * Token](https://developer.signalwire.com/apis/reference/create_room_token)
+   * on the server side.
+   *
+   * @example Muting the camera:
+   * ```typescript
+   * await roomdevice.videoMute()
+   * ```
+   */
+  videoMute(): Rooms.VideoMuteMember
+
+  /**
+   * Unmutes the video if it had been previously muted. Participants will start
+   * seeing the video stream again.
+   *
+   * @permissions
+   *  - `room.self.video_unmute`
+   *
+   * You need to specify the permissions when [creating the Video Room
+   * Token](https://developer.signalwire.com/apis/reference/create_room_token)
+   * on the server side.
+   *
+   * @example Unmuting the camera:
+   * ```typescript
+   * await roomdevice.videoUnmute()
+   * ```
+   */
+  videoUnmute(): Rooms.VideoUnmuteMember
+
+  /**
+   * @deprecated Use {@link setInputVolume} instead.
+   */
+  setMicrophoneVolume(params: { volume: number }): Rooms.SetInputVolumeMember
+
+  /**
+   * Sets the input volume level (e.g. for the microphone).
+   * @param params
+   * @param params.volume desired volume. Values range from -50 to 50, with a
+   * default of 0.
+   *
+   * @permissions
+   *  - `room.self.set_input_volume`
+   *
+   * You need to specify the permissions when [creating the Video Room
+   * Token](https://developer.signalwire.com/apis/reference/create_room_token)
+   * on the server side.
+   *
+   * @example
+   * ```typescript
+   * await roomdevice.setMicrophoneVolume({volume: -10})
+   * ```
+   */
+  setInputVolume(params: { volume: number }): Rooms.SetInputVolumeMember
+
+  /**
+   * Sets the input level at which the participant is identified as currently
+   * speaking.
+   * @param params
+   * @param params.value desired sensitivity. The default value is 30 and the
+   * scale goes from 0 (lowest sensitivity, essentially muted) to 100 (highest
+   * sensitivity).
+   *
+   * @permissions
+   *  - `room.self.set_input_sensitivity`
+   *
+   * You need to specify the permissions when [creating the Video Room
+   * Token](https://developer.signalwire.com/apis/reference/create_room_token)
+   * on the server side.
+   *
+   * @example
+   * ```typescript
+   * await roomdevice.setInputSensitivity({value: 80})
+   * ```
+   */
+  setInputSensitivity(params: {
+    value: number
+  }): Rooms.SetInputSensitivityMember
+}
 
 /**
  * We are using these interfaces in combination of
@@ -287,3 +385,13 @@ export interface RoomSessionJoinAudienceParams {
   audio?: boolean
   video?: boolean
 }
+
+export type PagingCursor =
+  | {
+      before: string
+      after?: never
+    }
+  | {
+      before?: never
+      after: string
+    }
