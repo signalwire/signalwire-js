b3d37400d4d9e530e2151607881fcaf512bd46de
Francisco Ramini <framini@gmail.com>
2022-07-20 11:49:05 +0200

    move onRoomSubscribed to a saga

--- packages/js/src/utils/constants.ts
+++ packages/js/src/utils/constants.ts
@@ -1,7 +1,6 @@
 export const ROOM_COMPONENT_LISTENERS = {
   state: 'onStateChange',
   remoteSDP: 'onRemoteSDP',
-  roomId: 'onRoomSubscribed',
   videoConstraints: 'onVideoConstraints',
   audioConstraints: 'onAudioConstraints',
   errors: 'onError',
