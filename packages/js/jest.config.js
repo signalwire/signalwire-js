d2c8001aecd2fc84c13011158f00c588574c6cdc
Francisco Ramini <framini@gmail.com>
2022-07-22 09:35:29 +0200

    poc for running different set of tests based on environment

--- packages/js/jest.config.js
+++ packages/js/jest.config.js
@@ -1,7 +1,24 @@
-module.exports = {
+/** @type {import('@jest/types').Config.InitialOptions} */
+const commonConfig = {
+  resolver: '<rootDir>/test/resolver.js',
+  setupFiles: ['./src/setupTests.ts'],
   transform: {
     '\\.[jt]sx?$': ['babel-jest', { configFile: './../../babel.config.js' }],
   },
-  resolver: '<rootDir>/test/resolver.js',
-  setupFiles: ['./src/setupTests.ts'],
+}
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  projects: [
+    {
+      ...commonConfig,
+      displayName: 'stable',
+      testMatch: ['<rootDir>/src/**/*.test.ts'],
+    },
+    {
+      ...commonConfig,
+      displayName: 'experimental',
+      testMatch: ['<rootDir>/src/**/*.exp-test.ts'],
+    },
+  ],
 }
