aa8c37c19a667e990e5d0022fed01b99ef98de52
Francisco Ramini <framini@gmail.com>
2022-08-10 09:34:15 +0200

    change name

--- sw.config.js
+++ sw.config.js
@@ -3,10 +3,10 @@ module.exports = {
     // Maps packages to agent-name. Take into account that
     // these names are just a part within the fully
     // qualified agent sent to the server:
-    // @signalwire/<platform>/<agent-name>/<x.y.x>
+    // @signalwire/<platform>/<sdk-name>/<x.y.x>
     byName: {
       '@signalwire/realtime-api': 'nodejs/realtime-api',
-      '@signalwire/js': 'browser/js',
+      '@signalwire/js': 'js/browser',
       '@signalwire/compatibility-api': 'nodejs/compatibility-api',
     },
   },
