1fa76de5325e7f389ec8541b61322895cac99b3d
Edoardo Gallo <edo91.gallo@gmail.com>
2022-08-10 14:14:32 +0200

    logs on pubSubSaga for cancelled

--- packages/core/src/redux/features/pubSub/pubSubSaga.ts
+++ packages/core/src/redux/features/pubSub/pubSubSaga.ts
@@ -1,5 +1,5 @@
 import { SagaIterator } from '@redux-saga/core'
-import { take } from '@redux-saga/core/effects'
+import { take, cancelled } from '@redux-saga/core/effects'
 import {
   isInternalGlobalEvent,
   toInternalEventName,
@@ -18,32 +18,38 @@ export function* pubSubSaga({
   pubSubChannel,
   emitter,
 }: PubSubSagaParams): SagaIterator<any> {
-  while (true) {
-    const pubSubAction: PubSubAction = yield take(pubSubChannel, '*')
-    const { type, payload } = pubSubAction
-    try {
-      const namespace = findNamespaceInPayload(pubSubAction)
-      /**
-       * There are events (like `video.room.started`/`video.room.ended`) that can
-       * be consumed from different places, like from a `roomObj`
-       * (namespaced Event Emitter) or from a `client`
-       * (non-namespaced/global Event Emitter) so we must trigger the
-       * event twice to reach everyone.
-       */
-      if (isInternalGlobalEvent(type)) {
-        emitter.emit(type, payload)
-      }
+  try {
+    while (true) {
+      const pubSubAction: PubSubAction = yield take(pubSubChannel, '*')
+      const { type, payload } = pubSubAction
+      try {
+        const namespace = findNamespaceInPayload(pubSubAction)
+        /**
+         * There are events (like `video.room.started`/`video.room.ended`) that can
+         * be consumed from different places, like from a `roomObj`
+         * (namespaced Event Emitter) or from a `client`
+         * (non-namespaced/global Event Emitter) so we must trigger the
+         * event twice to reach everyone.
+         */
+        if (isInternalGlobalEvent(type)) {
+          emitter.emit(type, payload)
+        }
 
-      getLogger().trace(
-        'Emit:',
-        toInternalEventName<string>({ namespace, event: type })
-      )
-      emitter.emit(
-        toInternalEventName<string>({ namespace, event: type }),
-        payload
-      )
-    } catch (error) {
-      getLogger().error(error)
+        getLogger().trace(
+          'Emit:',
+          toInternalEventName<string>({ namespace, event: type })
+        )
+        emitter.emit(
+          toInternalEventName<string>({ namespace, event: type }),
+          payload
+        )
+      } catch (error) {
+        getLogger().error(error)
+      }
+    }
+  } finally {
+    if (yield cancelled()) {
+      getLogger().debug('pubSubSaga [cancelled]')
     }
   }
 }
