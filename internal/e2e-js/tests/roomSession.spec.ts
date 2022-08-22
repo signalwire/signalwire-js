03b5b054aa7ddd17d70caf791e001f12ebcbc6de
Edoardo Gallo <edo91.gallo@gmail.com>
2022-08-22 12:34:04 +0200

    restore changes in e2e-js scripts

--- internal/e2e-js/tests/roomSession.spec.ts
+++ internal/e2e-js/tests/roomSession.spec.ts
@@ -66,15 +66,15 @@ test.describe('RoomSession', () => {
 
     // --------------- Joining the room ---------------
     const joinParams: any = await page.evaluate(() => {
-      return new Promise((resolve) => {
+      return new Promise((r) => {
         // @ts-expect-error
         const roomObj = window._roomObj
-        roomObj.on('room.joined', (params: any) => resolve(params))
+        roomObj.on('room.joined', (params: any) => r(params))
         roomObj.join()
       })
     })
 
-    expect(joinParams.room).toBeDefined() // backwards
+    expect(joinParams.room).toBeDefined()
     expect(joinParams.room_session).toBeDefined()
     expect(
       joinParams.room.members.some(
