50f2e07f2e51a11b202d30b38cd37bc0d2270dc6
Edoardo Gallo <edoardo@signalwire.com>
2022-09-22 18:24:07 +0200

    Fix realtime-api get recordings/playbacks/streams  (#657)
    * update video playground
    
    * tests in realtime-api for getRecordings
    
    * pass room_session_id down to the objects
    
    * changeset

--- internal/playground-realtime-api/src/basic/index.ts
+++ internal/playground-realtime-api/src/basic/index.ts
@@ -1,15 +1,40 @@
-import { createClient } from '@signalwire/realtime-api'
+import { Video } from '@signalwire/realtime-api'
 
 async function run() {
   try {
-    const client = await createClient({
+    const video = new Video.Client({
       // @ts-expect-error
-      host: process.env.HOST || 'relay.swire.io',
+      host: process.env.HOST,
       project: process.env.PROJECT as string,
       token: process.env.TOKEN as string,
+      // debug: { logWsTraffic: true },
     })
 
-    await client.connect()
+    video.on('room.started', async (roomSession) => {
+      console.log('Room started', roomSession.id)
+
+      roomSession.on('member.updated', async (member) => {
+        console.log(member)
+      })
+    })
+
+    video.on('room.ended', async (roomSession) => {
+      console.log('Room ended', roomSession.id)
+    })
+
+    const { roomSessions } = await video.getRoomSessions()
+
+    roomSessions.forEach(async (roomSession) => {
+      console.log('RoomSession:', roomSession.id)
+      const { recordings } = await roomSession.getRecordings()
+      console.log('Recordings', recordings.length)
+      recordings.forEach(async (rec) => {
+        console.log('REC', rec.id, rec.roomSessionId, rec.state, rec.startedAt)
+        if (rec.state === 'recording') {
+          await rec.stop()
+        }
+      })
+    })
 
     console.log('Client Running..')
   } catch (error) {
