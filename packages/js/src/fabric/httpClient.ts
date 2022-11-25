110675b82329314ed8937d705312b6ff6755b6f1
Edoardo Gallo <edo91.gallo@gmail.com>
2022-11-25 16:32:11 +0100

    changeset

--- packages/js/src/fabric/httpClient.ts
+++ packages/js/src/fabric/httpClient.ts
@@ -56,42 +56,6 @@ export const createHttpClient = (
     path: string,
     options?: HttpClientRequestInit
   ): Promise<{ body: T }> => {
-    let body: T
-    if (path.includes('addresses')) {
-      // @ts-expect-error
-      body = {
-        links: {
-          self: 'https://stulentsev.signalwire.com/api/fabric/addresses?page_number=0&page_size=1',
-          first:
-            'https://stulentsev.signalwire.com/api/fabric/addresses?page_size=1',
-          next: 'https://stulentsev.signalwire.com/api/fabric/addresses?page_number=1&page_size=1&page_token=PA4659e81b-cddc-4440-81b1-a3adb3b8f511',
-        },
-        data: [
-          {
-            name: 'third-room-with-address',
-            display_name: 'third room with address',
-            resource_id: '55061565-2d5e-409e-9338-28732010cf4b',
-            type: 'room',
-            cover_url: null,
-            preview_url: null,
-            channels: {
-              video: '/public/third-room-with-address?channel=video',
-              voice: '/public/third-room-with-address?channel=voice',
-            },
-          },
-        ],
-      }
-    } else {
-      // @ts-expect-error
-      body = {
-        strategy: 'room',
-        params: {
-          token:
-            'eyJ0eXAiOiJWUlQiLCJjaCI6InJlbGF5LnNpZ25hbHdpcmUuY29tIiwiYWxnIjoiSFM1MTIifQ.eyJpYXQiOjE2NjkyMjEyNTAsImp0aSI6ImU4MDY3YTk2LTcyNTMtNDliZS05ODc1LWU0YjdkNTg4OGI2OSIsInN1YiI6IjRiN2FlNzhhLWQwMmUtNDg4OS1hNjNiLTA4YjE1NmQ1OTE2ZSIsInUiOiJlZG9hcmRvIiwiamEiOiJtZW1iZXIiLCJyIjoic3VwcG9ydC0yMzc3IiwicyI6WyJyb29tLmxpc3RfYXZhaWxhYmxlX2xheW91dHMiLCJyb29tLnNldF9tZXRhIiwicm9vbS5zZXRfbGF5b3V0Iiwicm9vbS5zZWxmLmF1ZGlvX211dGUiLCJyb29tLnNlbGYuYXVkaW9fdW5tdXRlIiwicm9vbS5zZWxmLnZpZGVvX211dGUiLCJyb29tLnNlbGYudmlkZW9fdW5tdXRlIiwicm9vbS5tZW1iZXIuZGVtb3RlIiwicm9vbS5tZW1iZXIucHJvbW90ZSIsInJvb20uaGlkZV92aWRlb19tdXRlZCIsInJvb20ubWVtYmVyLnNldF9tZXRhIiwicm9vbS5zdHJlYW0iXSwiYWNyIjp0cnVlLCJtYSI6ImFsbCIsIm10YSI6e30sInJtdGEiOnt9fQ.nhGWwbBmtd89bKCMG90kWDAZ3bjyvFpbPKH6TNKdEB6a23sj8udj5UQ_PzF07SxXWtYWfS_uxdEGcunxjLOHpA',
-        },
-      }
-    }
-    return { body }
     const headers = {
       ...(options?.body ? { 'Content-Type': 'application/json' } : {}),
       ...globalOptions.headers,
