ee178a810760b5faa34fba41cf7b9e8a9bf083a0
Edoardo Gallo <edo91.gallo@gmail.com>
2023-01-09 20:21:52 +0100

    revert smoke tests

--- internal/e2e-js/utils.ts
+++ internal/e2e-js/utils.ts
@@ -73,10 +73,10 @@ export const createTestRoomSession = async (
         rootElement: document.getElementById('rootElement'),
         audio: true,
         video: true,
-        logLevel: 'debug',
-        debug: {
-          logWsTraffic: true,
-        },
+        logLevel: 'warn',
+        // debug: {
+        //   logWsTraffic: true,
+        // },
       })
 
       options.initialEvents?.forEach((event) => {
