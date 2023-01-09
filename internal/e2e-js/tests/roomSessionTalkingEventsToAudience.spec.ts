d01f761f05514335bc12ca0136875730a136d858
Edoardo Gallo <edo91.gallo@gmail.com>
2023-01-09 20:18:15 +0100

    join audience first

--- internal/e2e-js/tests/roomSessionTalkingEventsToAudience.spec.ts
+++ internal/e2e-js/tests/roomSessionTalkingEventsToAudience.spec.ts
@@ -43,6 +43,41 @@ test.describe('RoomSession talking events to audience', () => {
       createTestRoomSession(pageTwo, audienceSettings),
     ])
 
+    // --------------- Joining from the 2st tab as audience and resolve on 'room.joined' ---------------
+    const pageTwoRoomJoined: any = await pageTwo.evaluate(() => {
+      return new Promise((resolve) => {
+        // @ts-expect-error
+        const roomObj = window._roomObj
+        roomObj.once('room.joined', resolve)
+        roomObj.join()
+      })
+    })
+
+    // --------------- Make sure pageTwo exposes the correct memberId  ---------------
+    await expectMemberId(pageTwo, pageTwoRoomJoined.member_id)
+
+    // --------------- Make sure on pageTwo we have a audience ---------------
+    await expectInteractivityMode(pageTwo, 'audience')
+
+    // --------------- Check SDP/RTCPeer on audience (recvonly since audience) ---------------
+    await expectSDPDirection(pageTwo, 'recvonly', true)
+
+    // Checks that the video is visible on pageTwo
+    await pageTwo.waitForSelector('#rootElement video', {
+      timeout: 10000,
+    })
+
+    // --------------- Resolve when audience receives member.talking ----------
+    const audienceMemberTalkingPromise = pageTwo.evaluate(async () => {
+      return new Promise((resolve) => {
+        // @ts-expect-error
+        const roomObj = window._roomObj
+        roomObj.on('member.talking', resolve)
+      })
+    })
+
+    await pageTwo.waitForTimeout(1000)
+
     // --------------- Joining from the 1st tab as member and resolve on 'room.joined' ---------------
     await pageOne.evaluate(() => {
       return new Promise((resolve) => {
@@ -64,43 +99,8 @@ test.describe('RoomSession talking events to audience', () => {
       timeout: 5000,
     })
 
-    // --------------- Joining from the 2st tab as audience and resolve on 'room.joined' ---------------
-    const pageTwoRoomJoined: any = await pageTwo.evaluate(() => {
-      return new Promise((resolve) => {
-        // @ts-expect-error
-        const roomObj = window._roomObj
-        roomObj.once('room.joined', resolve)
-        roomObj.join()
-      })
-    })
-
-    // Stable ref of the initial memberId for the audience
-    const audienceId = pageTwoRoomJoined.member_id
-
-    // --------------- Make sure pageTwo exposes the correct memberId  ---------------
-    await expectMemberId(pageTwo, audienceId)
-
-    // --------------- Make sure on pageTwo we have a audience ---------------
-    await expectInteractivityMode(pageTwo, 'audience')
-
-    // --------------- Check SDP/RTCPeer on audience (recvonly since audience) ---------------
-    await expectSDPDirection(pageTwo, 'recvonly', true)
-
-    // Checks that the video is visible on pageTwo
-    await pageTwo.waitForSelector('#rootElement video', {
-      timeout: 10000,
-    })
-
-    // --------------- Resolve when audience receives member.talking ----------
-    await pageTwo.evaluate(async () => {
-      return new Promise((resolve) => {
-        // @ts-expect-error
-        const roomObj = window._roomObj
-        roomObj.on('member.talking', resolve)
-      })
-    })
-
-    await pageTwo.waitForTimeout(2000)
+    // Wait for `member.talking` on pageTwo
+    await audienceMemberTalkingPromise
 
     // --------------- Leaving the rooms ---------------
     await Promise.all([
