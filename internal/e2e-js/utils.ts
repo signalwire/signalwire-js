8d33e56e80846e07f8f7313944991df9343c903b
Giacomo Vacca <giacomo.vacca@gmail.com>
2023-01-17 14:56:39 +0100

    e2e audio energy level

--- internal/e2e-js/utils.ts
+++ internal/e2e-js/utils.ts
@@ -73,7 +73,7 @@ export const createTestRoomSession = async (
         host: options.RELAY_HOST,
         token: options.API_TOKEN,
         rootElement: document.getElementById('rootElement'),
-        logLevel: 'debug',
+        logLevel: options.CI ? 'warn' : 'debug',
         debug: {
           logWsTraffic: !options.CI,
         },
