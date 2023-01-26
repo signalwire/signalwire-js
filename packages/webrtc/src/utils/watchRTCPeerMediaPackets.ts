17121e083570613a729d33d5e3b699fbfeffeb92
Edoardo Gallo <edo91.gallo@gmail.com>
2023-01-26 15:01:19 +0100

    add helper to watchRTCPeerMediaPackets

---
+++ packages/webrtc/src/utils/watchRTCPeerMediaPackets.ts
@@ -0,0 +1,69 @@
+import { EventEmitter, getLogger } from '@signalwire/core'
+import type RTCPeer from '../RTCPeer'
+
+export const watchRTCPeerMediaPackets = <
+  T extends EventEmitter.ValidEventTypes
+>(
+  rtcPeer: RTCPeer<T>
+) => {
+  if (!rtcPeer.hasAudioReceiver && !rtcPeer.hasVideoReceiver) {
+    getLogger().warn(
+      `Missing receivers to inspect media for RTCPeer "${rtcPeer.uuid}"`
+    )
+    return
+  }
+  getLogger().debug(`Start watching media for RTCPeer "${rtcPeer.uuid}"`)
+  let previousAudioValue = 0
+  let previousVideoValue = 0
+
+  let timer: ReturnType<typeof setTimeout>
+  const clearTimer = () => {
+    clearTimeout(timer)
+  }
+
+  const meter = async () => {
+    let audioPacketsReceived = 0
+    let videoPacketsReceived = 0
+    try {
+      const stats = await rtcPeer.instance.getStats(null)
+      stats.forEach((report) => {
+        if (report.type === 'inbound-rtp' && report.kind === 'audio') {
+          getLogger().debug(
+            `audio inbound-rtp: packetsReceived: ${report.packetsReceived} (at ${report.lastPacketReceivedTimestamp})`
+          )
+          audioPacketsReceived = report.packetsReceived
+        }
+        if (report.type === 'inbound-rtp' && report.kind === 'video') {
+          getLogger().debug(
+            `video inbound-rtp: packetsReceived: ${report.packetsReceived} (at ${report.lastPacketReceivedTimestamp})`
+          )
+          videoPacketsReceived = report.packetsReceived
+        }
+      })
+    } catch (error) {
+      getLogger().warn('getStats error', error)
+    } finally {
+      const noAudioChanged =
+        audioPacketsReceived && audioPacketsReceived <= previousAudioValue
+      const noVideoChanged =
+        videoPacketsReceived && videoPacketsReceived <= previousVideoValue
+      if (noAudioChanged && noVideoChanged) {
+        getLogger().warn(
+          `audioPacketsReceived: ${audioPacketsReceived} - previousAudioValue: ${previousAudioValue}`
+        )
+        getLogger().warn(
+          `videoPacketsReceived: ${videoPacketsReceived} - previousVideoValue: ${previousVideoValue}`
+        )
+        rtcPeer.triggerResume()
+      } else {
+        previousAudioValue = audioPacketsReceived ?? previousAudioValue
+        previousVideoValue = videoPacketsReceived ?? previousVideoValue
+        clearTimer()
+        timer = setTimeout(() => meter(), rtcPeer.watchMediaPacketsTimeout)
+      }
+    }
+  }
+
+  clearTimer()
+  meter()
+}
