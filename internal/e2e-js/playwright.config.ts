17d60e1aad3ae88bdad5e773041289a71e472b27
Giacomo Vacca <giacomo.vacca@gmail.com>
2023-02-03 17:37:06 +0100

    e2e reattach wrong call id

--- internal/e2e-js/playwright.config.ts
+++ internal/e2e-js/playwright.config.ts
@@ -8,7 +8,7 @@ const config: PlaywrightTestConfig = {
   testDir: 'tests',
   globalSetup: require.resolve('./global-setup'),
   testMatch: testMatch.length ? testMatch : undefined,
-  testIgnore: [],
+  testIgnore: ['roomSessionReattachWrongCallId.spec.ts'],
   timeout: 120_000,
   expect: {
     // Default is 5000
