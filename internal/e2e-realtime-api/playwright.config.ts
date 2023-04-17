363e1c65b8a65e93cb02d70b0cc08a43025bc813
Ammar Ansari <iammaransari@gmail.com>
2023-04-17 20:31:01 +0200

    E2E test for realtime-api video with Playwright (#779)
    * E2E test for realtime-api video with Playwright
    
    * set js app action for realtime-api test
    
    * include playwright as a dependency for e2e-realtime-api

---
+++ internal/e2e-realtime-api/playwright.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, devices } from '@playwright/test'
+
+require('dotenv').config()
+
+const useDesktopChrome = {
+  ...devices['Desktop Chrome'],
+  launchOptions: {
+    args: [
+      '--use-fake-ui-for-media-stream',
+      '--use-fake-device-for-media-stream',
+    ],
+  },
+}
+
+const config = defineConfig({
+  testDir: './src/playwright',
+  globalSetup: require.resolve('./global-setup'),
+  testMatch: undefined,
+  testIgnore: undefined,
+  timeout: 120_000,
+  expect: {
+    timeout: 10_000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  workers: 1,
+  projects: [
+    {
+      name: 'default',
+      use: useDesktopChrome,
+    },
+  ],
+})
+
+export default config
