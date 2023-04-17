9f8cf0943df77297fde264a59d9b5fabe327f4b6
Ammar Ansari <iammaransari@gmail.com>
2023-04-17 20:33:14 +0200

    Use base event emitter for Message API (#782)
    * Use base event emitter for Message API
    
    * emitter transform remove and changeset include
    
    * ApplyEventListeners comment update

---
+++ packages/realtime-api/src/messaging/workers/messagingWorker.ts
@@ -0,0 +1,53 @@
+import {
+  MessagingAction,
+  SDKActions,
+  SDKWorker,
+  SagaIterator,
+  getLogger,
+  sagaEffects,
+} from '@signalwire/core'
+import type { Client } from '../../client/index'
+import { Message } from '../Messaging'
+
+export const messagingWorker: SDKWorker<Client> = function* (
+  options
+): SagaIterator {
+  getLogger().trace('messagingWorker started')
+  const { channels, instance: client } = options
+  const { swEventChannel } = channels
+
+  function* worker(action: MessagingAction) {
+    const { payload, type } = action
+
+    // @ts-expect-error
+    const message = new Message(payload)
+
+    switch (type) {
+      case 'messaging.receive':
+        // @ts-expect-error
+        client.baseEmitter.emit('message.received', message)
+        break
+      case 'messaging.state':
+        // @ts-expect-error
+        client.baseEmitter.emit('message.updated', message)
+        break
+      default:
+        getLogger().warn(`Unknown message event: "${action.type}"`)
+        break
+    }
+  }
+
+  const isMessagingEvent = (action: SDKActions) =>
+    action.type.startsWith('messaging.')
+
+  while (true) {
+    const action: MessagingAction = yield sagaEffects.take(
+      swEventChannel,
+      isMessagingEvent
+    )
+
+    yield sagaEffects.fork(worker, action)
+  }
+
+  getLogger().trace('messagingWorker ended')
+}
