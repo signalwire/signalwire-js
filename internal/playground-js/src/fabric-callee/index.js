b6646a8518931e98c31ebec6d8d2a86b55c2ce6b
Joao Santos <jpsantos@gmail.com>
2023-05-24 14:26:00 -0300

    pass full invite

--- internal/playground-js/src/fabric-callee/index.js
+++ internal/playground-js/src/fabric-callee/index.js
@@ -166,9 +166,9 @@ window.acceptCall = async () => {
   try {
     const jsonrpc = JSON.parse(payload.value)
     console.debug('Payload is:\n', JSON.stringify(jsonrpc, null, 2))
-    const { node_id, params: inviteRPC } = jsonrpc
+    const { node_id } = jsonrpc
 
-    window.__call = await __client._acceptVertoInvite(inviteRPC, node_id)
+    window.__call = await __client._acceptVertoInvite(jsonrpc, node_id)
 
     uiReady()
     connectStatus.innerHTML = 'Answering...'
