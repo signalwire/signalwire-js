f7bb97614aa6a94ff3ae968fe4d0af0deb6687b0
Ammar Ansari <iammaransari@gmail.com>
2023-06-07 15:46:20 +0200

    Task namespace with new interface

---
+++ packages/realtime-api/src/signalwire/SignalWire.ts
@@ -0,0 +1,14 @@
+import { SWClient, SWClientOptions } from './SWClient'
+
+export const SignalWire = (options: SWClientOptions): Promise<SWClient> => {
+  return new Promise(async (resolve, reject) => {
+    const swClient = new SWClient(options)
+
+    try {
+      await swClient.connect()
+      resolve(swClient)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
---
+++ packages/realtime-api/src/signalwire/task/Task.ts
@@ -0,0 +1,101 @@
+import { request } from 'node:https'
+import { TaskInboundEvent } from '@signalwire/core'
+import { SWClient } from '../SWClient'
+import { taskWorker } from '../../task/workers'
+
+const PATH = '/api/relay/rest/tasks'
+const HOST = 'relay.signalwire.com'
+
+export interface TaskListenOptions {
+  topic: string[]
+  onTaskReceived?: (payload: TaskInboundEvent['message']) => unknown
+}
+
+export type TaskListeners = Omit<TaskListenOptions, 'topic'>
+
+export class Task {
+  private _client
+  private _sw
+  private listeners: {
+    key: keyof TaskListeners
+    event: string
+  }[] = [{ key: 'onTaskReceived', event: 'task.received' }]
+
+  constructor(options: SWClient) {
+    this._sw = options
+    this._client = options.client
+
+    this._client.runWorker('taskWorker', {
+      worker: taskWorker,
+    })
+  }
+
+  private subscribeListeners(listeners: TaskListeners) {
+    this.listeners.forEach(({ key, event }) => {
+      // @ts-expect-error
+      this._client._on(event, listeners[key])
+    })
+    const unsubscribe = () => {
+      this.listeners.forEach(({ key, event }) => {
+        // @ts-expect-error
+        this._client._off(event, listeners[key])
+      })
+    }
+    return unsubscribe
+  }
+
+  listen(listenOptions: TaskListenOptions) {
+    return new Promise<() => void>(async (resolve, reject) => {
+      try {
+        const { topic } = listenOptions
+        await this._sw.addTopics(topic)
+        const unsubscribe = this.subscribeListeners(listenOptions)
+        resolve(unsubscribe)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  send({
+    topic,
+    message,
+  }: {
+    topic: string
+    message: Record<string, unknown>
+  }) {
+    if (!this._sw.userOptions.project || !this._sw.userOptions.token) {
+      throw new Error('Invalid options: project and token are required!')
+    }
+    return new Promise<void>((resolve, reject) => {
+      try {
+        const Authorization = `Basic ${Buffer.from(
+          `${this._sw.userOptions.project}:${this._sw.userOptions.token}`
+        ).toString('base64')}`
+
+        const data = JSON.stringify({ context: topic, message })
+        const options = {
+          host: this._sw.userOptions.host ?? HOST,
+          port: 443,
+          method: 'POST',
+          path: PATH,
+          headers: {
+            Authorization,
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+          },
+        }
+        const req = request(options, ({ statusCode }) => {
+          statusCode === 204 ? resolve() : reject()
+        })
+
+        req.on('error', reject)
+
+        req.write(data)
+        req.end()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
