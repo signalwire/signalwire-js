83791351f91b5600a771955fb29821fe2bc2edca
Ammar Ansari <iammaransari@gmail.com>
2023-06-13 15:20:00 +0200

    test return value from camera and mic updated event

--- internal/e2e-js/tests/roomSessionDevices.spec.ts
+++ internal/e2e-js/tests/roomSessionDevices.spec.ts
@@ -31,68 +31,41 @@ test.describe('RoomSessionDevices', () => {
     expect(joinParams.room_session).toBeDefined()
 
     // --------------- Change the microphone & camera ---------------
-    await page.evaluate(async () => {
+    const device = await page.evaluate(async () => {
       // @ts-expect-error
       const roomObj: Video.RoomSession = window._roomObj
 
       const microphoneUpdated = new Promise((resolve) => {
-        roomObj.on('microphone.updated', () => {
-          resolve(true)
+        roomObj.on('microphone.updated', (payload) => {
+          resolve(payload)
         })
       })
 
       const cameraUpdated = new Promise((resolve) => {
-        roomObj.on('camera.updated', () => {
-          resolve(true)
+        roomObj.on('camera.updated', (payload) => {
+          resolve(payload)
+        })
+      })
+
+      const speakerUpdated = new Promise((resolve) => {
+        roomObj.on('speaker.updated', (payload) => {
+          resolve(payload)
         })
       })
 
       await roomObj.updateMicrophone({ deviceId: 'test-mic-id' })
       await roomObj.updateCamera({ deviceId: 'test-camera-id' })
+      // await roomObj.updateSpeaker({ deviceId: 'default' })
 
       return Promise.all([microphoneUpdated, cameraUpdated])
     })
-  })
 
-  test.skip('should emit the speaker updated event', async ({
-    createCustomPage,
-    browserName,
-  }) => {
-    if (browserName.toLowerCase() !== 'chromium') return
-
-    const page = await createCustomPage({ name: '[page]' })
-    await page.goto(SERVER_URL)
-
-    const roomName = randomizeRoomName('join-leave-e2e')
-    await createTestRoomSession(page, {
-      vrt: {
-        room_name: roomName,
-        user_name: 'e2e_test',
-        auto_create_room: true,
-      },
-      initialEvents: [],
-    })
-
-    // --------------- Joining the room ---------------
-    const joinParams = await expectRoomJoined(page)
-
-    expect(joinParams.room).toBeDefined()
-    expect(joinParams.room_session).toBeDefined()
-
-    // --------------- Change the speaker ---------------
-    await page.evaluate(async () => {
-      // @ts-expect-error
-      const roomObj: Video.RoomSession = window._roomObj
-
-      const speakerUpdated = new Promise((resolve) => {
-        roomObj.on('speaker.updated', () => {
-          resolve(true)
-        })
-      })
-
-      await roomObj.updateSpeaker({ deviceId: 'default' })
-
-      return Promise.all([speakerUpdated])
+    expect(device).toHaveLength(2)
+    device.forEach((item) => {
+      expect(item).toHaveProperty('previous.deviceId')
+      expect(item).toHaveProperty('previous.label')
+      expect(item).toHaveProperty('current.deviceId')
+      expect(item).toHaveProperty('current.label')
     })
   })
 })
