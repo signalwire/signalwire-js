349133da03c47f15195f3e6d05fa73a1e7b34d43
edolix <edo91.gallo@gmail.com>
2023-06-23 11:16:58 +0200

    script to prepare static folders for each branch in the repo

---
+++ prepare_public_folder.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -e
+mkdir -p public
+
+branch="eg/gh-workflow-to-pages"
+# ref: https://stackoverflow.com/a/57748047
+# for branch in $(git for-each-ref --format='%(refname:short)' refs/heads); do
+  folder="public/$branch"
+  echo "\nCreate folder $folder"
+  mkdir -p $folder
+  echo "Checkout $branch"
+  git checkout "$branch"
+  
+  echo "NPM install and Build SDK for this branch"
+  # TODO: Build only JS/required sdks 
+  npm i && npm run build
+  
+  echo "Build playgrounds"
+  VITE_BASE="/$branch/" npm run -w=@sw-internal/playground-js build
+
+  echo "Move static assets to 'public'"
+  cp -R ./internal/playground-js/dist/* $folder
+  echo "\n"
+# done
