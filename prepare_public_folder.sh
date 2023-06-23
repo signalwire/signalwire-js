4669bf800576980aceac4b107bea1a419648ef06
edolix <edo91.gallo@gmail.com>
2023-06-23 11:59:29 +0200

    smoke test

--- prepare_public_folder.sh
+++ prepare_public_folder.sh
@@ -4,22 +4,28 @@ set -e
 mkdir -p public
 echo "<h1>JS SDK Playground</h1>" > public/index.html
 
-# ref: https://stackoverflow.com/a/57748047
-for branch in $(git for-each-ref --format='%(refname:short)' refs/heads); do
-  folder="public/$branch"
-  echo "\nCreate folder $folder"
-  mkdir -p $folder
-  echo "Checkout $branch"
-  git checkout "$branch"
-  
-  echo "NPM install and Build SDK for this branch"
-  # TODO: Build only JS/required sdks 
-  npm i && npm run build
-  
-  echo "Build playgrounds"
-  VITE_BASE="/$branch/" npm run -w=@sw-internal/playground-js build
+echo $(git for-each-ref --format='%(refname:short)' refs/heads)
 
-  echo "Move static assets to 'public'"
-  cp -R ./internal/playground-js/dist/* $folder
-  echo "\n"
-done
+git fetch origin
+
+echo $(git for-each-ref --format='%(refname:short)' refs/heads)
+
+# ref: https://stackoverflow.com/a/57748047
+# for branch in $(git for-each-ref --format='%(refname:short)' refs/heads); do
+#   folder="public/$branch"
+#   echo "\nCreate folder $folder"
+#   mkdir -p $folder
+#   echo "Checkout $branch"
+#   git checkout "$branch"
+  
+#   echo "NPM install and Build SDK for this branch"
+#   # TODO: Build only JS/required sdks 
+#   npm i && npm run build
+  
+#   echo "Build playgrounds"
+#   VITE_BASE="/$branch/" npm run -w=@sw-internal/playground-js build
+
+#   echo "Move static assets to 'public'"
+#   cp -R ./internal/playground-js/dist/* $folder
+#   echo "\n"
+# done
