c69af72643084e73791e2fa4ad48980e47a5a600
edolix <edo91.gallo@gmail.com>
2023-06-28 13:14:44 +0200

    _sdpIsValid can return true if there are no ice servers

--- packages/webrtc/src/RTCPeer.ts
+++ packages/webrtc/src/RTCPeer.ts
@@ -679,7 +679,7 @@ export default class RTCPeer<EventTypes extends EventEmitter.ValidEventTypes> {
       return sdpHasValidCandidates(this.localSdp)
     }
 
-    return false
+    return Boolean(this.localSdp)
   }
 
   private _forceNegotiation() {
