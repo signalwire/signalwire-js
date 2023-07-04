9c495bd2480fc117b4ee6a211eed7438f81de9ea
edolix <edo91.gallo@gmail.com>
2023-07-04 11:11:45 +0200

    welcome sessionStorageManager helper

--- packages/js/src/utils/storage.ts
+++ packages/js/src/utils/storage.ts
@@ -1,4 +1,5 @@
-export const CALL_ID = 'callId'
+import jwtDecode from 'jwt-decode'
+import { getLogger } from '@signalwire/core'
 
 /**
  * Note: ready to support RN with a "storage.native.ts" file.
@@ -8,4 +9,24 @@ export const getStorage = () => {
     return window.sessionStorage
   }
   return undefined
+}
+
+export const sessionStorageManager = (token: string) => {
+  let roomName: string = ''
+  try {
+    const jwtPayload = jwtDecode<{ r: string; ja: string }>(token)
+    roomName = jwtPayload?.r ?? ''
+  } catch (e) {
+    if (process.env.NODE_ENV !== 'production') {
+      getLogger().error('[sessionStorageManager] error decoding JWT', token)
+    }
+    roomName = ''
+  }
+
+  const valid = Boolean(roomName)
+  return {
+    authStateKey: valid && `as-${roomName}`,
+    protocolKey: valid && `pt-${roomName}`,
+    callIdKey: valid && `ci-${roomName}`,
+  }
 }
