fe0434607ab52d70d987278e4c196a9dae98c486
Ammar Ansari <iammaransari@gmail.com>
2023-07-25 17:32:30 +0200

    remove event prefix from the base component

--- packages/core/src/BaseComponent.test.ts
+++ packages/core/src/BaseComponent.test.ts
@@ -5,8 +5,6 @@ import { EventEmitter } from './utils/EventEmitter'
 describe('BaseComponent', () => {
   describe('as an event emitter', () => {
     class JestComponent extends BaseComponent<any> {
-      protected _eventsPrefix = 'video' as const
-
       constructor(namespace = '') {
         super({
           store: configureJestStore(),
@@ -23,109 +21,82 @@ describe('BaseComponent', () => {
       instance = new JestComponent()
     })
 
-    it('should transform the event name to the internal format', () => {
-      instance.on('test.event_one', () => {})
-      instance.on('test.eventOne', () => {})
-      instance.once('video.test.eventOne', () => {})
-
-      instance.once('video.test.eventTwo', () => {})
-
-      expect(instance.listenerCount('video.test.event_one')).toEqual(3)
-      expect(instance.listenerCount('video.test.event_two')).toEqual(1)
-    })
-
-    it('should transform the event name to the internal format with namespace', () => {
-      const customInstance = new JestComponent('custom')
-      customInstance.on('test.event_one', () => {})
-      customInstance.on('test.eventOne', () => {})
-      customInstance.once('video.test.eventOne', () => {})
-
-      customInstance.once('video.test.eventTwo', () => {})
-
-      expect(
-        customInstance.listenerCount('custom:video.test.event_one')
-      ).toEqual(3)
-      expect(
-        customInstance.listenerCount('custom:video.test.event_two')
-      ).toEqual(1)
-    })
-
     it('should remove the listeners with .off', () => {
       const instance = new JestComponent('custom')
       const mockOne = jest.fn()
-      instance.on('test.eventOne', mockOne)
-      instance.once('test.eventOne', mockOne)
+      instance._on('test.eventOne', mockOne)
+      instance._once('test.eventOne', mockOne)
       const mockTwo = jest.fn()
-      instance.on('test.eventTwo', mockTwo)
-      instance.once('test.eventTwo', mockTwo)
+      instance._on('test.eventTwo', mockTwo)
+      instance._once('test.eventTwo', mockTwo)
 
-      expect(instance.listenerCount('custom:video.test.event_one')).toEqual(2)
-      expect(instance.listenerCount('custom:video.test.event_two')).toEqual(2)
+      expect(instance.listenerCount('test.eventOne')).toEqual(2)
+      expect(instance.listenerCount('test.eventTwo')).toEqual(2)
 
-      expect(instance.eventNames()).toStrictEqual([
-        'custom:video.test.event_one',
-        'custom:video.test.event_two',
+      expect(instance.baseEventNames()).toStrictEqual([
+        'test.eventOne',
+        'test.eventTwo',
       ])
 
       // No-op
-      instance.off('test.eventOne', () => {})
-      expect(instance.listenerCount('custom:video.test.event_one')).toEqual(2)
+      instance._off('test.eventOne', () => {})
+      expect(instance.listenerCount('test.eventOne')).toEqual(2)
 
-      instance.off('test.eventOne', mockOne)
-      expect(instance.listenerCount('custom:video.test.event_one')).toEqual(0)
+      instance._off('test.eventOne', mockOne)
+      expect(instance.listenerCount('test.eventOne')).toEqual(0)
 
-      instance.off('test.eventTwo', mockTwo)
-      expect(instance.listenerCount('custom:video.test.event_two')).toEqual(0)
+      instance._off('test.eventTwo', mockTwo)
+      expect(instance.listenerCount('test.eventTwo')).toEqual(0)
 
-      expect(instance.eventNames()).toStrictEqual([])
+      expect(instance.baseEventNames()).toStrictEqual([])
     })
 
     it('should remove all the listeners with .removeAllListeners', () => {
       const instance = new JestComponent('custom')
       const mockOne = jest.fn()
-      instance.on('test.eventOne', mockOne)
-      instance.once('test.eventOne', mockOne)
+      instance._on('test.eventOne', mockOne)
+      instance._once('test.eventOne', mockOne)
       const mockTwo = jest.fn()
-      instance.on('test.eventTwo', mockTwo)
-      instance.once('test.eventTwo', mockTwo)
+      instance._on('test.eventTwo', mockTwo)
+      instance._once('test.eventTwo', mockTwo)
 
-      expect(instance.listenerCount('custom:video.test.event_one')).toEqual(2)
-      expect(instance.listenerCount('custom:video.test.event_two')).toEqual(2)
+      expect(instance.listenerCount('test.eventOne')).toEqual(2)
+      expect(instance.listenerCount('test.eventTwo')).toEqual(2)
 
-      expect(instance.eventNames()).toStrictEqual([
-        'custom:video.test.event_one',
-        'custom:video.test.event_two',
+      expect(instance.baseEventNames()).toStrictEqual([
+        'test.eventOne',
+        'test.eventTwo',
       ])
 
       instance.removeAllListeners()
 
-      expect(instance.listenerCount('custom:video.test.event_one')).toEqual(0)
-      expect(instance.listenerCount('custom:video.test.event_two')).toEqual(0)
-      expect(instance.eventNames()).toStrictEqual([])
+      expect(instance.listenerCount('test.eventOne')).toEqual(0)
+      expect(instance.listenerCount('test.eventTwo')).toEqual(0)
+      expect(instance.baseEventNames()).toStrictEqual([])
     })
 
     it('should handle snake_case events', (done) => {
-      const serverEvent = 'video.event.server_side'
+      const serverEvent = 'event.server_side'
       const payload = { test: 1 }
-      instance.on('event.server_side', (data) => {
+      instance._on('event.server_side', (data) => {
         expect(data).toStrictEqual(payload)
 
         done()
       })
 
-      instance.emit(serverEvent, payload)
+      instance.baseEmitter.emit(serverEvent, payload)
     })
 
     it('should handle camelCase events', (done) => {
-      const serverEvent = 'video.event.server_side'
+      const serverEvent = 'event.serverSide'
       const payload = { test: 1 }
-      instance.on('event.serverSide', (data) => {
+      instance._on('event.serverSide', (data) => {
         expect(data).toStrictEqual(payload)
 
         done()
       })
 
-      instance.emit(serverEvent, payload)
+      instance.baseEmitter.emit(serverEvent, payload)
     })
   })
 })
