bff575dc8e15f74e6b08235424510bc2a49622a2
edolix <edo91.gallo@gmail.com>
2023-08-01 12:40:26 +0200

    fix build

--- scripts/sw-build-all/index.js
+++ scripts/sw-build-all/index.js
@@ -66,9 +66,13 @@ export async function cli(args) {
           fs.readFileSync(path.resolve(pathname, 'package.json'), 'utf-8')
         )
 
-        const deps = Object.keys(pkgJson.dependencies || {}).filter((key) =>
-          key.includes('@signalwire')
-        )
+        const deps = Object.keys(pkgJson.dependencies || {}).filter((key) => {
+          return (
+            key.startsWith('@signalwire') &&
+            // compatibility-api is not part of this monorepo
+            key !== '@signalwire/compatibility-api'
+          )
+        })
 
         const pkgName = parsePackageName(pathname)
 
@@ -92,6 +96,7 @@ export async function cli(args) {
 
   console.log('🌲 Constructing the build tree...')
   const pkgDeps = scan(path.join(__dirname, '../../packages'))
+  console.log('pkgDeps', pkgDeps)
   const tree = buildTree(
     pkgDeps,
     new Map([[0, Array.from(packagesWithNoDeps)]])
