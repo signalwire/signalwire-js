5ae8e5f55a6f95a26909f013cc25e98cbc53c7e9
edolix <edo91.gallo@gmail.com>
2023-08-14 14:40:26 -0500

    revert changes

--- scripts/sw-test/runNodeScript.js
+++ scripts/sw-test/runNodeScript.js
@@ -17,7 +17,7 @@ const getScriptOptions = (pathname, config) => {
     const itemPath = pathname + '/' + item
     if (
       fs.lstatSync(itemPath).isFile() &&
-      item.includes('voiceRecording.test.') &&
+      item.includes('.test.') &&
       !ignoreFiles.includes(item)
     ) {
       acc.push({
