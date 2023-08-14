3c9a974605eea7853d67505eea87d9eae81f3e3b
edolix <edo91.gallo@gmail.com>
2023-08-14 13:46:51 -0500

    do not extend from Event at all

--- packages/core/src/utils/SWCloseEvent.ts
+++ packages/core/src/utils/SWCloseEvent.ts
@@ -1,30 +1,17 @@
 /**
- * Polyfill the Event class for React Native platform
- */
-class SwEvent {
-  constructor(public type: string, public options?: EventInit) {}
-}
-
-/**
  * Class representing a close event.
  * The `ws` package does not expose it so we can easily create one in here.
  *
  * @extends Event
  */
-export class SWCloseEvent extends (typeof Event === 'function'
-  ? Event
-  : SwEvent) {
+export class SWCloseEvent {
   public code: number
   public reason: string
   public wasClean: boolean
-  public type: string
   constructor(
-    type: string,
+    public type: string,
     options: { code?: number; reason?: string; wasClean?: boolean } = {}
   ) {
-    super(type)
-
-    this.type = type
     this.code = options.code === undefined ? 0 : options.code
     this.reason = options.reason === undefined ? '' : options.reason
     this.wasClean = options.wasClean === undefined ? false : options.wasClean
