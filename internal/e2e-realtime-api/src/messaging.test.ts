453ccd5316c8a8ac625a1e079c091092c3e01bd0
Ammar Ansari <iammaransari@gmail.com>
2023-08-29 18:32:35 +0200

    e2e test case for messagin api

--- internal/e2e-realtime-api/src/messaging.test.ts
+++ internal/e2e-realtime-api/src/messaging.test.ts
@@ -1,32 +1,46 @@
-import { Messaging } from '@signalwire/realtime-api'
+import { SignalWire } from '@signalwire/realtime-api'
 import { createTestRunner } from './utils'
 
 const handler = () => {
   return new Promise<number>(async (resolve, reject) => {
     const context = process.env.MESSAGING_CONTEXT
 
-    const client = new Messaging.Client({
+    const client = await SignalWire({
       host: process.env.RELAY_HOST as string,
       project: process.env.RELAY_PROJECT as string,
       token: process.env.RELAY_TOKEN as string,
-      contexts: [context],
     })
 
-    client.on('message.received', (message) => {
-      console.log('message.received', message)
-      if (message.state === 'received' && message.body === 'Hello e2e!') {
-        return resolve(0)
-      }
-      console.error('Invalid message on `message.received`', message)
-      return reject(4)
+    const unsub = await client.messaging.listen({
+      topics: [process.env.MESSAGING_CONTEXT!],
+      async onMessageReceived(message) {
+        console.log('message.received', message)
+        if (message.body === 'Hello e2e!') {
+          await unsub()
+
+          client.disconnect()
+
+          return resolve(0)
+        }
+        console.error('Invalid message on `message.received`', message)
+        return reject(4)
+      },
+      onMessageUpdated(message) {
+        // TODO: Test message.updated
+        console.log('message.updated', message)
+      },
     })
 
-    client.on('message.updated', (message) => {
-      // TODO: Test message.updated
-      console.log('message.updated', message)
+    // This should never run since the topics are wrong
+    await client.messaging.listen({
+      topics: ['wrong'],
+      onMessageReceived(message) {
+        console.error('Invalid message on `wrong` topic', message)
+        return reject(4)
+      },
     })
 
-    const response = await client.send({
+    const response = await client.messaging.send({
       context,
       from: process.env.MESSAGING_FROM_NUMBER as string,
       to: process.env.MESSAGING_TO_NUMBER as string,
