a5f54f53499c76499958e1df8dba0e456cfa9e4d
edolix <edo91.gallo@gmail.com>
2023-09-12 13:10:39 +0200

    run only voiceCollect

--- scripts/sw-test/runNodeScript.js
+++ scripts/sw-test/runNodeScript.js
@@ -17,7 +17,7 @@ const getScriptOptions = (pathname, config) => {
     const itemPath = pathname + '/' + item
     if (
       fs.lstatSync(itemPath).isFile() &&
-      item.includes('.test.') &&
+      item.includes('voiceCollect.test.') &&
       !ignoreFiles.includes(item)
     ) {
       acc.push({
