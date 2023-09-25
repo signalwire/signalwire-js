2447f04b6206a3664cf9b96ab0105bf773dacfbf
Ammar Ansari <iammaransari@gmail.com>
2023-09-25 11:44:00 +0200

    fix stack test

--- internal/stack-tests/src/video/app.ts
+++ internal/stack-tests/src/video/app.ts
@@ -1,20 +1,22 @@
-import { Video } from '@signalwire/realtime-api'
+import { SignalWire } from '@signalwire/realtime-api'
 import tap from 'tap'
 
 async function run() {
   try {
-    const video = new Video.Client({
-      // @ts-expect-error
+    const client = await SignalWire({
       host: process.env.RELAY_HOST || 'relay.swire.io',
       project: process.env.RELAY_PROJECT as string,
       token: process.env.RELAY_TOKEN as string,
     })
 
-    tap.ok(video.on, 'video.on is defined')
-    tap.ok(video.once, 'video.once is defined')
-    tap.ok(video.off, 'video.off is defined')
-    tap.ok(video.subscribe, 'video.subscribe is defined')
-    tap.ok(video.removeAllListeners, 'video.removeAllListeners is defined')
+    tap.ok(client.video, 'client.video is defined')
+    tap.ok(client.video.listen, 'client.video.listen is defined')
+    tap.ok(client.video.getRoomSessions, 'video.getRoomSessions is defined')
+    tap.ok(
+      client.video.getRoomSessionById,
+      'video.getRoomSessionById is defined'
+    )
+    tap.ok(client.disconnect, 'video.disconnect is defined')
 
     process.exit(0)
   } catch (error) {
