74815845ecc5cfa65c6bd45a3151ae489f6b2f7d
Ryan Williams <ryan.williams@signalwire.com>
2023-10-03 05:04:56 -0700

    Use the github Playwright reporter in ci (#891)
    * Use the JUnit reporter in order to be processed by test reporter
    
    * Add test reporter action
    
    * Add test reporter action
    
    * Use builtin GHA
    
    * Revert "Add test reporter action"
    
    This reverts commit afe485b10f2797b9eb110f8730b1a2ce6376fe98.
    
    * Revert "Add test reporter action"
    
    This reverts commit b1c46e42e4b50c62d4bc250cd67dd586aa5324ed.

--- internal/e2e-js/playwright.config.ts
+++ internal/e2e-js/playwright.config.ts
@@ -25,6 +25,7 @@ const useDesktopChrome = {
 
 const config: PlaywrightTestConfig = {
   testDir: 'tests',
+  reporter: process.env.CI ? 'github' : 'list',
   globalSetup: require.resolve('./global-setup'),
   testMatch: undefined,
   testIgnore: undefined,
