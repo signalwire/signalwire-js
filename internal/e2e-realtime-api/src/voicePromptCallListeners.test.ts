40b9c3eec999147467291bf56d4ea6a1920883f7
Ammar Ansari <iammaransari@gmail.com>
2023-11-15 20:52:04 +0500

    end the call when caller ends the prompt

--- internal/e2e-realtime-api/src/voicePromptCallListeners.test.ts
+++ internal/e2e-realtime-api/src/voicePromptCallListeners.test.ts
@@ -30,6 +30,11 @@ const handler: TestHandler = ({ domainApp }) => {
         waitForPromptStartResolve = resolve
       })
 
+      let waitForPromptEndResolve: () => void
+      const waitForPromptEnd = new Promise<void>((resolve) => {
+        waitForPromptEndResolve = resolve
+      })
+
       const unsubVoice = await client.voice.listen({
         topics: [domainApp.call_relay_context, 'home'],
         onCallReceived: async (call) => {
@@ -42,7 +47,7 @@ const handler: TestHandler = ({ domainApp }) => {
               'Inbound - Call answered gets the same instance'
             )
 
-            // Wait till the caller start the prompt
+            // Wait until the caller starts the prompt
             await waitForPromptStart
 
             // Send digits 1234 to the caller
@@ -52,6 +57,9 @@ const handler: TestHandler = ({ domainApp }) => {
               sendDigits.id,
               'Inbound - sendDigit returns the same instance'
             )
+
+            // Wait until the caller ends the prompt
+            await waitForPromptEnd
 
             await call.hangup()
           } catch (error) {
@@ -107,7 +115,7 @@ const handler: TestHandler = ({ domainApp }) => {
         'Outbound - Prompt returns the same call instance'
       )
 
-      // Resolve the promise to inform callee
+      // Resolve the prompt start to inform callee
       waitForPromptStartResolve!()
 
       console.log('Waiting for the digits from the inbound call')
@@ -115,6 +123,9 @@ const handler: TestHandler = ({ domainApp }) => {
       // Compare what caller has received
       const recDigits = await prompt.ended()
       tap.equal(recDigits.digits, '1234', 'Outbound - Received the same digit')
+
+      // Resolve the prompt end to inform callee
+      waitForPromptEndResolve!()
 
       // Resolve if the call has ended or ending
       const waitForParams = ['ended', 'ending', ['ending', 'ended']] as const
--- internal/e2e-realtime-api/src/voicePromptListeners.test.ts
+++ internal/e2e-realtime-api/src/voicePromptListeners.test.ts
@@ -29,6 +29,11 @@ const handler: TestHandler = ({ domainApp }) => {
         waitForPromptStartResolve = resolve
       })
 
+      let waitForPromptEndResolve: () => void
+      const waitForPromptEnd = new Promise<void>((resolve) => {
+        waitForPromptEndResolve = resolve
+      })
+
       const unsubVoice = await client.voice.listen({
         topics: [domainApp.call_relay_context, 'home'],
         onCallReceived: async (call) => {
@@ -41,7 +46,7 @@ const handler: TestHandler = ({ domainApp }) => {
               'Inbound - Call answered gets the same instance'
             )
 
-            // Wait till the caller start the prompt
+            // Wait until the caller starts the prompt
             await waitForPromptStart
 
             // Send digits 1234 to the caller
@@ -51,6 +56,9 @@ const handler: TestHandler = ({ domainApp }) => {
               sendDigits.id,
               'Inbound - sendDigit returns the same instance'
             )
+
+            // Wait until the caller ends the prompt
+            await waitForPromptEnd
 
             await call.hangup()
           } catch (error) {
@@ -138,7 +146,7 @@ const handler: TestHandler = ({ domainApp }) => {
         },
       })
 
-      // Resolve the promise to inform callee
+      // Resolve the prompt start to inform callee
       waitForPromptStartResolve!()
 
       console.log('Waiting for the digits from the inbound call')
@@ -146,6 +154,9 @@ const handler: TestHandler = ({ domainApp }) => {
       // Compare what caller has received
       const recDigits = await prompt.ended()
       tap.equal(recDigits.digits, '1234', 'Outbound - Received the same digit')
+
+      // Resolve the prompt end to inform callee
+      waitForPromptEndResolve!()
 
       // Resolve if the call has ended or ending
       const waitForParams = ['ended', 'ending', ['ending', 'ended']] as const
