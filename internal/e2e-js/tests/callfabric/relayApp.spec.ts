6074d03e72664ef64a09c87efd92f2a645165e77
Ammar Ansari <iammaransari@gmail.com>
2023-12-27 17:05:04 +0500

    run only silence test

--- internal/e2e-js/tests/callfabric/relayApp.spec.ts
+++ internal/e2e-js/tests/callfabric/relayApp.spec.ts
@@ -1,89 +1,89 @@
 import { SignalWire } from '@signalwire/realtime-api'
 import {
   createCFClient,
-  expectPageReceiveAudio,
+  // expectPageReceiveAudio,
   getAudioStats,
   SERVER_URL,
 } from '../../utils'
 import { test, expect } from '../../fixtures'
 
 test.describe('CallFabric Relay Application', () => {
-  test('should connect to the relay app and expect an audio playback', async ({
-    createCustomPage,
-  }) => {
-    const client = await SignalWire({
-      host: process.env.RELAY_HOST,
-      project: process.env.RELAY_PROJECT as string,
-      token: process.env.RELAY_TOKEN as string,
-      debug: {
-        logWsTraffic: true,
-      },
-    })
+  // test('should connect to the relay app and expect an audio playback', async ({
+  //   createCustomPage,
+  // }) => {
+  //   const client = await SignalWire({
+  //     host: process.env.RELAY_HOST,
+  //     project: process.env.RELAY_PROJECT as string,
+  //     token: process.env.RELAY_TOKEN as string,
+  //     debug: {
+  //       logWsTraffic: true,
+  //     },
+  //   })
 
-    await client.voice.listen({
-      topics: ['cf-e2e-test-relay'],
-      onCallReceived: async (call) => {
-        try {
-          console.log('Call received', call.id)
+  //   await client.voice.listen({
+  //     topics: ['cf-e2e-test-relay'],
+  //     onCallReceived: async (call) => {
+  //       try {
+  //         console.log('Call received', call.id)
 
-          await call.answer()
-          console.log('Inbound call answered')
+  //         await call.answer()
+  //         console.log('Inbound call answered')
 
-          const playback = await call
-            .playAudio({
-              url: 'https://cdn.signalwire.com/default-music/welcome.mp3',
-            })
-            .onStarted()
-          await playback.setVolume(10)
-        } catch (error) {
-          console.error('Inbound call error', error)
-        }
-      },
-    })
+  //         const playback = await call
+  //           .playAudio({
+  //             url: 'https://cdn.signalwire.com/default-music/welcome.mp3',
+  //           })
+  //           .onStarted()
+  //         await playback.setVolume(10)
+  //       } catch (error) {
+  //         console.error('Inbound call error', error)
+  //       }
+  //     },
+  //   })
 
-    try {
-      const page = await createCustomPage({ name: '[page]' })
-      await page.goto(SERVER_URL)
+  //   try {
+  //     const page = await createCustomPage({ name: '[page]' })
+  //     await page.goto(SERVER_URL)
 
-      await createCFClient(page)
+  //     await createCFClient(page)
 
-      const resourceName = 'cf-e2e-test-relay'
+  //     const resourceName = 'cf-e2e-test-relay'
 
-      await page.evaluate(
-        async (options) => {
-          // @ts-expect-error
-          const client = window._client
+  //     await page.evaluate(
+  //       async (options) => {
+  //         // @ts-expect-error
+  //         const client = window._client
 
-          const call = await client.dial({
-            to: `/public/${options.resourceName}`,
-            nodeId: undefined,
-          })
+  //         const call = await client.dial({
+  //           to: `/public/${options.resourceName}`,
+  //           nodeId: undefined,
+  //         })
 
-          // @ts-expect-error
-          window._roomObj = call
+  //         // @ts-expect-error
+  //         window._roomObj = call
 
-          await call.start()
-        },
-        {
-          resourceName,
-        }
-      )
+  //         await call.start()
+  //       },
+  //       {
+  //         resourceName,
+  //       }
+  //     )
 
-      await expectPageReceiveAudio(page)
+  //     await expectPageReceiveAudio(page)
 
-      // Hangup the call
-      await page.evaluate(async () => {
-        // @ts-expect-error
-        const call = window._roomObj
+  //     // Hangup the call
+  //     await page.evaluate(async () => {
+  //       // @ts-expect-error
+  //       const call = window._roomObj
 
-        await call.hangup()
-      })
+  //       await call.hangup()
+  //     })
 
-      await client.disconnect()
-    } catch (error) {
-      console.error('CreateRoomSession Error', error)
-    }
-  })
+  //     await client.disconnect()
+  //   } catch (error) {
+  //     console.error('CreateRoomSession Error', error)
+  //   }
+  // })
 
   test('should connect to the relay app and expect a silence', async ({
     createCustomPage,
@@ -162,70 +162,70 @@ test.describe('CallFabric Relay Application', () => {
     }
   })
 
-  test('should connect to the relay app and expect a hangup', async ({
-    createCustomPage,
-  }) => {
-    const client = await SignalWire({
-      host: process.env.RELAY_HOST,
-      project: process.env.RELAY_PROJECT as string,
-      token: process.env.RELAY_TOKEN as string,
-      debug: {
-        logWsTraffic: true,
-      },
-    })
+  // test('should connect to the relay app and expect a hangup', async ({
+  //   createCustomPage,
+  // }) => {
+  //   const client = await SignalWire({
+  //     host: process.env.RELAY_HOST,
+  //     project: process.env.RELAY_PROJECT as string,
+  //     token: process.env.RELAY_TOKEN as string,
+  //     debug: {
+  //       logWsTraffic: true,
+  //     },
+  //   })
 
-    await client.voice.listen({
-      topics: ['cf-e2e-test-relay'],
-      onCallReceived: async (call) => {
-        try {
-          console.log('Call received', call.id)
+  //   await client.voice.listen({
+  //     topics: ['cf-e2e-test-relay'],
+  //     onCallReceived: async (call) => {
+  //       try {
+  //         console.log('Call received', call.id)
 
-          await call.answer()
-          console.log('Inbound call answered')
+  //         await call.answer()
+  //         console.log('Inbound call answered')
 
-          await call.hangup()
-        } catch (error) {
-          console.error('Inbound call error', error)
-        }
-      },
-    })
+  //         await call.hangup()
+  //       } catch (error) {
+  //         console.error('Inbound call error', error)
+  //       }
+  //     },
+  //   })
 
-    const page = await createCustomPage({ name: '[page]' })
-    await page.goto(SERVER_URL)
+  //   const page = await createCustomPage({ name: '[page]' })
+  //   await page.goto(SERVER_URL)
 
-    const resourceName = 'cf-e2e-test-relay'
+  //   const resourceName = 'cf-e2e-test-relay'
 
-    await createCFClient(page)
+  //   await createCFClient(page)
 
-    // Dial an address and listen a TTS
-    await page.evaluate(
-      async ({ resourceName }) => {
-        // @ts-expect-error
-        const client = window._client
+  //   // Dial an address and listen a TTS
+  //   await page.evaluate(
+  //     async ({ resourceName }) => {
+  //       // @ts-expect-error
+  //       const client = window._client
 
-        const call = await client.dial({
-          to: `/public/${resourceName}`,
-          nodeId: undefined,
-        })
+  //       const call = await client.dial({
+  //         to: `/public/${resourceName}`,
+  //         nodeId: undefined,
+  //       })
 
-        // @ts-expect-error
-        window._roomObj = call
+  //       // @ts-expect-error
+  //       window._roomObj = call
 
-        await call.start()
-      },
-      { resourceName }
-    )
+  //       await call.start()
+  //     },
+  //     { resourceName }
+  //   )
 
-    await page.waitForTimeout(5000)
+  //   await page.waitForTimeout(5000)
 
-    const roomSession = await page.evaluate(() => {
-      // @ts-expect-error
-      const roomObj = window._roomObj
-      return roomObj
-    })
+  //   const roomSession = await page.evaluate(() => {
+  //     // @ts-expect-error
+  //     const roomObj = window._roomObj
+  //     return roomObj
+  //   })
 
-    expect(roomSession.state).toBe('destroy')
+  //   expect(roomSession.state).toBe('destroy')
 
-    await client.disconnect()
-  })
+  //   await client.disconnect()
+  // })
 })
