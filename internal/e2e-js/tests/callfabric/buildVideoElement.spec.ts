69a8df3234a8a5058a2f76e9350d6129acf2d45a
Ammar Ansari <iammaransari@gmail.com>
2024-05-27 17:10:20 +0200

    add one more e2e test

--- internal/e2e-js/tests/callfabric/buildVideoElement.spec.ts
+++ internal/e2e-js/tests/callfabric/buildVideoElement.spec.ts
@@ -167,4 +167,51 @@ test.describe('buildVideoElement', () => {
     const videoElements4 = await page.$$('div[id^="sw-sdk-"] > video')
     expect(videoElements4).toHaveLength(2)
   })
+
+  test('should render the video even if the function is called before call.start', async ({
+    createCustomPage,
+  }) => {
+    const page = await createCustomPage({ name: '[page]' })
+    await page.goto(SERVER_URL)
+    const roomName = 'cf-e2e-test-room'
+
+    await createCFClient(page)
+
+    // Create and expect 1 video elements
+    await page.evaluate(
+      async ({ roomName }) => {
+        return new Promise<any>(async (resolve, _reject) => {
+          // @ts-expect-error
+          const client = window._client
+
+          const call = await client.dial({
+            to: `/public/${roomName}`,
+            rootElement: document.getElementById('rootElement'),
+          })
+
+          call.on('room.joined', resolve)
+
+          // @ts-expect-error
+          window._roomObj = call
+
+          const rootElement = document.createElement('div')
+          rootElement.id = 'rootElement2'
+          document.body.appendChild(rootElement)
+          // @ts-expect-error
+          await window._SWJS.buildVideoElement({
+            room: call,
+            rootElement,
+          })
+
+          await call.start()
+        })
+      },
+      { roomName }
+    )
+
+    await expectMCUVisible(page)
+
+    const videoElements1 = await page.$$('div[id^="sw-sdk-"] > video')
+    expect(videoElements1).toHaveLength(2)
+  })
 })
