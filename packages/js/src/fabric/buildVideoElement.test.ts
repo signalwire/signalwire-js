d31483d366dc40b5e68e8a05e81a822454d8080a
Ammar Ansari <iammaransari@gmail.com>
2024-05-27 16:54:40 +0200

    fix unit tests

--- packages/js/src/fabric/buildVideoElement.test.ts
+++ packages/js/src/fabric/buildVideoElement.test.ts
@@ -12,6 +12,19 @@ describe('buildVideoElement', () => {
   let stack: ReturnType<typeof configureFullStack>
   let store: any
   let jsdom: JSDOM
+  const callId = 'call-id-1'
+  const mockPeer = {
+    uuid: callId,
+    onRemoteSdp: jest.fn(),
+  }
+
+  const setupRoomForTests = () => {
+    // @ts-expect-error
+    room.getRTCPeerById = jest.fn((_id: string) => mockPeer)
+
+    // @ts-expect-error
+    room.runRTCPeerWorkers(callId)
+  }
 
   beforeEach(() => {
     stack = configureFullStack()
@@ -21,7 +34,7 @@ describe('buildVideoElement', () => {
       // @ts-expect-error
       emitter: stack.emitter,
     })
-
+    setupRoomForTests()
     jsdom = new JSDOM('<!doctype html><html><body></body></html>')
     global.document = jsdom.window.document
     global.HTMLDivElement = jsdom.window.HTMLDivElement
@@ -35,84 +48,47 @@ describe('buildVideoElement', () => {
     delete global.HTMLDivElement
   })
 
-  describe('without the peer', () => {
-    it('should handle errors when the room does not have an RTC peer', async () => {
-      await expect(buildVideoElement({ room })).rejects.toThrow(
-        'No RTC Peer exist on the room!'
-      )
-    })
+  it('should take a video element and return it with unsubscribe function', async () => {
+    const mockRootEl = document.createElement('div')
+    const result = await buildVideoElement({ room, rootElement: mockRootEl })
+
+    expect(result).toHaveProperty('element')
+    expect(result).toHaveProperty('unsubscribe')
+    expect(result.element).toBeInstanceOf(HTMLDivElement)
+    expect(result.unsubscribe).toBeInstanceOf(Function)
   })
 
-  describe('with the peer', () => {
-    const callId = 'call-id-1'
+  it('should create a video element and return it with unsubscribe function', async () => {
+    const result = await buildVideoElement({ room })
 
-    const mockPeer = {
-      uuid: callId,
-      onRemoteSdp: jest.fn(),
-    }
+    expect(result).toHaveProperty('element')
+    expect(result).toHaveProperty('unsubscribe')
+    expect(result.element).toBeInstanceOf(HTMLDivElement)
+    expect(result.unsubscribe).toBeInstanceOf(Function)
+  })
 
-    const setupRoomForTests = () => {
-      // @ts-expect-error
-      room.getRTCPeerById = jest.fn((_id: string) => mockPeer)
+  it('should unsubscribe from all events on unsubscribe', async () => {
+    const mockVideoEl = document.createElement('div')
+    const result = await buildVideoElement({ room, rootElement: mockVideoEl })
 
-      // @ts-expect-error
-      room.runRTCPeerWorkers(callId)
-    }
+    // Mock the room.off function to spy on it
+    room.off = jest.fn()
 
-    beforeEach(() => {
-      setupRoomForTests()
-    })
+    result.unsubscribe()
 
-    afterEach(() => {
-      jest.clearAllMocks()
-    })
-
-    it('should take a video element and return it with unsubscribe function', async () => {
-      const mockRootEl = document.createElement('div')
-      const result = await buildVideoElement({ room, rootElement: mockRootEl })
-
-      expect(result).toHaveProperty('element')
-      expect(result).toHaveProperty('unsubscribe')
-      expect(result.element).toBeInstanceOf(HTMLDivElement)
-      expect(result.unsubscribe).toBeInstanceOf(Function)
-    })
-
-    it('should create a video element and return it with unsubscribe function', async () => {
-      const result = await buildVideoElement({ room })
-
-      expect(result).toHaveProperty('element')
-      expect(result).toHaveProperty('unsubscribe')
-      expect(result.element).toBeInstanceOf(HTMLDivElement)
-      expect(result.unsubscribe).toBeInstanceOf(Function)
-    })
-
-    it('should unsubscribe from all events on unsubscribe', async () => {
-      const mockVideoEl = document.createElement('div')
-      const result = await buildVideoElement({ room, rootElement: mockVideoEl })
-
-      // Mock the room.off function to spy on it
-      room.off = jest.fn()
-
-      result.unsubscribe()
-
-      expect(room.off).toHaveBeenCalledWith('track', expect.any(Function))
-      expect(room.off).toHaveBeenCalledWith(
-        'layout.changed',
-        expect.any(Function)
-      )
-      expect(room.off).toHaveBeenCalledWith(
-        'room.subscribed',
-        expect.any(Function)
-      )
-      expect(room.off).toHaveBeenCalledWith(
-        'member.updated.video_muted',
-        expect.any(Function)
-      )
-      expect(room.off).toHaveBeenCalledWith(
-        `${LOCAL_EVENT_PREFIX}.mirror.video`,
-        expect.any(Function)
-      )
-      expect(room.off).toHaveBeenCalledWith('destroy', expect.any(Function))
-    })
+    expect(room.off).toHaveBeenCalledWith('track', expect.any(Function))
+    expect(room.off).toHaveBeenCalledWith(
+      'layout.changed',
+      expect.any(Function)
+    )
+    expect(room.off).toHaveBeenCalledWith(
+      'member.updated.video_muted',
+      expect.any(Function)
+    )
+    expect(room.off).toHaveBeenCalledWith(
+      `${LOCAL_EVENT_PREFIX}.mirror.video`,
+      expect.any(Function)
+    )
+    expect(room.off).toHaveBeenCalledWith('destroy', expect.any(Function))
   })
 })
