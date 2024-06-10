534b51d72318fde6da738b754bf384415b592a1d
Ryan Williams <ryan.williams@signalwire.com>
2024-06-10 13:51:20 -0700

    add pause

--- internal/e2e-js/tests/v2Webrtc/v2WebrtcFromRest.spec.ts
+++ internal/e2e-js/tests/v2Webrtc/v2WebrtcFromRest.spec.ts
@@ -52,8 +52,10 @@ test.describe('v2WebrtcFromRestSilence', () => {
 
     await expectRelayConnected(pageCallee, envRelayProject, jwtCallee)
 
+    // TODO: review doing silence without tts
     const inlineLaml = `<?xml version="1.0" encoding="UTF-8"?>
       <Response>
+      <Pause length="60"/>
       <Say>
         <prosody volume="silent">Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak Words to speak</prosody>
       </Say>
