2c5ffde720225272e71394efa665646673089bb6
Ryan Williams <ryan.williams@signalwire.com>
2024-06-10 12:56:52 -0700

    put a string on it

--- packages/realtime-api/src/task/Task.ts
+++ packages/realtime-api/src/task/Task.ts
@@ -68,9 +68,11 @@ export class Task extends BaseNamespace<TaskListenOptions, TaskEvents> {
             'Content-Length': data.length,
           },
         }
+        getLogger().debug(options)
 
         getLogger().debug('Task send -', data)
         const req = request(options, ({ statusCode }) => {
+          getLogger().debug(statusCode)
           statusCode === 204 ? resolve() : reject()
         })
 
