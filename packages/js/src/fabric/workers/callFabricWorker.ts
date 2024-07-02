97ca3fb8f55a15558ebbe24a3439f3fe332eab89
Joao Santos <jpsantos@gmail.com>
2024-07-02 15:12:39 -0300

    rename

--- packages/js/src/fabric/workers/callFabricWorker.ts
+++ packages/js/src/fabric/workers/callFabricWorker.ts
@@ -35,13 +35,13 @@ export const callFabricWorker: SDKWorker<CallFabricRoomSessionConnection> =
       const originCallId = action.payload.origin_call_id
       const isCallStateEvent = action.type === 'call.state'
       const isCallJoinedEvent = action.type === 'call.joined'
-      const wasOriginCallJoinedHandled = !!cfRoomSessionConnection.selfMember
+      const shouldDiscardEvent = !!cfRoomSessionConnection.selfMember
 
       return (
         // FIXME call.state events are not beeing fired after the call.joined as expected
         isCallStateEvent ||
         (isCallJoinedEvent &&
-          (wasOriginCallJoinedHandled ||
+          (shouldDiscardEvent ||
             (eventRoutingId === originCallId || action.payload.origin_call_id === roomEventsRoutingId)))
       )
     }
