dbe1c009a8b3e2f5720eef458bde8c9c1e5a33a9
Joao Santos <jpsantos@gmail.com>
2024-07-02 16:08:11 -0300

    revert formating

--- packages/core/src/utils/toInternalAction.ts
+++ packages/core/src/utils/toInternalAction.ts
@@ -3,7 +3,7 @@ import { MapToPubSubShape } from '../redux/interfaces'
 import { isWebrtcEventType } from './common'
 
 export const toInternalAction = <
-  T extends { event_type: string; params?: unknown; node_id?: string}
+  T extends { event_type: string; params?: unknown; node_id?: string }
 >(
   event: T
 ) => {
