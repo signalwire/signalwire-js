616f84048c52e1f5e9f38e6f2d2f8d90196a1b4a
João Paulo Barbosa Marques dos Santos <jpsantos@gmail.com>
2024-07-22 10:42:14 -0300

    CF Session Reattach (#1059)
    * add reattch method
    
    * save and restore previous CallId
    
    * replace callSegments with callSegmentWorker
    
    * atempting to reattch
    
    * merge fix
    
    * merge fix
    
    * use reattaching
    
    * Update packages/core/src/redux/interfaces.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * review
    
    * Update packages/js/src/fabric/workers/callSegmentWorker.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * revert actions refactor
    
    * destroy on call.left from the original segment
    
    * from main
    
    * cleanup
    
    * fix
    
    * review changes
    
    * fix
    
    * fix
    
    * cleanup
    
    * fix tests and bugs
    
    * fix build
    
    * Update packages/core/src/utils/toInternalAction.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * Update packages/js/src/fabric/CallFabricRoomSession.test.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * fix call.state events
    
    * revert change
    
    * fix hangup
    
    * fix
    
    * new e2e tests for reattach added
    
    * fixed swml reattach tests and updated readme
    
    * 3 tests consistently passing
    
    * fix to handle room events routing
    
    * lint fix
    
    * Update packages/js/src/fabric/workers/callSegmentWorker.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * Update packages/core/src/utils/toInternalAction.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * fix "==" issues
    
    * fix CallFabricAction type
    
    * rename
    
    * rename again
    
    * Update packages/js/src/fabric/workers/callSegmentWorker.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * revert formating
    
    * revert formating
    
    * revert formating
    
    * revert formating
    
    * typo
    
    * destroy on hangup
    
    * revert
    
    * type fix
    
    * Update packages/js/src/fabric/workers/callSegmentWorker.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * cleanup
    
    * destroy on hangup
    
    * hardening
    
    * cleanup
    
    * cleanup
    
    * typo
    
    * destroy on destroy event
    
    * fix tests
    
    * Update packages/js/src/fabric/CallFabricRoomSession.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * Update packages/core/src/BaseComponent.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * Update packages/core/src/redux/index.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * Update packages/js/src/fabric/CallFabricRoomSession.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * Update packages/js/src/fabric/workers/callJoinWorker.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * expect destroy
    
    * fix test
    
    * missing destroy expect
    
    * lint
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * changeset
    
    * fix
    
    * review changes
    
    * Update packages/js/src/fabric/CallFabricRoomSession.ts
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    
    * cleanup
    
    * cleanup
    
    ---------
    
    Co-authored-by: Ammar Ansari <iammaransari@gmail.com>
    Co-authored-by: Aye Min Aung <ayemin@signalwire.com>

---
+++ internal/e2e-js/tests/callfabric/reattach.spec.ts
@@ -0,0 +1,183 @@
+import { uuid } from '@signalwire/core'
+import { test, expect } from '../../fixtures'
+import {
+  SERVER_URL,
+  createCFClient,
+  expectMCUVisible
+} from '../../utils'
+
+test.describe('Reattach Tests', () => {
+  test('WebRTC to Room', async ({
+    createCustomPage,
+    resource,
+  }) => {
+
+    const page = await createCustomPage({ name: '[page]' })
+    await page.goto(SERVER_URL)
+
+    const roomName = `e2e-video-room_${uuid()}`
+    await resource.createVideoRoomResource(roomName)
+
+    await createCFClient(page)
+
+    // Dial an address and join a video room
+    let roomSession = await page.evaluate(
+      async ({ roomName }) => {
+        return new Promise<any>(async (resolve, _reject) => {
+          // @ts-expect-error
+          const client = window._client
+
+          const call = await client.dial({
+            to: `/public/${roomName}`,
+            rootElement: document.getElementById('rootElement'),
+          })
+
+          call.on('call.joined', resolve)
+
+          // @ts-expect-error
+          window._roomObj = call
+
+          await call.start()
+        })
+      },
+      { roomName }
+    )
+
+    expect(roomSession.room_session).toBeDefined()
+    const currentCallId = roomSession.call_id
+
+    await expectMCUVisible(page)
+
+    await page.reload({ waitUntil: 'domcontentloaded'})
+    await createCFClient(page)
+
+    // Reattach to an address to join the same call session
+    roomSession = await page.evaluate(
+      async ({ roomName }) => {
+        return new Promise<any>(async (resolve, _reject) => {
+          // @ts-expect-error
+          const client = window._client
+
+          const call = await client.reattach({
+            to: `/public/${roomName}`,
+            rootElement: document.getElementById('rootElement'),
+          })
+
+          call.on('call.joined', resolve)
+
+          // @ts-expect-error
+          window._roomObj = call
+          await call.start()
+        })
+      },
+      { roomName }
+    )
+
+    expect(roomSession.call_id).toEqual(currentCallId)
+    // TODO the server is not sending a layout state on reattach 
+    // await expectMCUVisible(page)
+  })
+
+  // TODO uncomment after fixed in the backend
+  // test('WebRTC to SWML to Room', async ({
+  //   createCustomPage,
+  //   resource,
+  // }) => {
+
+  //   const page = await createCustomPage({ name: '[page]' })
+  //   await page.goto(SERVER_URL)
+
+  //   const roomName = `e2e-video-room_${uuid()}`
+  //   await resource.createVideoRoomResource(roomName)
+  //   const resourceName = `e2e-swml-app_${uuid()}`
+  //   await resource.createSWMLAppResource({
+  //     name: resourceName,
+  //     contents: {
+  //       sections: {
+  //         main: [
+  //           'answer',
+  //           {
+  //             play: {
+  //               volume: 10,
+  //               urls: [
+  //                 'silence:1.0',
+  //                 'say:Hello, connecting to a fabric resource that is a room',
+  //               ],
+  //             },
+  //             connect: {
+  //               to: `/public/${roomName}`,
+  //               answer_on_bridge: true
+  //             }
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   })
+
+  //   await createCFClient(page)
+
+  //   // Dial an address and join a video room
+  //   let roomSession = await page.evaluate(
+  //     async ({ resourceName }) => {
+  //       return new Promise<any>(async (resolve, _reject) => {
+  //         // @ts-expect-error
+  //         const client = window._client
+  //         let callJoinedCount = 0
+
+  //         const call = await client.dial({
+  //           to: `/private/${resourceName}`,
+  //           rootElement: document.getElementById('rootElement'),
+  //         })
+
+  //         call.on('call.joined', (event: any) => {
+  //           callJoinedCount++
+  //           if(callJoinedCount >= 2) {
+  //             resolve(event)
+  //           }
+  //         })
+
+  //         // @ts-expect-error
+  //         window._roomObj = call
+
+  //         await call.start()
+  //       })
+  //     },
+  //     { resourceName }
+  //   )
+
+  //   expect(roomSession.room_session).toBeDefined()
+  //   const currentCallId = roomSession.call_id
+
+  //   await expectMCUVisible(page)
+
+  //   await page.reload({ waitUntil: 'domcontentloaded'})
+  //   await createCFClient(page)
+
+  //   // FIXME Server is not accepting the invite
+  //   // Reattach to an address to join the same call session
+  //   roomSession = await page.evaluate(
+  //     async ({ resourceName }) => {
+  //       return new Promise<any>(async (resolve, _reject) => {
+  //         // @ts-expect-error
+  //         const client = window._client
+
+  //         const call = await client.reattach({
+  //           to: `/private/${resourceName}`,
+  //           rootElement: document.getElementById('rootElement'),
+  //         })
+
+  //         call.on('call.joined', resolve)
+
+  //         // @ts-expect-error
+  //         window._roomObj = call
+  //         await call.start()
+  //       })
+  //     },
+  //     { resourceName }
+  //   )
+
+  //   expect(roomSession.call_id).toEqual(currentCallId)
+  //   // TODO the server is not sending a layout state on reattach 
+  //   // await expectMCUVisible(page)
+  // })
+})
