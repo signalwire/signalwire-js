edcb3bdd4e6f9d3ebbcc2d568e0ed2abf65b70bc
Ammar Ansari <iammaransari@gmail.com>
2024-08-29 17:03:50 +0200

    more e2e tests

--- internal/e2e-js/tests/callfabric/reattach.spec.ts
+++ internal/e2e-js/tests/callfabric/reattach.spec.ts
@@ -122,6 +122,69 @@ test.describe('CallFabric Reattach', () => {
     expect(remoteIP).toBe(remoteIPReattached)
   })
 
+  test('should fail reattach with bad auth', async ({
+    createCustomPage,
+    resource,
+  }) => {
+    const page = await createCustomPage({ name: '[page]' })
+    await page.goto(SERVER_URL)
+
+    const roomName = `e2e-video-room-reattach-bad-auth_${uuid()}`
+    await resource.createVideoRoomResource(roomName)
+
+    await createCFClient(page)
+
+    // Dial an address and join a video room
+    const roomSession = await expectCallJoined(page, {
+      to: `/public/${roomName}`,
+    })
+    expect(roomSession.room_session).toBeDefined()
+
+    await expectMCUVisible(page)
+
+    // --------------- Reattaching ---------------
+    await page.reload()
+
+    await createCFClient(page)
+
+    console.time('reattach-time')
+    // Dial the same address with a bogus authorization_state
+    const roomSessionReattached = await page.evaluate(
+      async ({ roomName }) => {
+        // Inject wrong values for authorization state
+        const key = 'as-SAT'
+        const state = btoa('just wrong')
+        window.sessionStorage.setItem(key, state)
+        console.log(
+          `Injected authorization state for ${key} with value ${state}`
+        )
+
+        // @ts-expect-error
+        const client = window._client
+        const call = await client.dial({
+          to: `/public/${roomName}`,
+          rootElement: document.getElementById('rootElement'),
+        })
+        // @ts-expect-error
+        window._roomObj = call
+
+        // Now try to reattach, which should not succeed
+        return call.start().catch((error: any) => error)
+      },
+      { roomName }
+    )
+    console.timeEnd('reattach-time')
+
+    const { code, message } = roomSessionReattached
+
+    expect([-32002, '27']).toContain(code)
+    expect([
+      'CALL ERROR',
+      'DESTINATION_OUT_OF_ORDER',
+      'Cannot reattach this call with this member ID',
+    ]).toContain(message)
+  })
+
   // TODO uncomment after fixed in the backend
   // test('WebRTC to SWML to Room', async ({
   //   createCustomPage,
