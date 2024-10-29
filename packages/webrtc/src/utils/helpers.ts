0ea3fa0eb374417086997c72cde14babaa387952
Ammar Ansari <iammaransari@gmail.com>
2024-10-29 19:43:02 +0500

    remove helper changes

--- packages/webrtc/src/utils/helpers.ts
+++ packages/webrtc/src/utils/helpers.ts
@@ -17,7 +17,7 @@ export const getUserMedia = (constraints: MediaStreamConstraints) => {
 
 const _shouldNegotiateVideo = (options: ConnectionOptions) => {
   return (
-    (options.negotiateVideo || (options.video ?? !!options.camId)) &&
+    (options.negotiateVideo || true) &&
     (!options.remoteSdp ||
       hasMediaSection(options.remoteSdp, 'video'))
   )
