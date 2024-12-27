93ab24fc37b573eefc63994fd30f8d14c08139c5
Ammar Ansari <iammaransari@gmail.com>
2024-12-27 17:26:23 +0500

    revert changes for the playwrigth-ws-inspector

--- internal/e2e-js/fixtures.ts
+++ internal/e2e-js/fixtures.ts
@@ -1,4 +1,5 @@
 import type { Video } from '@signalwire/js'
+import { PageWithWsInspector, intercepWsTraffic } from 'playwrigth-ws-inspector'
 import { test as baseTest, expect, type Page } from '@playwright/test'
 import {
   CreateRelayAppResourceParams,
@@ -16,7 +17,9 @@ type CustomPage = Page & {
   swNetworkUp: () => Promise<void>
 }
 type CustomFixture = {
-  createCustomPage(options: { name: string }): Promise<CustomPage>
+  createCustomPage(options: {
+    name: string
+  }): Promise<PageWithWsInspector<CustomPage>>
   createCustomVanillaPage(options: { name: string }): Promise<Page>
   resource: {
     createVideoRoomResource: typeof createVideoRoomResource
@@ -28,9 +31,13 @@ type CustomFixture = {
 
 const test = baseTest.extend<CustomFixture>({
   createCustomPage: async ({ context }, use) => {
-    const maker = async (options: { name: string }): Promise<CustomPage> => {
-      const page = await context.newPage()
+    const maker = async (options: {
+      name: string
+    }): Promise<PageWithWsInspector<CustomPage>> => {
+      let page = await context.newPage()
       enablePageLogs(page, options.name)
+      //@ts-ignore
+      page = await intercepWsTraffic(page)
 
       // @ts-expect-error
       page.swNetworkDown = () => {
