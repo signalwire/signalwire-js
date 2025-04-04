bfbdc88646762a04672fa7bac4fdf65f1b815e93
Ammar Ansari <iammaransari@gmail.com>
2025-04-04 13:08:36 +0200

    create multi instance v4 client

--- packages/js/src/fabric/v4/SignalWireV4.ts
+++ packages/js/src/fabric/v4/SignalWireV4.ts
@@ -8,85 +8,68 @@ import {
 } from '../utils/constants'
 import { WSClientV4 } from './WSClientV4'
 
-export const SignalWireV4 = (() => {
-  let instance: Promise<SignalWireClient> | null = null
-
-  return (params: SignalWireClientParams) => {
-    if (!instance) {
-      instance = new Promise<SignalWireClient>(async (resolve, reject) => {
-        try {
-          if (!params.token) {
-            throw new Error('Token is required')
-          }
-
-          const options = {
-            maxApiRequestRetries: DEFAULT_API_REQUEST_RETRIES,
-            apiRequestRetriesDelay: DEFAULT_API_REQUEST_RETRIES_DELAY,
-            apiRequestRetriesDelayIncrement:
-              DEFAULT_API_REQUEST_RETRIES_DELAY_INCREMENT,
-            ...params,
-          }
-
-          const wsClient = new WSClientV4(options)
-          const httpClient = new HTTPClient(options)
-          const conversation = new Conversation({ httpClient, wsClient })
-
-          // Connect the WebSocket and authenticate the user
-          await wsClient.connect()
-
-          resolve({
-            registerDevice: httpClient.registerDevice.bind(httpClient),
-            unregisterDevice: httpClient.unregisterDevice.bind(httpClient),
-            getSubscriberInfo: httpClient.getSubscriberInfo.bind(httpClient),
-            disconnect: async () => {
-              await wsClient.disconnect()
-              instance = null // Reset the singleton instance on disconnect
-            },
-            online: wsClient.online.bind(wsClient),
-            offline: wsClient.offline.bind(wsClient),
-            dial: wsClient.dial.bind(wsClient),
-            reattach: wsClient.reattach.bind(wsClient),
-            handlePushNotification:
-              wsClient.handlePushNotification.bind(wsClient),
-            updateToken: wsClient.updateToken.bind(wsClient),
-            address: {
-              getAddresses: httpClient.getAddresses.bind(httpClient),
-              getAddress: httpClient.getAddress.bind(httpClient),
-            },
-            conversation: {
-              getConversations:
-                conversation.getConversations.bind(conversation),
-              getMessages: conversation.getMessages.bind(conversation),
-              getConversationMessages:
-                conversation.getConversationMessages.bind(conversation),
-              subscribe: conversation.subscribe.bind(conversation),
-              sendMessage: conversation.sendMessage.bind(conversation),
-              join: conversation.joinConversation.bind(conversation),
-            },
-            chat: {
-              getMessages: conversation.getChatMessages.bind(conversation),
-              subscribe: conversation.subscribeChatMessages.bind(conversation),
-              sendMessage: conversation.sendMessage.bind(conversation),
-              join: conversation.joinConversation.bind(conversation),
-            },
-            // @ts-expect-error For debugging purposes
-            on: wsClient.on.bind(wsClient),
-            off: wsClient.off.bind(wsClient),
-            __httpClient: httpClient,
-            __wsClient: wsClient,
-          })
-        } catch (error) {
-          reject(error)
-        }
-      }).catch((error) => {
-        /**
-         * Reset the instance to null explicitly since
-         * Promises are immutable: once rejected, a Promise remains rejected forever.
-         */
-        instance = null
-        throw error
-      })
+export const SignalWireV4 = (
+  params: SignalWireClientParams
+): Promise<SignalWireClient> => {
+  return new Promise(async (resolve, reject) => {
+    if (!params.token) {
+      throw new Error('Token is required')
     }
-    return instance
-  }
-})()
+
+    try {
+      const options = {
+        maxApiRequestRetries: DEFAULT_API_REQUEST_RETRIES,
+        apiRequestRetriesDelay: DEFAULT_API_REQUEST_RETRIES_DELAY,
+        apiRequestRetriesDelayIncrement:
+          DEFAULT_API_REQUEST_RETRIES_DELAY_INCREMENT,
+        ...params,
+      }
+
+      const wsClient = new WSClientV4(options)
+      const httpClient = new HTTPClient(options)
+      const conversation = new Conversation({ httpClient, wsClient })
+
+      // Connect the WebSocket and authenticate the user
+      await wsClient.connect()
+
+      resolve({
+        registerDevice: httpClient.registerDevice.bind(httpClient),
+        unregisterDevice: httpClient.unregisterDevice.bind(httpClient),
+        getSubscriberInfo: httpClient.getSubscriberInfo.bind(httpClient),
+        disconnect: wsClient.disconnect.bind(wsClient),
+        online: wsClient.online.bind(wsClient),
+        offline: wsClient.offline.bind(wsClient),
+        dial: wsClient.dial.bind(wsClient),
+        reattach: wsClient.reattach.bind(wsClient),
+        handlePushNotification: wsClient.handlePushNotification.bind(wsClient),
+        updateToken: wsClient.updateToken.bind(wsClient),
+        address: {
+          getAddresses: httpClient.getAddresses.bind(httpClient),
+          getAddress: httpClient.getAddress.bind(httpClient),
+        },
+        conversation: {
+          getConversations: conversation.getConversations.bind(conversation),
+          getMessages: conversation.getMessages.bind(conversation),
+          getConversationMessages:
+            conversation.getConversationMessages.bind(conversation),
+          subscribe: conversation.subscribe.bind(conversation),
+          sendMessage: conversation.sendMessage.bind(conversation),
+          join: conversation.joinConversation.bind(conversation),
+        },
+        chat: {
+          getMessages: conversation.getChatMessages.bind(conversation),
+          subscribe: conversation.subscribeChatMessages.bind(conversation),
+          sendMessage: conversation.sendMessage.bind(conversation),
+          join: conversation.joinConversation.bind(conversation),
+        },
+        // @ts-expect-error For debugging purposes
+        on: wsClient.on.bind(wsClient),
+        off: wsClient.off.bind(wsClient),
+        __httpClient: httpClient,
+        __wsClient: wsClient,
+      })
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
