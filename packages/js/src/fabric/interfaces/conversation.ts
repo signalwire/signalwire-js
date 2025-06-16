6690517cc5634ca98e859e49f2d8a9ed3eca7ce9
Ammar Ansari <iammaransari@gmail.com>
2025-06-16 13:23:30 +0200

    Migrate conversations APIs payload

--- packages/js/src/fabric/interfaces/conversation.ts
+++ packages/js/src/fabric/interfaces/conversation.ts
@@ -66,7 +66,8 @@ export type ConversationChatMessagesSubscribeResult =
   ConversationSubscribeResult
 
 export interface JoinConversationParams {
-  addressId: string
+  fromAddressId: string
+  addressIds: string[]
 }
 
 export interface JoinConversationResponse {
