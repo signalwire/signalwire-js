0826c319ce7ea4a157a77bd6c1cf3a3649ca44f4
Joao Santos <jpsantos@gmail.com>
2025-07-10 10:07:53 -0300

    fix build

--- packages/js/src/fabric/Conversation.ts
+++ packages/js/src/fabric/Conversation.ts
@@ -2,15 +2,10 @@ import { type ConversationEventParams } from '@signalwire/core'
 import { HTTPClient } from './HTTPClient'
 import { WSClient } from './WSClient'
 import type {
-  GetConversationsResponse,
   GetMessagesParams,
   GetConversationsParams,
   GetConversationMessagesParams,
-  GetConversationMessagesResponse,
-  ConversationMessage,
   SendConversationMessageParams,
-  SendConversationMessageResponse,
-  ConversationChatMessage,
   GetMessagesResult,
   GetConversationMessagesResult,
   SendConversationMessageResult,
@@ -21,18 +16,11 @@ import type {
   ConversationChatMessagesSubscribeResult,
   GetConversationChatMessageResult,
   JoinConversationParams,
-  JoinConversationResponse,
   JoinConversationResult,
   ConversationSubscribeResult,
-  GetAddressResponse,
 } from './interfaces'
 import { conversationWorker } from './workers'
-import { buildPaginatedResult } from '../utils/paginatedResult'
-import { makeQueryParamsUrls } from '../utils/makeQueryParamsUrl'
-import { ConversationAPI } from './ConversationAPI'
 
-const DEFAULT_CHAT_MESSAGES_PAGE_SIZE = 10
-const CACHE_ITEM_EXPIRATION = 1000 * 60 * 3 // 3 minutes
 interface ConversationOptions {
   httpClient: HTTPClient
   wsClient: WSClient
@@ -40,20 +28,14 @@ interface ConversationOptions {
 
 // TODO: Implement a TS contract
 export class Conversation {
-  private httpClient: HTTPClient
   private wsClient: WSClient
   private callbacks = new Set<ConversationSubscribeCallback>()
   private chatSubscriptions: Record<
     string,
     Set<ConversationSubscribeCallback>
   > = {}
-  private lookupCache = new Map<
-    string,
-    { lastRequested: number; promise: Promise<GetAddressResponse> }
-  >()
 
   constructor(options: ConversationOptions) {
-    this.httpClient = options.httpClient
     this.wsClient = options.wsClient
 
     this.wsClient.runWorker('conversationWorker', {
@@ -62,23 +44,6 @@ export class Conversation {
         conversation: this,
       },
     })
-  }
-
-  private lookupUsername(addressId: string) {
-    if (
-      Date.now() - (this.lookupCache.get(addressId)?.lastRequested ?? 0) >=
-      CACHE_ITEM_EXPIRATION
-    ) {
-      this.lookupCache.set(addressId, {
-        lastRequested: Date.now(),
-        promise: this.httpClient.getAddress({
-          id: addressId,
-        }),
-      })
-    }
-
-    return async () =>
-      (await this.lookupCache.get(addressId)?.promise)?.display_name
   }
 
   /** @internal */
@@ -114,7 +79,7 @@ export class Conversation {
   }
 
   public async getMessages(
-    params?: GetMessagesParams
+    _params?: GetMessagesParams
   ): Promise<GetMessagesResult> {
     throw new Error(
       'This version Conversation.getMessages is unsupported by the backend. Use @signalwire/client instead.'
@@ -154,7 +119,7 @@ export class Conversation {
   }
 
   public async joinConversation(
-    params: JoinConversationParams
+    _params: JoinConversationParams
   ): Promise<JoinConversationResult> {
     throw new Error(
       'This version Conversation.joinConversation is unsupported by the backend. Use @signalwire/client instead.'
--- packages/js/src/fabric/ConversationAPI.ts
+++ packages/js/src/fabric/ConversationAPI.ts
@@ -1,20 +1,14 @@
-import { GetConversationChatMessageResult } from 'packages/client/dist/client/src'
-import { Conversation } from './Conversation'
 import {
   ConversationAPIGetMessagesParams,
   ConversationAPISendMessageParams,
   ConversationContract,
   ConversationResponse,
   GetConversationMessagesResult,
-  PaginatedResponse,
   SendConversationMessageResponse,
 } from './interfaces'
 
 export class ConversationAPI implements ConversationContract {
-  constructor(
-    private conversation: Conversation,
-    private payload: ConversationResponse
-  ) {}
+  constructor(private payload: ConversationResponse) {}
 
   get id() {
     return this.payload.id
@@ -40,13 +34,24 @@ export class ConversationAPI implements ConversationContract {
     return this.payload.name
   }
 
-  sendMessage(_params: ConversationAPISendMessageParams): Promise<SendConversationMessageResponse> {
+  /**
+   * @deprecated this method is deprecated and will be removed in future versions. Use @signalwire/client instead.
+   */
+  sendMessage(
+    _params: ConversationAPISendMessageParams
+  ): Promise<SendConversationMessageResponse> {
     throw new Error(
       'This version ConversationAPI.sendMessage is unsupported by the backend. Use @signalwire/client instead.'
     )
   }
 
-  getMessages(_params?: ConversationAPIGetMessagesParams): Promise<GetConversationMessagesResult> {
+  /**
+
+   * @deprecated this method is deprecated and will be removed in future versions. Use @signalwire/client instead.
+   */
+  getMessages(
+    _params?: ConversationAPIGetMessagesParams
+  ): Promise<GetConversationMessagesResult> {
     throw new Error(
       'This version ConversationAPI.getMessages is unsupported by the backend. Use @signalwire/client instead.'
     )
