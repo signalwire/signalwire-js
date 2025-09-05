64ba7b097e486e7b3da26b803d790553e7976145
Ammar Ansari <iammaransari@gmail.com>
2025-09-05 16:48:03 +0200

    remove the client pacakge

--- packages/client/src/Client.ts
+++
@@ -1,7 +0,0 @@
-import { ConnectionOptions } from '@signalwire/webrtc'
-import { CallParams } from './fabric/interfaces'
-
-export interface MakeRoomOptions extends CallParams, ConnectionOptions {
-  /** Local media stream to override the local video and audio stream tracks */
-  localStream?: MediaStream
-}
